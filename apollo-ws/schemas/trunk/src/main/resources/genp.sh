d57eb9707988c3153e7212b76ec7f8ac544b4eab
john.d.levander <john.d.levander@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2013-02-09 03:47:11 +0000

    Added the service skeletons

---
+++ apollo-ws/schemas/trunk/src/main/resources/genp.sh
@@ -0,0 +1,10 @@
+FRDSRC=~/devapps/Apollo-FRED-WS-v2
+
+wsdl2py -b -l -f simulatorservice.wsdl -o out
+wsdl2dispatch -e -f simulatorservice.wsdl -o out
+cp out/*.py $FRDSRC
+cp simulatorservice.wsdl $FRDSRC
+cp -r input-types $FRDSRC
+cp -r base-types $FRDSRC
+cp *.xsd $FRDSRC
+ls $FRDSRC
