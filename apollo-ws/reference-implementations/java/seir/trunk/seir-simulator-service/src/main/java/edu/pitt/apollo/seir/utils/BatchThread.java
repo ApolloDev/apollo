e4b328219006015fec23ca0d94e5b4f6b21c7287
john.d.levander <JohnLevander@users.noreply.github.com>
2013-03-19 18:17:53 +0000

    Handing the code off to Nick to continue work on the Cache and the Batch modes

--- apollo-ws/reference-implementations/java/seir/trunk/seir-simulator-service/src/main/java/edu/pitt/apollo/seir/utils/BatchThread.java
+++ apollo-ws/reference-implementations/java/seir/trunk/seir-simulator-service/src/main/java/edu/pitt/apollo/seir/utils/BatchThread.java
@@ -1,38 +1,74 @@
 package edu.pitt.apollo.seir.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Iterator;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import edu.pitt.apollo.types.BatchRunSimulatorResult;
+import edu.pitt.apollo.types.SimulatorConfiguration;
 
 public class BatchThread extends Thread {
 
 	private String batchRunId;
 	private String batchRunIdMd5Hash;
-	private String batchConfigurationFile;
+	private String batchConfigurationFileUrl;
+	private BatchRunSimulatorResult result;
 
-	public BatchThread(String batchConfigurationFile, String batchRunId,
+	public BatchThread(String batchConfigurationFile,
+			BatchRunSimulatorResult result, String batchRunId,
 			String batchRunIdMd5Hash) {
 		this.batchRunId = batchRunId;
-		this.batchConfigurationFile = batchConfigurationFile;
+		this.batchConfigurationFileUrl = batchConfigurationFile;
 		this.batchRunIdMd5Hash = batchRunIdMd5Hash;
+		this.result = result;
 	}
 
 	public void run() {
 		try {
-		RunUtils.setStarted(batchRunIdMd5Hash);
-		//dl the file!
-		
-		//FileUtils.downloadFile(new URL("http://i.imgur.com/XQkplNG.jpg"), RunUtils.getWorkDir(batchRunIdMd5Hash) + "dog.jpg");
-		FileUtils.downloadFile(new URL(batchConfigurationFile), RunUtils.getWorkDir(batchRunIdMd5Hash) + batchRunIdMd5Hash);
-		
-		//run 1 at a time for now
-		
-		
-		// SeirModelAdapter.run(sc, batchRunId);
+			// mark the run as started (write a started.txt file)
+			RunUtils.setStarted(batchRunIdMd5Hash);
+
+			// determine local file to save remote config file to, e.g.
+			// http://blah.com/batchConfig.json should be saved as
+			// /home/apollo/34234.json <--34234.json is the localFn
+			String localFn = RunUtils.getWorkDir(batchRunIdMd5Hash)
+					+ batchRunIdMd5Hash;
+
+			// downlaod the file
+			org.apache.commons.io.FileUtils.copyURLToFile(new URL(
+					batchConfigurationFileUrl), new File(localFn));
+
+			// read each JSON encoded simulatorConfiguration one at a time, uses
+			// Jackson
+			FileInputStream fis = new FileInputStream(new File(localFn));
+			for (Iterator it = new ObjectMapper().readValues(
+					new JsonFactory().createJsonParser(fis),
+					SimulatorConfiguration.class); it.hasNext();) {
+
+				// it.next will return the next simulatorConfiguration
+				SimulatorConfiguration sc = (SimulatorConfiguration) it.next();
 				
-		RunUtils.setFinished(batchRunIdMd5Hash);
+				//here run the simulator
+				System.out.println(sc.getSimulatorIdentification()
+						.getSoftwareDeveloper());
+
+				//for each simulatorConfiguration call edu.pitt.apollo.SeirSimulatorServiceImpl.runSimulation(sc)
+			}
+			fis.close();
+			
+
+			// SeirModelAdapter.run(sc, batchRunId);
+
+			RunUtils.setFinished(batchRunIdMd5Hash);
 		} catch (Exception e) {
 			try {
-				RunUtils.setError(batchRunIdMd5Hash, e.getMessage() + e.getStackTrace());
+				RunUtils.setError(batchRunIdMd5Hash,
+						e.getMessage() + e.getStackTrace());
 				e.printStackTrace();
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
@@ -40,5 +76,4 @@ public class BatchThread extends Thread {
 			}
 		}
 	}
-
 }
