a57886f25404c2c0417359042f2839315a806ed0
JohnLevander <jdl50@pitt.edu>
2013-03-26 04:05:22 +0000

    Updated the schema objects and services to version 1.1

--- apollo-ws/epidemic-simulator-services/FRED/trunk/ApolloFactory.py
+++ apollo-ws/epidemic-simulator-services/FRED/trunk/ApolloFactory.py
@@ -20,20 +20,88 @@ Created on Nov 29, 2012
 '''
 
 from SimulatorService_services_types import *
+import inspect
+
+
 
 class ApolloFactory:
     
     def new_SimulatorConfiguration(self):
         cfg = ns1.SimulatorConfiguration_Def(None).pyclass()
         cfg._authentication = ns1.Authentication_Def(None).pyclass()
-        cfg._simulatorIdentification = ns1.SimulatorIdentification_Def(None).pyclass()
+        cfg._simulatorIdentification = ns1.SoftwareIdentification_Def(None).pyclass()
         cfg._populationInitialization = ns1.SimulatedPopulation_Def(None).pyclass()
         cfg._simulatorTimeSpecification= ns1.SimulatorTimeSpecification_Def(None).pyclass()
         cfg._disease = ns1.Disease_Def(None).pyclass()
-        cfg._antiviralControlMeasure = ns1.AntiviralControlMeasure_Def(None).pyclass()
-        cfg._vaccinationControlMeasure = ns1.VaccinationControlMeasure_Def(None).pyclass()
+        cfg._controlMeasures = ns1.ControlMeasures_Def(None).pyclass()
         return cfg
     
+    def new_Vaccination(self):
+        vacc = ns1.Vaccination_Def(None).pyclass()
+        vacc._vaccinationEfficacy = ns1.TreatmentEfficacyOverTime_Def(None).pyclass()
+        return vacc
+    
+    
+    def new_VaccinationControlMeasure(self):
+        vcm = ns1.VaccinationControlMeasure_Def(None).pyclass()
+        for name,obj in inspect.getmembers(vcm):
+            if name[0:11] == "set_element":
+                #print name
+                obj(None)
+        return vcm
+    
+    def new_AntiviralControlMeasure(self):
+        avcm = ns1.AntiviralTreatmentControlMeasure_Def(None).pyclass()
+        for name,obj in inspect.getmembers(avcm):
+            if name[0:11] == "set_element":
+                #print name
+                obj(None)
+        return avcm
+    
+    
+    def new_Vaccine(self):
+        return ns1.Vaccine_Def(None).pyclass()    
+    
+    def new_Antiviral(self):
+        return ns1.Antiviral_Def(None).pyclass()
+    
+    def new_AntiviralTreatment(self):
+        avt = ns1.AntiviralTreatment_Def(None).pyclass()
+        avt._antiviralTreatmentEfficacy = ns1.TreatmentEfficacyOverTime_Def(None).pyclass()
+        return avt
+                
+    def new_FixedStartTimeControlMeasure(self):
+        cm = ns1.FixedStartTimeControlMeasure_Def(None).pyclass()
+        cm._controlMeasureFixedStartTime = ns1.FixedStartTime_Def(None).pyclass()
+        return cm;
+    
+    def new_TreatmentEfficacyOverTime(self):
+        eff = ns1.TreatmentEfficacyOverTime_Def(None).pyclass()
+        return eff
+        
+    def new_TargetPriorityPopulation(self):
+        tpp = ns1.TargetPriorityPopulation_Def(None).pyclass()        
+        return tpp
+        
+    def new_ReactiveControlMeasure(self):
+        rcm = ns1.ReactiveControlMeasure_Def(self).pyclass()
+        rcm._controlMeasureReactiveTriggersDefinition = ns1.ReactiveTriggersDefinition_Def(None).pyclass()
+        return rcm;
+    
+    
+    def new_SchoolClosureControlMeasure(self):
+        sc = ns1.SchoolClosureControlMeasure_Def(None).pyclass()
+        
+        return sc;
+    def new_PopulationStrataDefinition(self):
+        psd = ns1.PopulationStrataDefinition_Def(None).pyclass()
+               
+        for name,obj in inspect.getmembers(psd):
+            if name[0:11] == "set_element":
+                #print name
+                obj(None)
+        return psd
+
     def new_RunStatus(self):
         run_status = ns1.RunStatus_Def(None).pyclass()
         return run_status
