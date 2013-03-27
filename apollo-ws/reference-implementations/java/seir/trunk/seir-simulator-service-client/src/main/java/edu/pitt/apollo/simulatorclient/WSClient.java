81005b0187fc1bb31b72d9b996fe114cf3b5c7d8
nickmillett <nem41@pitt.edu>
2013-03-27 22:49:56 +0000

    changed to use new apollo types safety commit

--- apollo-ws/reference-implementations/java/seir/trunk/seir-simulator-service-client/src/main/java/edu/pitt/apollo/simulatorclient/WSClient.java
+++ apollo-ws/reference-implementations/java/seir/trunk/seir-simulator-service-client/src/main/java/edu/pitt/apollo/simulatorclient/WSClient.java
@@ -12,9 +12,12 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package edu.pitt.apollo.simulatorclient;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import edu.pitt.apollo.types.AntiviralTreatment;
+import edu.pitt.apollo.types.AntiviralTreatmentControlMeasure;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -31,9 +34,11 @@ import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
-import edu.pitt.apollo.types.AntiviralControlMeasure;
 import edu.pitt.apollo.types.Authentication;
+import edu.pitt.apollo.types.ControlMeasure;
+import edu.pitt.apollo.types.ControlMeasures;
 import edu.pitt.apollo.types.Disease;
+import edu.pitt.apollo.types.FixedStartTimeControlMeasure;
 import edu.pitt.apollo.types.PopulationDiseaseState;
 import edu.pitt.apollo.types.ServiceRegistrationRecord;
 import edu.pitt.apollo.types.SimulatedPopulation;
@@ -41,140 +46,164 @@ import edu.pitt.apollo.types.SimulatorConfiguration;
 import edu.pitt.apollo.types.SimulatorTimeSpecification;
 import edu.pitt.apollo.types.SoftwareIdentification;
 import edu.pitt.apollo.types.TimeStepUnit;
+import edu.pitt.apollo.types.TreatmentEfficacyOverTime;
+import edu.pitt.apollo.types.Vaccination;
 import edu.pitt.apollo.types.VaccinationControlMeasure;
 
 public class WSClient {
-	public static void main(String[] args) throws JsonGenerationException,
-			JsonMappingException, IOException {
-		// SimulatorService service = new SimulatorService(
-		// new URL(
-		// "http://localhost:8080/seirsimulatorservice/services/seirsimulatorservice?wsdl"));
-		// SimulatorServiceEI port = service.getSimulatorServiceEndpoint();
 
-		ServiceRegistrationRecord srr = new ServiceRegistrationRecord();
+    public static void main(String[] args) throws JsonGenerationException,
+            JsonMappingException, IOException {
+        // SimulatorService service = new SimulatorService(
+        // new URL(
+        // "http://localhost:8080/seirsimulatorservice/services/seirsimulatorservice?wsdl"));
+        // SimulatorServiceEI port = service.getSimulatorServiceEndpoint();
 
-		Authentication auth = new Authentication();
-		auth.setRequesterId("fake_user");
-		auth.setRequesterPassword("fake_password");
-		srr.setAuthentication(auth);
+        ServiceRegistrationRecord srr = new ServiceRegistrationRecord();
 
-		// ServiceRecord sr = new ServiceRecord();
-		SoftwareIdentification si = new SoftwareIdentification();
-		si.setSoftwareDeveloper("UPitt,PSC,CMU");
-		si.setSoftwareName("FRED");
-		si.setSoftwareVersion("2.0.1");
-		// si.set
-		// sr.setSoftwareIdentification(si);
+        Authentication auth = new Authentication();
+        auth.setRequesterId("fake_user");
+        auth.setRequesterPassword("fake_password");
+        srr.setAuthentication(auth);
 
-		srr.setSoftwareIdentification(si);
-		srr.setUrl("http://warhol-fred.psc.edu:8087/fred?wsdl");
-		// srr.setUrl("http://localhost:8087/fred?wsdl");
+        // ServiceRecord sr = new ServiceRecord();
+        SoftwareIdentification si = new SoftwareIdentification();
+        si.setSoftwareDeveloper("UPitt,PSC,CMU");
+        si.setSoftwareName("FRED");
+        si.setSoftwareVersion("2.0.1");
+        // si.set
+        // sr.setSoftwareIdentification(si);
 
-		Holder<Boolean> success = new Holder<Boolean>();
-		Holder<String> msg = new Holder<String>();
-		// port.registerService(srr, success, msg);
-		System.out.println(msg.value);
+        srr.setSoftwareIdentification(si);
+        srr.setUrl("http://warhol-fred.psc.edu:8087/fred?wsdl");
+        // srr.setUrl("http://localhost:8087/fred?wsdl");
 
-		SimulatorConfiguration simulatorConfiguration = new SimulatorConfiguration();
+        Holder<Boolean> success = new Holder<Boolean>();
+        Holder<String> msg = new Holder<String>();
+        // port.registerService(srr, success, msg);
+        System.out.println(msg.value);
 
-		simulatorConfiguration.setAuthentication(auth);
-		simulatorConfiguration
-				.setAntiviralControlMeasure(new AntiviralControlMeasure());
-		AntiviralControlMeasure acm = simulatorConfiguration
-				.getAntiviralControlMeasure();
-		acm.setAntiviralCmCompliance(0d);
-		acm.setAntiviralEfficacy(0d);
-		acm.setAntiviralEfficacyDelay(0d);
-		acm.getAntiviralAdminSchedule().add(1d);
-		acm.getAntiviralSupplySchedule().add(1d);
+        SimulatorConfiguration simulatorConfiguration = new SimulatorConfiguration();
 
-		simulatorConfiguration.setSimulatorIdentification(si);
+        simulatorConfiguration.setAuthentication(auth);
+        
+        ControlMeasures controlMeasures = new ControlMeasures();
+        List<FixedStartTimeControlMeasure> fixedStartControlMeasures = controlMeasures.getFixedStartTimeControlMeasures();
+        FixedStartTimeControlMeasure avControlMeasure = new FixedStartTimeControlMeasure();
+        simulatorConfiguration.setControlMeasures(controlMeasures);
+        
+        
+//        simulatorConfiguration.set
+        AntiviralTreatmentControlMeasure acm = new AntiviralTreatmentControlMeasure();
+        TreatmentEfficacyOverTime avEfficacy = new TreatmentEfficacyOverTime();
+        avEfficacy.getEfficacyValues().add(0.0);
+        AntiviralTreatment avTreatment = new AntiviralTreatment();
+        avTreatment.setAntiviralTreatmentEfficacy(avEfficacy);
+        acm.setAntiviralTreatment(avTreatment);
+        acm.setControlMeasureCompliance(0.0);
+        acm.setDescription("antiviral");
+        avControlMeasure.setControlMeasure(acm);
+        fixedStartControlMeasures.add(avControlMeasure);
+        
+        simulatorConfiguration.setSimulatorIdentification(si);
 
-		simulatorConfiguration.setDisease(new Disease());
-		Disease disease = simulatorConfiguration.getDisease();
-		disease.setAsymptomaticInfectionFraction(0.5);
-		disease.setDiseaseName("Influenza");
-		disease.setInfectiousPeriod(3.2);
-		disease.setLatentPeriod(2.0);
-		disease.setReproductionNumber(1.7);
+        simulatorConfiguration.setDisease(new Disease());
+        Disease disease = simulatorConfiguration.getDisease();
+        disease.setAsymptomaticInfectionFraction(0.5);
+        disease.setDiseaseName("Influenza");
+        disease.setInfectiousPeriod(3.2);
+        disease.setLatentPeriod(2.0);
+        disease.setReproductionNumber(1.7);
 
-		simulatorConfiguration
-				.setPopulationInitialization(new SimulatedPopulation());
-		SimulatedPopulation sp = simulatorConfiguration
-				.getPopulationInitialization();
-		sp.setPopulationLocation("42003");
+        simulatorConfiguration.setPopulationInitialization(new SimulatedPopulation());
+        SimulatedPopulation sp = simulatorConfiguration.getPopulationInitialization();
+        sp.setPopulationLocation("42003");
 
-		List<PopulationDiseaseState> ds = sp.getPopulationDiseaseState();
-		PopulationDiseaseState pds = new PopulationDiseaseState();
-		pds.setDiseaseState("susceptible");
-		pds.setPopCount(new BigInteger("1157474"));
-		ds.add(pds);
-		pds = new PopulationDiseaseState();
-		pds.setDiseaseState("exposed");
-		pds.setPopCount(new BigInteger("0"));
-		ds.add(pds);
-		pds = new PopulationDiseaseState();
-		pds.setDiseaseState("infectious");
-		pds.setPopCount(new BigInteger("100"));
-		ds.add(pds);
-		pds = new PopulationDiseaseState();
-		pds.setDiseaseState("recovered");
-		pds.setPopCount(new BigInteger("60920"));
-		ds.add(pds);
+        List<PopulationDiseaseState> ds = sp.getPopulationDiseaseState();
+        PopulationDiseaseState pds = new PopulationDiseaseState();
+        pds.setDiseaseState("susceptible");
+        pds.setFractionOfPopulation(0.94859);
+        ds.add(pds);
+        pds = new PopulationDiseaseState();
+        pds.setDiseaseState("exposed");
+        pds.setFractionOfPopulation(0.00538);
+        ds.add(pds);
+        pds = new PopulationDiseaseState();
+        pds.setDiseaseState("infectious");
+        pds.setFractionOfPopulation(0.00603);
+        ds.add(pds);
+        pds = new PopulationDiseaseState();
+        pds.setDiseaseState("recovered");
+        pds.setFractionOfPopulation(0.04);
+        ds.add(pds);
 
-		simulatorConfiguration
-				.setSimulatorTimeSpecification(new SimulatorTimeSpecification());
-		SimulatorTimeSpecification stc = simulatorConfiguration
-				.getSimulatorTimeSpecification();
-		stc.setRunLength(new BigInteger("30"));
-		stc.setTimeStepUnit(TimeStepUnit.DAY);
-		stc.setTimeStepValue(1d);
+        simulatorConfiguration.setSimulatorTimeSpecification(new SimulatorTimeSpecification());
+        SimulatorTimeSpecification stc = simulatorConfiguration.getSimulatorTimeSpecification();
+        stc.setRunLength(new BigInteger("30"));
+        stc.setTimeStepUnit(TimeStepUnit.DAY);
+        stc.setTimeStepValue(1d);
 
-		simulatorConfiguration
-				.setVaccinationControlMeasure(new VaccinationControlMeasure());
-		VaccinationControlMeasure vcm = simulatorConfiguration
-				.getVaccinationControlMeasure();
-		vcm.setVaccineCmCompliance(0d);
-		vcm.setVaccineEfficacy(0d);
-		vcm.setVaccineEfficacyDelay(0d);
-		for (int i = 0; i < 30; i++) {
-			vcm.getVaccinationAdminSchedule().add(0d);
-			vcm.getVaccineSupplySchedule().add(0d);
-		}
+        FixedStartTimeControlMeasure vaccControlMeasure = new FixedStartTimeControlMeasure();
+        VaccinationControlMeasure vcm = new VaccinationControlMeasure();
+        vcm.setControlMeasureCompliance(0.0);
+        TreatmentEfficacyOverTime vaccEfficacy = new TreatmentEfficacyOverTime();
+        vaccEfficacy.getEfficacyValues().add(0.0);
+        Vaccination vaccTreatment = new Vaccination();
+        vaccTreatment.setVaccinationEfficacy(vaccEfficacy);
+        vcm.setVaccination(vaccTreatment);
+        vcm.setDescription("vaccination");
+        vaccControlMeasure.setControlMeasure(vcm);
+        
+        fixedStartControlMeasures.add(vaccControlMeasure);
+        
+        List<Integer> avSupply = acm.getAntiviralSupplySchedule();
+        List<BigInteger> avAdmin = acm.getAntiviralTreatmentAdministrationCapacity();
+        List<Integer> vaccSupply = vcm.getVaccineSupplySchedule();
+        List<BigInteger> vaccAdmin = vcm.getVaccinationAdministrationCapacity();
+        
+        for (int i = 0; i < 30; i++) {
+            vaccAdmin.add(new BigInteger("0"));
+            vaccSupply.add(0);
+            avAdmin.add(new BigInteger("0"));
+            avSupply.add(0);
+        }
 
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
-		FileOutputStream out = new FileOutputStream(new File(
-				"test.json"));
-		for (int i = 0; i < 50000; i++) {
-			simulatorConfiguration.getSimulatorIdentification()
-					.setSoftwareDeveloper(String.valueOf(i));
-			mapper.writeValue(out, simulatorConfiguration);
-		}
-		out.close();
+//        XStream x = new XStream(new DomDriver());
+//      
+//        System.out.println(x.toXML(simulatorConfiguration));
+        
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        FileOutputStream out = new FileOutputStream(new File(
+                "test.json"));
+        for (int i = 0; i < 1; i++) {
+            simulatorConfiguration.getSimulatorIdentification().setSoftwareDeveloper(String.valueOf(i));
+            mapper.writeValue(out, simulatorConfiguration);
+        }
+        out.close();
 
-		FileInputStream fis = new FileInputStream(new File("test.json"));
-		for (Iterator it = new ObjectMapper().readValues(
-				new JsonFactory().createJsonParser(fis),
-				SimulatorConfiguration.class); it.hasNext();)
+//        FileInputStream fis = new FileInputStream(new File("test.json"));
+//        for (Iterator it = new ObjectMapper().readValues(
+//                new JsonFactory().createJsonParser(fis),
+//                SimulatorConfiguration.class); it.hasNext();) {
+//            System.out.println(((SimulatorConfiguration) it.next()).getSimulatorIdentification().getSoftwareDeveloper());
+//        }
+//        fis.close();
 
-			System.out.println(((SimulatorConfiguration) it.next())
-					.getSimulatorIdentification().getSoftwareDeveloper());
-		fis.close();
+        // //String runId = port.runSimulation(simulatorConfiguration);
+        // System.out.println("Simulator returned runId: " + runId );
+        // // String runId = "Pitt,PSC,CMU_FRED_2.0.1_231162";
+        // // RunStatus rs = port.getRunStatus(runId, sr);
+        // while (rs.getStatus() != RunStatusEnum.COMPLETED) {
+        // System.out.println("Status is " + rs.getStatus());
+        // System.out.println("Message is " + rs.getMessage());
+        // System.out.println("\n");
+        // Thread.sleep(500);
+        // rs = port.getRunStatus(runId, sr);
+        // }
+        // System.out.println("Status is " + rs.getStatus());
+        // System.out.println("Message is " + rs.getMessage());
 
-		// //String runId = port.runSimulation(simulatorConfiguration);
-		// System.out.println("Simulator returned runId: " + runId );
-		// // String runId = "Pitt,PSC,CMU_FRED_2.0.1_231162";
-		// // RunStatus rs = port.getRunStatus(runId, sr);
-		// while (rs.getStatus() != RunStatusEnum.COMPLETED) {
-		// System.out.println("Status is " + rs.getStatus());
-		// System.out.println("Message is " + rs.getMessage());
-		// System.out.println("\n");
-		// Thread.sleep(500);
-		// rs = port.getRunStatus(runId, sr);
-		// }
-		// System.out.println("Status is " + rs.getStatus());
-		// System.out.println("Message is " + rs.getMessage());
-
-	}
+    }
 }
