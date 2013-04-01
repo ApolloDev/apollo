67992d59180f74ccfae46c3c282554f5fa166440
nick.millett@gmail.com <nick.millett@gmail.com@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2013-04-01 22:13:22 +0000

    removing chromephp references for now

--- apollo-ws/end-user-applications/simple-web-app/trunk/exec_visualization.php
+++ apollo-ws/end-user-applications/simple-web-app/trunk/exec_visualization.php
@@ -7,7 +7,7 @@
 
 define('AROOT', getcwd());
 require_once AROOT . '/apollo/apollo.inc';
-require_once AROOT . '/ChromePhp/ChromePhp.php';
+//require_once AROOT . '/ChromePhp/ChromePhp.php';
 $ret = new Response();
 
 $apollo = new apollo();
@@ -55,7 +55,7 @@ $UrlOutputResourceList = null;
 
 
 //if ($vizName != 'GAIA') {
-ChromePhp::log($apolloResponse);
+//ChromePhp::log($apolloResponse);
     $UrlOutputResourceList = $apolloResponse->visualizerResult->visualizerOutputResource;
     $diseaseStatesURL = '';
     $incidenceURL = '';
