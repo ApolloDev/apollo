ebbdb6fd4830e7f528a373f2f221f2da5246b48c
shawntbrown@gmail.com <shawntbrown@gmail.com@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2013-04-01 15:30:34 +0000

    Updated the GAIA service to use the new schema and fixed a few bugs in the database calling procedures.

--- apollo-ws/visualizer-services/GAIA/trunk/apollo.py
+++ apollo-ws/visualizer-services/GAIA/trunk/apollo.py
@@ -11,7 +11,7 @@ _timeSeriesTable = "time_series"
 class ApolloSimulatorOutput:
     def __init__(self,runID_):
 	self.runID = runID_
-
+	print "RunID = " + str(self.runID)
 	### here are the structures used to define the total output of an Apollo data unit
 
 	### Initiate the database
@@ -21,7 +21,7 @@ class ApolloSimulatorOutput:
 	### Get the ID from the database for this runID
 	SQLStatement = 'SELECT * from '+_runTable+' WHERE label = "' + self.runID + '"'
 	self._dbRunIndex = self.apolloDB.query(SQLStatement)[0]['id']
-	#print str(self.dbRunIndex)
+	print str(self._dbRunIndex)
 
     def printSimulatedPopulations(self):
 	print "Simulated Populations for run: " + str(self.runID)
@@ -33,47 +33,21 @@ class ApolloSimulatorOutput:
 	fipsTranslate = {}
 	### get the populationAxis value for "location"
 	LocationAxisID = self.apolloDB._populationAxis()['location']
-
 	### Get distince populations
-	SQLStatement = 'select population_id,value from simulated_population_axis_value '\
-		      +'where value <> "newly exposed" and population_id in '\
-		      +'(select DISTINCT population_id from time_series where run_id = "'\
-		      + str(self._dbRunIndex) + '" and population_id in '\
-		      +'(select id from (select p.id from simulated_population p, '\
-		      +'simulated_population_axis_value v, population_axis a '\
-		      +' where p.id = v.population_id and v.value like "newly exposed" '\
-		      +'and v.axis_id = a.id and a.label like "disease_state" and '\
-		      +'p.label like "% '+location+'%") as x) ) order by population_id'
+	SQLStatement = 'SELECT population_id,value from simulated_population_axis_value '\
+		 	' where value like "%'+str(location) + '%"'
 	popResults = self.apolloDB.query(SQLStatement)
-	#print "Pop = " + str(len(popResults))
 	for row in popResults:
 	    fipsTranslate[row['population_id']] = row['value']
-	SQLStatement = 'select  v2.value as location, t.time_step, t.pop_count'\
-		       +'from simulated_population p, simulated_population_axis_value v1,'\
-		       +'simulated_population_axis_value v2, population_axis a1, '\
-		       'population_axis a2, time_series twhere p.id = v1.population_id and'\
-		       +' t.population_id = p.id and'\
-		       +'v1.value like "newly exposed" and '\
-		       +'v1.axis_id = a1.id and '\
-		       +'a1.label like "disease_state" and '\
-		       +'v2.axis_id = a2.id and '\
-		       +'a2.label like "location " and'\
-		       +'v1.population_id = v2.population_id and '\
-		       +'v2.value like "'+location+'%" and '\
-		       +'t.run_id = "' + str(self._dbRunIndex) + '"'\
-		       +'order by v2.value, t.population_id, t.time_step'
-
 	### Get all of the Results for this time series
-	#SQLStatement = 'select * from time_series where run_id = "'+str(self._dbRunIndex)+'" '\
-	#	      +'and population_id in (select id from (select p.id '\
-	#	      +'from simulated_population p, simulated_population_axis_value v, '\
-	#	      +'population_axis a where p.id = v.population_id and '\
-	#	      +'v.value like "newly exposed" and v.axis_id = a.id and '\
-	#	      +'a.label like "disease_state" and p.label like "% '+location+'%") as x)'\
-	#	      +' order by population_id, time_step'
-	#print SQLStatement
+	SQLStatement = 'select * from time_series where run_id = "'+str(self._dbRunIndex)+'" '\
+		      +'and population_id in (select id from (select p.id '\
+		      +'from simulated_population p, simulated_population_axis_value v, '\
+		      +'population_axis a where p.id = v.population_id and '\
+		      +'v.value like "newly exposed" and v.axis_id = a.id and '\
+		      +'a.label like "disease_state" and p.label like "%'+location+'%") as x)'\
+		      +' order by population_id, time_step'
 	tsResults = self.apolloDB.query(SQLStatement)
-	#print "TS = " + str(len(tsResults))
 	for row in tsResults:
 	    ## Get the fips code for this population
 	    fipsCode = fipsTranslate[row['population_id']]
