d190a824349f06b795f54be18acf8c69b8d72746
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2013-04-30 17:48:43 +0000

    updated flute results URL in flute_results.php

--- apollo-ws/end-user-applications/simple-web-app/trunk/flute_results.php
+++ apollo-ws/end-user-applications/simple-web-app/trunk/flute_results.php
@@ -6,7 +6,7 @@
  */
 $md5RunId = urldecode($_GET ['md5RunId']);
 
-$url = 'http://130.49.206.36:8080/apollo/' . $md5RunId . '/flute-results.txt';
+$url = 'http://research.rods.pitt.edu/flute-results/' . $md5RunId . '/flute-results.txt';
 
 $text = file_get_contents($url);
 $text = nl2br($text);
