bc89246f4fc65b17ce0ef13db6f8cc1cc7adf23d
nickmillett <nem41@pitt.edu>
2013-06-11 20:07:18 +0000

    Making a branch for the new population map

---
+++ apollo-ws/end-user-applications/simple-web-app/branches/population-map/flute_results.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$md5RunId = urldecode($_GET ['md5RunId']);
+
+$url = 'http://research.rods.pitt.edu/flute-results/' . $md5RunId . '/flute-results.txt';
+
+$text = file_get_contents($url);
+$text = nl2br($text);
+
+print $text;
+?>
