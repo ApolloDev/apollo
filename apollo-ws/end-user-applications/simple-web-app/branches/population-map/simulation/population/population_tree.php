236ab1c0ef581e086da3a5da3874a6c28192e2ab
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2013-06-18 17:58:01 +0000

    changed the simulated population section of the configuration panel to open the new tab adjusted how the population list and map are displayed added highcharts.js since it is needed to display control measure schedules

--- apollo-ws/end-user-applications/simple-web-app/branches/population-map/simulation/population/population_tree.php
+++ apollo-ws/end-user-applications/simple-web-app/branches/population-map/simulation/population/population_tree.php
@@ -1,12 +1,26 @@
 <html><head><title>Population Initialization</title>
 
+        <script type="text/javascript">
+            function toggleInstructions(id) {
+                var e = $("#" + id);
+                var text = $("#show-hide-instruction-text");
+                if(e.css("display") == 'block') {
+                    e.css("display", "none");
+                    text.text("Show instructions (+)");
+                }
+                else {
+                    e.css("display", "block");
+                    text.text("Hide instructions (-)");
+                }
+            }
+        </script>
     </head>
     <body>
 
-        <table cellpadding="12">
+        <table cellpadding="12" style="width: 100%;">
             <tr>
-                <td style="width: 500px; background-color: #E0F0FF; border: 1px solid #B8B8B8; text-align: left">
-                    <div style="overflow:auto;height:700px;">
+                <td style="width: 350px; background-color: #E0F0FF; border: 1px solid #B8B8B8; text-align: left; vertical-align: top">
+                    <div style="overflow:auto; height: 600px">
 
                         <button id="store-fractions" onclick="storeSelectedRegionFractions();" style="width: 250px; height: 40px; font-size: 12px; border: 1px solid #99cccc">
                             <span class="ui-button-text">Store fractions for selected regions</span>
@@ -19,20 +33,28 @@
                 </td>
                 <td style="vertical-align: top">
                     <h2>Population Initialization</h2>
-                    Use the tree on the left to specify how the simulated population will be initialized by selecting locations and 
-                    initial disease fractions. 
-                    The population will be initialized from the tree according to the following rules:
-                    <ul>
-                        <li>The top-most region selected will be the entire region of the epidemic model</li>
-                        <li>Any selected sub-region will be used as an exception to selected regions containing it</li>
-                    </ul>
-                    As an example, say "United States" is selected with "Fraction infected: 0.1", "Alabama" is selected with "Fraction infected: 0.05", 
-                    and "Baldwin County" is selected with "Fraction infected: 0.2". This means that the epidemic model is for the entire
-                    United States, and everywhere except the state of Alabama will have an initial infected fraction of 0.1. In Alabama,
-                    everywhere except Baldwin county will have an initial infected fraction of 0.05. In Baldwin county, the initial infected
-                    fraction will be 0.2.
-                    <div id="map"></div>
+                    <a onclick="toggleInstructions('population-instructions')" id="show-hide-instruction-text" style="cursor: hand; cursor: pointer">Show instructions (+)</a>
+                    <br/>
+                    <div style="overflow: auto; height: 500px">
+                        <div id="population-instructions" style="display: none">
+                            <br/>
+                            Use the tree on the left to specify how the simulated population will be initialized by selecting locations and 
+                            initial disease fractions. 
+                            The population will be initialized from the tree according to the following rules:
+                            <ul>
+                                <li>The top-most region selected will be the entire region of the epidemic model</li>
+                                <li>Any selected sub-region will be used as an exception to selected regions containing it</li>
+                            </ul>
+                            As an example, say "United States" is selected with "Fraction infected: 0.1", "Alabama" is selected with "Fraction infected: 0.05", 
+                            and "Baldwin County" is selected with "Fraction infected: 0.2". This means that the epidemic model is for the entire
+                            United States, and everywhere except the state of Alabama will have an initial infected fraction of 0.1. In Alabama,
+                            everywhere except Baldwin county will have an initial infected fraction of 0.05. In Baldwin county, the initial infected
+                            fraction will be 0.2.
+                        </div>
+                        <br/>
 
+                        <div id="map"></div>
+                    </div>
                 </td>
 
 
