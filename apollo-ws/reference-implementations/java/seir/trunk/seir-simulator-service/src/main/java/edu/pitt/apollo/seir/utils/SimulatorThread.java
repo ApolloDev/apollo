3247c7e550341c4b78c9d15144818b3878bdce94
nickmillett <nem41@pitt.edu>
2013-07-01 18:30:03 +0000

    miscellaneous improvements to the batch thread and simulator thread handling

--- apollo-ws/reference-implementations/java/seir/trunk/seir-simulator-service/src/main/java/edu/pitt/apollo/seir/utils/SimulatorThread.java
+++ apollo-ws/reference-implementations/java/seir/trunk/seir-simulator-service/src/main/java/edu/pitt/apollo/seir/utils/SimulatorThread.java
@@ -17,109 +17,105 @@ import edu.pitt.rods.apollo.SeirModelAdapter.SeirModelAdapter;
  */
 public class SimulatorThread extends Thread {
 
-	SimulatorConfiguration sc;
-	String runId;
-	String simConfigHash;
-	String simConfigJson;
-	boolean runSimulator = true;
-	boolean useFile;
-	boolean useDatabase;
+    SimulatorConfiguration sc;
+    String runId;
+    String simConfigHash;
+    String simConfigJson;
+    boolean runSimulator = true;
+    boolean useFile;
+    boolean useDatabase;
 
-	public SimulatorThread(SimulatorConfiguration sc, String simConfigHash,
-			String runId, String simConfigJson, boolean runSimulator,
-			boolean useFile, boolean useDatabase) {
-		super();
-		this.sc = sc;
-		this.runId = runId;
-		this.simConfigHash = simConfigHash;
-		this.runSimulator = runSimulator;
-		this.simConfigJson = simConfigJson;
-		this.useFile = useFile;
-		this.useDatabase = useDatabase;
-	}
+    public SimulatorThread(SimulatorConfiguration sc, String simConfigHash,
+            String runId, String simConfigJson, boolean runSimulator,
+            boolean useFile, boolean useDatabase) {
+        super();
+        this.sc = sc;
+        this.runId = runId;
+        this.simConfigHash = simConfigHash;
+        this.runSimulator = runSimulator;
+        this.simConfigJson = simConfigJson;
+        this.useFile = useFile;
+        this.useDatabase = useDatabase;
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		String runIdHash = RunUtils.getMd5HashFromBytes(runId.getBytes());
-		try {
+        String runIdHash = RunUtils.getMd5HashFromBytes(runId.getBytes());
+        try {
 
-			String directory = RunUtils.setStarted(runIdHash);
-			if (runSimulator) {
+            String directory = RunUtils.setStarted(runIdHash);
+            SeirSimulatorServiceImpl.removeRunFromQueuedList(runIdHash);
+            if (runSimulator) {
 
-				SeirModelAdapter.runSeirModel(sc, simConfigHash, runId,
-						simConfigJson, directory, useFile, useDatabase);
-			}
-			RunUtils.setFinished(runIdHash);
-			SeirSimulatorServiceImpl.removeRunFromQueuedList(runIdHash);
+                SeirModelAdapter.runSeirModel(sc, simConfigHash, runId,
+                        simConfigJson, directory, useFile, useDatabase);
+            }
+            RunUtils.setFinished(runIdHash);
+        } catch (NullPointerException e) {
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                PrintStream ps = new PrintStream(baos);
+                e.printStackTrace(ps);
+                String stackTrace = baos.toString("ISO-8859-1");
 
-			SeirSimulatorServiceImpl.simulatorRunFinished();
-			SeirSimulatorServiceImpl.runSimulatorThreads();
-		} catch (NullPointerException e) {
-			try {
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				PrintStream ps = new PrintStream(baos);
-				e.printStackTrace(ps);
-				String stackTrace = baos.toString("ISO-8859-1");
+                String rawUnformattedJSONString = SeirSimulatorServiceImpl.getJSONString(sc);
+                String formattedJSONString = SeirSimulatorServiceImpl.formatJSONString(rawUnformattedJSONString);
 
-				String rawUnformattedJSONString = SeirSimulatorServiceImpl
-						.getJSONString(sc);
-				String formattedJSONString = SeirSimulatorServiceImpl
-						.formatJSONString(rawUnformattedJSONString);
+                RunUtils.setError(
+                        runIdHash,
+                        "Enountered unexpected null value.  If one is attached, please send the following "
+                        + "stack trace to the developer of the simulator:\n"
+                        + stackTrace
+                        + "\nConfiguration object was: "
+                        + formattedJSONString);
 
-				RunUtils.setError(
-						runIdHash,
-						"Enountered unexpected null value.  If one is attached, please send the following "
-								+ "stack trace to the developer of the simulator:\n"
-								+ stackTrace
-								+ "\nConfiguration object was: "
-								+ formattedJSONString);
+                e.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException e) {
+            try {
+                RunUtils.setError(runIdHash,
+                        "Error creating run status file.  Specific error was:\n"
+                        + e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (SQLException e) {
+            try {
+                RunUtils.setError(runIdHash,
+                        "Database error during simulator execution.  Specific message was:\n"
+                        + e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            try {
+                RunUtils.setError(runIdHash,
+                        "Database driver not found on server.  Specific message was:\n"
+                        + e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        } catch (InvalidSimulatorConfigurationException e) {
+            try {
 
-				e.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		} catch (IOException e) {
-			try {
-				RunUtils.setError(runIdHash,
-						"Error creating run status file.  Specific error was:\n"
-								+ e.getMessage());
-				e.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		} catch (SQLException e) {
-			try {
-				RunUtils.setError(runIdHash,
-						"Database error during simulator execution.  Specific message was:\n"
-								+ e.getMessage());
-				e.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		} catch (ClassNotFoundException e) {
-			try {
-				RunUtils.setError(runIdHash,
-						"Database driver not found on server.  Specific message was:\n"
-								+ e.getMessage());
-				e.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		} catch (InvalidSimulatorConfigurationException e) {
-			try {
+                String rawUnformattedJSONString = SeirSimulatorServiceImpl.getJSONString(sc);
+                String formattedJSONString = SeirSimulatorServiceImpl.formatJSONString(rawUnformattedJSONString);
+                RunUtils.setError(runIdHash, e.getMessage()
+                        + "\nConfiguration object was: " + formattedJSONString);
+                e.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
 
-				String rawUnformattedJSONString = SeirSimulatorServiceImpl
-						.getJSONString(sc);
-				String formattedJSONString = SeirSimulatorServiceImpl
-						.formatJSONString(rawUnformattedJSONString);
-				RunUtils.setError(runIdHash, e.getMessage()
-						+ "\nConfiguration object was: " + formattedJSONString);
-				e.printStackTrace();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		}
-	}
-
+        } finally {
+            SeirSimulatorServiceImpl.simulatorRunFinished();
+            SeirSimulatorServiceImpl.runSimulatorThreads();
+        }
+    }
 }
