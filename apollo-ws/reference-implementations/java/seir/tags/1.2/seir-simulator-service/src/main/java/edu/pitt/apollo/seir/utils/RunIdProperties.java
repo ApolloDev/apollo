e03b77ba9ff9f4cbe54d86d85c4149e71137b9e4
JohnLevander <JohnLevander@users.noreply.github.com>
2013-07-01 18:53:06 +0000

    Tagging for version 1.2

---
+++ apollo-ws/reference-implementations/java/seir/tags/1.2/seir-simulator-service/src/main/java/edu/pitt/apollo/seir/utils/RunIdProperties.java
@@ -0,0 +1,32 @@
+package edu.pitt.apollo.seir.utils;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Mar 21, 2013
+ * Time: 1:45:49 PM
+ * Class: RunIdProperties
+ * IDE: NetBeans 6.9.1
+ */
+public class RunIdProperties {
+
+    private String runId = null;
+    private boolean runIdInDatabase = false;
+
+    public void setRunId(String runId) {
+        this.runId = runId;
+    }
+
+    public void setRunIdInDatabase(boolean runIdInDatabase) {
+        this.runIdInDatabase = runIdInDatabase;
+    }
+
+    public String getRunId() {
+        return runId;
+    }
+
+    public boolean isRunIdInDatabase() {
+        return runIdInDatabase;
+    }
+}
