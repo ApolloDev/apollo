d475d43b83fe1e3b5fad281e6943e946f72961f6
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2013-07-02 14:01:16 +0000

    version 1.2 of the SEUA this uses version 1.2 of the schema

---
+++ apollo-ws/end-user-applications/simple-web-app/tags/1.2/simulation/apollo_exec.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+
+  Copyright (C) 2011 University of Pittsburgh
+
+  This file is part of Apollo.
+
+  Apollo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of
+  the License, or (at your option) any later version.
+
+  Apollo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Apollo.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+/**
+ * Apollo Web Service Consumer
+ *
+ * @author Yang Hu <yah14@pitt.edu>
+ */
+require_once __DIR__ . '/../util/apollo.inc';
+require_once __DIR__ . '/../util/misc.inc';
+//require_once __DIR__ . '/../chromephp/ChromePhp.php';
+
+$ret = new Response();
+
+try {
+    //snomed code
+//    $snomed = $_POST ['SNOMED'];
+    //model type
+//    $modelType = $_POST ['ModelType'];
+    //jurisdiction
+//    $jurisdiction = $_POST['Jurisdiction'];
+
+    $simulatorDevArray = explode(",", $_POST['simulatorDeveloper']);
+    $simulatorNameArray = explode(",", $_POST['simulatorName']);
+    $simulatorVerArray = explode(",", $_POST['simulatorVersion']);
+
+    //model parameters
+    $rawParams = json_decode($_POST ['Parameters']);
+
+    // Define the jason errors.
+//	$json_errors = array(
+//	    JSON_ERROR_NONE => 'No error has occurred',
+//	    JSON_ERROR_DEPTH => 'The maximum stack depth has been exceeded',
+//	    JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+//	    JSON_ERROR_SYNTAX => 'Syntax error',
+//	);
+//	if (json_last_error() != JSON_ERROR_NONE)
+//		throw new Exception('Json parse error : '. $json_errors[json_last_error()]. PHP_EOL);
+    //cache the model parameters into map
+    $params = array();
+    foreach ($rawParams as $rawParam) {
+        if ($rawParam->isLeaf == false)
+            continue;
+        //extra saves the complex data in json string
+        if (strlen($rawParam->extra) == 0) {
+            //if its a single value
+            $params[deleteSpace($rawParam->pname)] = $rawParam->value;
+        } else {//if its a complex object
+            $params[deleteSpace($rawParam->pname)] = json_decode($rawParam->extra);
+        }
+
+//        ChromePhp::log(deleteSpace($rawParam->pname));
+    }
+
+//    $params['VaccineEfficacy']);
+//    ChromePhp::log($params['VaccineControlMeasureCompliance']);
+
+    $apollo = new apollo();
+
+    $runIds = array();
+    $failedSimulators = array();
+    // execute in loop
+    for ($i = 0; $i < count($simulatorDevArray); $i++) {
+        $simulatorDev = $simulatorDevArray[$i];
+        $simulatorName = $simulatorNameArray[$i];
+        $simulatorVer = $simulatorVerArray[$i];
+
+        try {
+            $runIds[] = $apollo->exec($params, $simulatorDev, $simulatorName, $simulatorVer);
+        } catch (Exception $e) {
+            $failedSimulators[$simulatorName] = $e->getMessage();
+        }
+    }
+
+    $result['runIds'] = $runIds;
+    $result['failedSimulators'] = $failedSimulators;
+    $ret->data = $result;
+} catch (Exception $e) {
+    $ret->exception = $e->getMessage();
+}
+
+//ChromePhp::log($ret);
+
+echo json_encode($ret);
+?>
