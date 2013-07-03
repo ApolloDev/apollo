9bc09acf6a3e562bc1dc0347dcdc7573f1273899
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2013-07-03 21:36:25 +0000

    Initial shot at updating to version 1.3.  It compiles, and I will fix the functionality later.

--- apollo-ws/service-skeletons/java/trunk/syntheticpopulation-service/src/main/java/edu/pitt/apollo/SyntheticPopulationServiceImpl.java
+++ apollo-ws/service-skeletons/java/trunk/syntheticpopulation-service/src/main/java/edu/pitt/apollo/SyntheticPopulationServiceImpl.java
@@ -24,10 +24,11 @@ import javax.jws.WebService;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
 
-import edu.pitt.apollo.service.syntheticpopulationservice.SyntheticPopulationServiceEI;
-import edu.pitt.apollo.types.RunStatus;
-import edu.pitt.apollo.types.ServiceResult;
-import edu.pitt.apollo.types.SyntheticPopulationConfiguration;
+import edu.pitt.apollo.service.syntheticpopulationservice._07._03._2013.SyntheticPopulationServiceEI;
+import edu.pitt.apollo.types._07._03._2013.RunStatus;
+import edu.pitt.apollo.types._07._03._2013.ServiceResult;
+import edu.pitt.apollo.types._07._03._2013.SyntheticPopulationConfiguration;
+
 
 @WebService(targetNamespace = "http://service.apollo.pitt.edu/syntheticpopulationservice/", 
 portName="SyntheticPopulationServiceEndpoint",
