d4038ff1ad1f2d0f78400f2631776769e3452c29
nickmillett <nem41@pitt.edu>
2013-08-22 15:50:47 +0000

    Tagging FluTE version that uses Apollo 1.2 objects

---
+++ apollo-ws/epidemic-simulator-services/FluTE/tags/1.2.0/flute-simulator-service/src/main/java/edu/pitt/apollo/FluteSimulatorServiceException.java
@@ -0,0 +1,17 @@
+package edu.pitt.apollo;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: May 6, 2013
+ * Time: 5:26:04 PM
+ * Class: FluteSimulatorServiceException
+ * IDE: NetBeans 6.9.1
+ */
+public class FluteSimulatorServiceException extends Exception {
+    
+    public FluteSimulatorServiceException(String message) {
+        super(message);
+    }
+}
