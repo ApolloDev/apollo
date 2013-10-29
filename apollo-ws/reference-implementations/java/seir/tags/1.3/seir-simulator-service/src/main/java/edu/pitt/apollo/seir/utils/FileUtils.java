6301e6ae9e90572920d1cb5292e3589f899387a7
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2013-10-29 19:36:29 +0000

    Tagging as 1.3

---
+++ apollo-ws/reference-implementations/java/seir/tags/1.3/seir-simulator-service/src/main/java/edu/pitt/apollo/seir/utils/FileUtils.java
@@ -0,0 +1,21 @@
+package edu.pitt.apollo.seir.utils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class FileUtils {
+
+	public static void downloadFile(URL sourcefile, String destFileName)
+			throws IOException {
+
+		ReadableByteChannel rbc = Channels.newChannel(sourcefile.openStream());
+		FileOutputStream fos = new FileOutputStream(destFileName);
+		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+		fos.flush();
+		fos.close();
+	}
+}
