5f324bc9803042387481dcd30ddd6203c54b3ebf
nickmillett <nem41@pitt.edu>
2013-11-07 17:07:55 +0000

    edited visualizer client for testing changed library service apollo work directory

--- apollo-ws/service-skeletons/java/trunk/visualizer-service-client/src/main/java/edu/pitt/apollo/visualizerclient/WSClient.java
+++ apollo-ws/service-skeletons/java/trunk/visualizer-service-client/src/main/java/edu/pitt/apollo/visualizerclient/WSClient.java
@@ -19,16 +19,61 @@ package edu.pitt.apollo.visualizerclient;
 
 import edu.pitt.apollo.service.visualizerservice._10._28._2013.VisualizerServiceEI;
 import edu.pitt.apollo.service.visualizerservice._10._28._2013.VisualizerServiceV131;
+import edu.pitt.apollo.types._10._28._2013.ApolloSoftwareType;
+import edu.pitt.apollo.types._10._28._2013.Authentication;
 import edu.pitt.apollo.types._10._28._2013.RunStatus;
+import edu.pitt.apollo.types._10._28._2013.RunStatusEnum;
+import edu.pitt.apollo.types._10._28._2013.SoftwareIdentification;
+import edu.pitt.apollo.types._10._28._2013.VisualizationOptions;
+import edu.pitt.apollo.types._10._28._2013.VisualizerConfiguration;
+import edu.pitt.apollo.types._10._28._2013.VisualizerResult;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WSClient {
-	public static void main(String[] args) {
-		VisualizerServiceV131 service = new VisualizerServiceV131();
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		VisualizerServiceV131 service = new VisualizerServiceV131(new URL("http://warhol-fred.psc.edu:8091/gaia?wsdl"));
 		VisualizerServiceEI port = service.getVisualizerServiceEndpoint();
 
-		RunStatus rs = port.getRunStatus("test");
-		System.out.println(rs.getMessage());
-
-		System.out.println("Status: " + rs.getStatus());
+                VisualizerConfiguration  vizConfig = new VisualizerConfiguration();
+//                vizConfig.
+                VisualizationOptions options = new VisualizationOptions();
+                options.setRunId("UPitt,PSC,CMU_FRED_2.0.1_259387");
+                options.setLocation("06037");
+                options.setOutputFormat("mp4");
+                
+                vizConfig.setVisualizationOptions(options);
+                
+                SoftwareIdentification sid = new SoftwareIdentification();
+                sid.setSoftwareName("GAIA");
+                sid.setSoftwareType(ApolloSoftwareType.VISUALIZER);
+                sid.setSoftwareVersion("1.0");
+                sid.setSoftwareDeveloper("PSC");
+                
+                vizConfig.setVisualizerIdentification(sid);
+                
+                Authentication auth = new Authentication();
+                auth.setRequesterId("fake_id");
+                auth.setRequesterPassword("fake_password");
+                
+                vizConfig.setAuthentication(auth);
+                
+                
+                
+                VisualizerResult result =  port.run(vizConfig);
+                
+                String runId = result.getRunId();
+                
+                RunStatus status = port.getRunStatus(runId);
+                
+                while (status.getStatus() != RunStatusEnum.COMPLETED) {
+                    System.out.println(status.getStatus() + "    " + status.getMessage());
+                    System.out.println(result.getVisualizerOutputResource().get(0).getURL());
+                    
+                    Thread.sleep(5000);
+                    status = port.getRunStatus(runId);
+                }
+                
+		System.out.println(status.getStatus());
 	}
 }
