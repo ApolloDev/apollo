79115179a126b6d3699ced6d232d88cb5f94af08
JohnLevander <JohnLevander@users.noreply.github.com>
2014-02-10 14:50:31 +0000

    Initial commit for the Eclipse tutorial

---
+++ apollo-ws/tutorial/eclipse/trunk/src/edu/pitt/apollo/service/apolloservice/v2_0/GetRunStatusResponse.java
@@ -0,0 +1,65 @@
+
+package edu.pitt.apollo.service.apolloservice.v2_0;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import edu.pitt.apollo.types.v2_0.MethodCallStatus;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="runStatus" type="{http://types.apollo.pitt.edu/v2_0/}MethodCallStatus"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "runStatus"
+})
+@XmlRootElement(name = "getRunStatusResponse")
+public class GetRunStatusResponse {
+
+    @XmlElement(required = true)
+    protected MethodCallStatus runStatus;
+
+    /**
+     * Gets the value of the runStatus property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link MethodCallStatus }
+     *     
+     */
+    public MethodCallStatus getRunStatus() {
+        return runStatus;
+    }
+
+    /**
+     * Sets the value of the runStatus property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link MethodCallStatus }
+     *     
+     */
+    public void setRunStatus(MethodCallStatus value) {
+        this.runStatus = value;
+    }
+
+}
