3cac78aa8c2338ba2e456b353e797bbe1f2da54e
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2014-02-12 15:53:30 +0000

    Initial checkin for tutorials

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-service-client/src/main/java/edu/pitt/apollo/apolloclient/UnconditionalProbabilityDistributionExample.java
@@ -0,0 +1,17 @@
+package edu.pitt.apollo.apolloclient;
+
+public class UnconditionalProbabilityDistributionExample {
+
+	public UnconditionalProbabilityDistributionExample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
