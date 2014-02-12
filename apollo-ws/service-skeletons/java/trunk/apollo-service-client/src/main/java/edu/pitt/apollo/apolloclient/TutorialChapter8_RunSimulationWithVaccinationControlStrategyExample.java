3f02f6730c42446158fef66f425d1e34412b02ff
john.d.levander@gmail.com <john.d.levander@gmail.com@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2014-02-12 17:16:18 +0000

    Updated the Tutorial code

--- apollo-ws/service-skeletons/java/trunk/apollo-service-client/src/main/java/edu/pitt/apollo/apolloclient/TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample.java
+++ apollo-ws/service-skeletons/java/trunk/apollo-service-client/src/main/java/edu/pitt/apollo/apolloclient/TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample.java
@@ -17,62 +17,61 @@ import edu.pitt.apollo.types.v2_0.VaccinationEfficacyForSimulatorConfiguration;
 import edu.pitt.apollo.types.v2_0.VaccinationPreventableOutcome;
 import edu.pitt.apollo.types.v2_0.Vaccine;
 
-public class RunSimulationWithVaccinationControlStrategyExample extends
-		BasicApolloBrokerClient {
+public class TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample extends
+		TutorialChapter2_BasicRunSimulationExample {
 
-	public RunSimulationWithVaccinationControlStrategyExample()
-			throws MalformedURLException {
+	public TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample() throws MalformedURLException {
 		super();
 	}
 
 	private Vaccination getVaccination() {
-		Vaccination vacc = new Vaccination();
-		vacc.setDescription("H1N1 Vaccine");
-		vacc.setNumDosesInTreatmentCourse(new BigInteger("1"));
-		vacc.setSpeciesOfTreatedOrganisms("9606");
-		vacc.getTreatmentContraindications();
+		Vaccination vaccination = new Vaccination();
+		vaccination.setDescription("H1N1 Vaccine");
+		vaccination.setNumDosesInTreatmentCourse(new BigInteger("1"));
+		vaccination.setSpeciesOfTreatedOrganisms("9606");
+		vaccination.getTreatmentContraindications();
 
 		Vaccine vaccine = new Vaccine();
 		vaccine.setDescription("Influenza A (H1N1) 2009 Monovalent Vaccine");
 		vaccine.setVaccineIdentifier("Influenza A (H1N1) 2009 Monovalent Vaccine");
 		vaccine.getValence().add(new BigInteger("1"));
-		vacc.setVaccine(vaccine);
+		vaccination.setVaccine(vaccine);
 
-		VaccinationEfficacyForSimulatorConfiguration vesc = new VaccinationEfficacyForSimulatorConfiguration();
-		
-		Treatment vacc2 = new Treatment();
-		vacc2.setDescription("H1N1 Vaccine");
-		vacc2.setNumDosesInTreatmentCourse(new BigInteger("1"));
-		vacc2.setSpeciesOfTreatedOrganisms("9606");
-		vacc2.getTreatmentContraindications();
+		VaccinationEfficacyForSimulatorConfiguration vaccinationEfficacy = new VaccinationEfficacyForSimulatorConfiguration();
+
+		// this Treatment is required but not read and will not be required in
+		// version 2.0.1
+		Treatment treatment = new Treatment();
+		treatment.setDescription("H1N1 Vaccine");
+		treatment.setNumDosesInTreatmentCourse(new BigInteger("1"));
+		treatment.setSpeciesOfTreatedOrganisms("9606");
+		treatment.getTreatmentContraindications();
 
 		ApolloPathogenCode strain = new ApolloPathogenCode();
 		strain.setNcbiTaxonId("114727");
-		vesc.setStrainIdentifier(strain);
-		vesc.setTreatment(vacc2);
-		vesc.setForVaccinationPreventableOutcome(VaccinationPreventableOutcome.INFECTION);
-		vesc.setHostIdentifier("9606"); //homo sapiens
-		vesc.setVaccineIdentifier("Influenza A (H1N1) 2009 Monovalent Vaccine");
-		vesc.setAverageVaccinationEfficacy(0.47);
-		vesc.setDescription("The vaccination efficacy for the Influenza A (H1N1) 2009 Monovalent Vaccine");
+		vaccinationEfficacy.setStrainIdentifier(strain);
+		vaccinationEfficacy.setTreatment(treatment);
+		vaccinationEfficacy.setForVaccinationPreventableOutcome(VaccinationPreventableOutcome.INFECTION);
+		vaccinationEfficacy.setHostIdentifier("9606"); // homo sapiens
+		vaccinationEfficacy.setVaccineIdentifier("Influenza A (H1N1) 2009 Monovalent Vaccine");
+		vaccinationEfficacy.setAverageVaccinationEfficacy(0.47);
+		vaccinationEfficacy
+				.setDescription("The vaccination efficacy for the Influenza A (H1N1) 2009 Monovalent Vaccine");
 
-		vacc.getVaccinationEfficacies().add(vesc);
+		vaccination.getVaccinationEfficacies().add(vaccinationEfficacy);
 
-		return vacc;
+		return vaccination;
 	}
 
 	private ProbabilisticParameterValue getControlStrategyCompilance() {
 		ProbabilisticParameterValue compliance = new ProbabilisticParameterValue();
 		compliance.setValue(0.5);
 		return compliance;
-
 	}
 
 	private ControlStrategyTargetPopulationsAndPrioritization getTargetPopulationsAndPrioritizations() {
-		ControlStrategyTargetPopulationsAndPrioritization targetPopulationsAndPrioritization = 
-				new ControlStrategyTargetPopulationsAndPrioritization();
-		targetPopulationsAndPrioritization
-				.setControlStrategyNamedPrioritizationScheme("ACIP");
+		ControlStrategyTargetPopulationsAndPrioritization targetPopulationsAndPrioritization = new ControlStrategyTargetPopulationsAndPrioritization();
+		targetPopulationsAndPrioritization.setControlStrategyNamedPrioritizationScheme("ACIP");
 		return targetPopulationsAndPrioritization;
 	}
 
@@ -107,23 +106,17 @@ public class RunSimulationWithVaccinationControlStrategyExample extends
 
 		return vaccinationControlStrategy;
 	}
-	
+
 	@Override
 	public RunSimulationMessage getRunSimulationMessage() {
 		RunSimulationMessage message = super.getRunSimulationMessage();
-		message.getInfectiousDiseaseScenario()
-				.getInfectiousDiseaseControlStrategies()
+		message.getInfectiousDiseaseScenario().getInfectiousDiseaseControlStrategies()
 				.add(getVaccinationControlStrategy());
 		return message;
 	};
 
-	/**
-	 * @param args
-	 * @throws MalformedURLException 
-	 */
 	public static void main(String[] args) throws MalformedURLException {
-		new RunSimulationWithVaccinationControlStrategyExample()
-				.runSimulationAndDisplayResults();
+		new TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample().runSimulationAndDisplayResults();
 
 	}
 
