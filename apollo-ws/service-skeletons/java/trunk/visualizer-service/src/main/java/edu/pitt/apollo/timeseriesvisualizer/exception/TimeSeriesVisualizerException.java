c56f79b2e2adadc7b18423cc8548fc63653b8e90
nickmillett <nem41@pitt.edu>
2014-03-17 19:19:36 +0000

    re-factored visualizer to use a single query

---
+++ apollo-ws/service-skeletons/java/trunk/visualizer-service/src/main/java/edu/pitt/apollo/timeseriesvisualizer/exception/TimeSeriesVisualizerException.java
@@ -0,0 +1,18 @@
+package edu.pitt.apollo.timeseriesvisualizer.exception;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Feb 27, 2014
+ * Time: 3:01:47 PM
+ * Class: TimeSeriesVisualizerException
+ * IDE: NetBeans 6.9.1
+ */
+public class TimeSeriesVisualizerException extends Exception {
+    
+    public TimeSeriesVisualizerException(String s) {
+        super(s);
+    }
+    
+}
---
+++ apollo-ws/service-skeletons/java/trunk/visualizer-service/src/main/java/edu/pitt/apollo/timeseriesvisualizer/types/ImageSeriesMap.java
@@ -0,0 +1,20 @@
+package edu.pitt.apollo.timeseriesvisualizer.types;
+
+import java.util.HashMap;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Feb 12, 2013
+ * Time: 7:14:53 PM
+ * Class: ImageSeriesMap
+ * IDE: NetBeans 6.9.1
+ */
+public class ImageSeriesMap extends HashMap<String, TimeSeriesContainer> {
+    
+    public ImageSeriesMap() {
+        super();
+    }
+    
+}
