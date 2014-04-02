88a4720ace26d7b1eac5e0e3919f57d9aeaa0968
john.d.levander@gmail.com <jdl50@pitt.edu>
2014-04-02 20:22:18 +0000

    Initial check-in

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-db-common/src/main/java/edu/pitt/apollo/db/ApolloDatabaseKeyNotFoundException.java
@@ -0,0 +1,34 @@
+package edu.pitt.apollo.db;
+
+import edu.pitt.apollo.db.ApolloDatabaseException;
+
+public class ApolloDatabaseKeyNotFoundException extends ApolloDatabaseException {
+
+	public ApolloDatabaseKeyNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseKeyNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseKeyNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseKeyNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
