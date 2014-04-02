19307d8e04d974ef722f43ea59b1ba458837b842
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2014-04-02 20:22:18 +0000

    Initial check-in

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-db-common/src/main/java/edu/pitt/apollo/db/ApolloDatabaseException.java
@@ -0,0 +1,32 @@
+package edu.pitt.apollo.db;
+
+public class ApolloDatabaseException extends Exception {
+
+	public ApolloDatabaseException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
