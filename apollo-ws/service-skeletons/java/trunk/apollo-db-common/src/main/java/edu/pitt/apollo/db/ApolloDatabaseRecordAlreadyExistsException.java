e14ff5c469aeca62bbf6c193a4a94d83a9abf185
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2014-04-02 20:22:18 +0000

    Initial check-in

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-db-common/src/main/java/edu/pitt/apollo/db/ApolloDatabaseRecordAlreadyExistsException.java
@@ -0,0 +1,32 @@
+package edu.pitt.apollo.db;
+
+public class ApolloDatabaseRecordAlreadyExistsException extends Exception {
+
+	public ApolloDatabaseRecordAlreadyExistsException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseRecordAlreadyExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseRecordAlreadyExistsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseRecordAlreadyExistsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
