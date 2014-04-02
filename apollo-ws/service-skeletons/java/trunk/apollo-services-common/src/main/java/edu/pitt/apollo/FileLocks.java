d5fde1cd519ac157c24d912c129185fef4efc0fc
JohnLevander <jdl50@pitt.edu>
2014-04-02 20:22:35 +0000

    initial check-in

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-services-common/src/main/java/edu/pitt/apollo/FileLocks.java
@@ -0,0 +1,37 @@
+package edu.pitt.apollo;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class FileLocks {
+
+	public enum FileLockAction {
+		LOCK, UNLOCK,
+
+	}
+
+	private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+	Lock write = readWriteLock.writeLock();
+
+	public FileLocks() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void lock() {
+		write.lock();
+	}
+
+	public void unlock() {
+		write.unlock();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
