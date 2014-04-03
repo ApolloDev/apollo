e6708755d6e3c8d4479c3e2abeb9fd06af060c7b
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2014-04-03 04:08:01 +0000

    Removed obsolete functions, completed some stubbed out functions, and added some new functions.

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-db-common/src/main/java/edu/pitt/apollo/db/ApolloDatabaseRecordNotInsertedException.java
@@ -0,0 +1,34 @@
+package edu.pitt.apollo.db;
+
+import edu.pitt.apollo.db.ApolloDatabaseException;
+
+public class ApolloDatabaseRecordNotInsertedException extends ApolloDatabaseException {
+
+	public ApolloDatabaseRecordNotInsertedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseRecordNotInsertedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseRecordNotInsertedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApolloDatabaseRecordNotInsertedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- apollo-ws/service-skeletons/java/trunk/apollo-db-common/src/test/java/edu/pitt/apollo/db/ApolloDbUtilsTest.java
+++ apollo-ws/service-skeletons/java/trunk/apollo-db-common/src/test/java/edu/pitt/apollo/db/ApolloDbUtilsTest.java
@@ -1,8 +1,11 @@
 package edu.pitt.apollo.db;
 
+import java.io.File;
 import java.sql.SQLException;
+import java.util.Map;
 
 import junit.framework.TestCase;
+import edu.pitt.apollo.TutorialChapter2_BasicRunSimulationExample;
 import edu.pitt.apollo.types.v2_0_1.ApolloSoftwareTypeEnum;
 import edu.pitt.apollo.types.v2_0_1.Authentication;
 import edu.pitt.apollo.types.v2_0_1.SoftwareIdentification;
@@ -14,7 +17,15 @@ public class ApolloDbUtilsTest extends TestCase {
 	private SoftwareIdentification gaiaSoftwareId;
 	private Authentication authUser1;
 	private Authentication authUser2;
+	
+	TutorialChapter2_BasicRunSimulationExample tutorial;
+	
+	private ApolloDbUtils apolloDbUtils;
 
+	private static String APOLLO_DIR = "";
+	private static final String DATABASE_PROPERTIES_FILE = "database.properties";
+	public static final String APOLLO_WORKDIR_ENVIRONMENT_VARIABLE = "APOLLO_201_WORK_DIR";
+	
 	public ApolloDbUtilsTest() {
 		// TODO Auto-generated constructor stub
 	}
@@ -26,150 +37,186 @@ public class ApolloDbUtilsTest extends TestCase {
 	// 3. Create a run record
 	// 4. Create data_content
 	// 5. Associate data content with run
-//	@Override
-//	protected void setUp() throws Exception {
-//		// TODO Auto-generated method stub
-//		super.setUp();
-//		TutorialChapter2_BasicRunSimulationExample tutorial = new TutorialChapter2_BasicRunSimulationExample();
-//
-//		authUser1 = new Authentication();
-//		authUser1.setRequesterId("user1");
-//		authUser1.setRequesterPassword("pass1");
-//		authUser2 = new Authentication();
-//		authUser2.setRequesterId("user2");
-//		authUser2.setRequesterPassword("pass2");
-//		//insertUsers();
-//		
-////		fredSoftwareId = tutorial.getSoftwareIdentificationForSimulator();
-////		gaiaSoftwareId = tutorial.getSoftwareIdentifiationForGaia();
-////		insertSoftwareIds();
-//
-//	
-//
-//	}
-//
-//	@Override
-//	protected void tearDown() throws Exception {
-//		// TODO Auto-generated method stub
-//		super.tearDown();
-//		tutorial = null;
-//	}
-//
-//	private void insertUsers() throws ApolloDatabaseException {
-//
-//		int userKey;
-//		try {
-//			userKey = ApolloDbUtils.addUser(authUser1.getRequesterId(), authUser1.getRequesterPassword(), "testemail@email.com");
-//			assertEquals(1, userKey);
-//
-//			userKey = ApolloDbUtils.addUser(authUser1.getRequesterId(), authUser1.getRequesterPassword(), "testemail@email.com");
-//			assertEquals(ApolloDbUtils.RECORD_ALREADY_EXISTS, userKey);
-//			
-//			boolean canRun = true;
-//			boolean canViewCache = true;
-//			ApolloDbUtils.addRole(tutorial.getSoftwareIdentificationForSimulator(), canRun, canViewCache);
-//
-//			userKey = ApolloDbUtils.getUserKey(authUser1.getRequesterId(), authUser1.getRequesterPassword());
-//			assertEquals(1, userKey);
-//
-//			userKey = ApolloDbUtils.getUserKey(authUser2.getRequesterId(), authUser2.getRequesterPassword());
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		Map<String, String> env = System.getenv();
+		APOLLO_DIR = env.get(APOLLO_WORKDIR_ENVIRONMENT_VARIABLE);
+		apolloDbUtils = new ApolloDbUtils(new File(APOLLO_DIR + "/" + DATABASE_PROPERTIES_FILE));
+		
+		authUser1 = new Authentication();
+		authUser1.setRequesterId("user1");
+		authUser1.setRequesterPassword("pass1");
+		authUser2 = new Authentication();
+		authUser2.setRequesterId("user2");
+		authUser2.setRequesterPassword("pass2");
+		
+		tutorial = new TutorialChapter2_BasicRunSimulationExample();
+		//insertUsers();
+		
+//		fredSoftwareId = tutorial.getSoftwareIdentificationForSimulator();
+//		gaiaSoftwareId = tutorial.getSoftwareIdentifiationForGaia();
+//		insertSoftwareIds();
+
+	
+
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+		tutorial = null;
+	}
+
+	private void insertUsers() throws ApolloDatabaseException, ApolloDatabaseRecordAlreadyExistsException {
+
+		int userKey;
+		try {
+			userKey = apolloDbUtils.addUser(authUser1.getRequesterId(), authUser1.getRequesterPassword(), "testemail@email.com");
+			assertEquals(1, userKey);
+
+						
+			boolean canRun = true;
+			boolean canViewCache = true;
+			apolloDbUtils.addRole(tutorial.getSoftwareIdentificationForSimulator(), canRun, canViewCache);
+
+			userKey = apolloDbUtils.getUserKey(authUser1.getRequesterId(), authUser1.getRequesterPassword());
+			assertEquals(1, userKey);
+
+//			userKey = apolloDbUtils.getUserKey(authUser2.getRequesterId(), authUser2.getRequesterPassword());
 //			assertEquals(ApolloDbUtils.RECORD_NOT_FOUND, userKey);
-//			
-//			userKey = ApolloDbUtils.addUser(authUser2.getRequesterId(), authUser2.getRequesterPassword(), "testemail2@email2.com");
-//			assertEquals(2, userKey);
-//			
-//			userKey = ApolloDbUtils.addUser(authUser2.getRequesterId(), authUser2.getRequesterPassword(), "hello!@hello.com");
+			
+			userKey = apolloDbUtils.addUser(authUser2.getRequesterId(), authUser2.getRequesterPassword(), "testemail2@email2.com");
+			assertEquals(2, userKey);
+			
+//			userKey = apolloDbUtils.addUser(authUser2.getRequesterId(), authUser2.getRequesterPassword(), "hello!@hello.com");
 //			assertEquals(ApolloDbUtils.RECORD_ALREADY_EXISTS, userKey);
-//			
-//			userKey = ApolloDbUtils.getUserKey(authUser2.getRequesterId(), authUser2.getRequesterPassword());
-//			assertEquals(2, userKey);
-//			
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	/* used by AB to build the "run" record */
-//	private void insertSoftwareIds() {
-//		// test inserting & retrieving
-//		int fredKey = ApolloDbUtils.getSoftwareIdentificationKey(fredSoftwareId);
-//		assertEquals(1, fredKey);
-//
-//		// test inserting the same record multiple times, ensure key value isn't
-//		// changed
-//		fredKey = ApolloDbUtils.getSoftwareIdentificationKey(fredSoftwareId);
-//		assertEquals(1, fredKey);
-//
-//		// test inserting new software id
-//		int gaiaKey = ApolloDbUtils.getSoftwareIdentificationKey(gaiaSoftwareId);
-//		assertEquals(2, gaiaKey);
-//	}
-//
-//	public int insertTextContent() {
-//		int key = ApolloDbUtils.insertTextContent("This is a sample config file!");
-//		assertEquals(1, key);
-//		return key;
-//
-//	}
-//
-//	private void insertCfgFile() {
-//		// FRED CONFIG FILE
-//	}
-//
-//	public void testGetSoftwareIdentification() {
-//		SoftwareIdentification fredSoftwareId = ApolloDbUtils.getSoftwareIdentification(1); // 1
-//																						// ==
-//																						// FRED
-//		SoftwareIdentification gaiaSoftwareId = ApolloDbUtils.getSoftwareIdentification(2); // 2
-//																						// ==
-//																						// GAIA
-//
-//		assertEquals("UPitt,PSC,CMU", fredSoftwareId.getSoftwareDeveloper());
-//		assertEquals("FRED", fredSoftwareId.getSoftwareName());
-//		assertEquals("2.0.1_i", fredSoftwareId.getSoftwareVersion());
-//		assertEquals(ApolloSoftwareTypeEnum.SIMULATOR, fredSoftwareId.getSoftwareType());
-//
-//		assertEquals("PSC", fredSoftwareId.getSoftwareDeveloper());
-//		assertEquals("GAIA", fredSoftwareId.getSoftwareName());
-//		assertEquals("1.0", fredSoftwareId.getSoftwareVersion());
-//		assertEquals(ApolloSoftwareTypeEnum.VISUALIZER, fredSoftwareId.getSoftwareType());
-//	}
-//
+			
+			userKey = apolloDbUtils.getUserKey(authUser2.getRequesterId(), authUser2.getRequesterPassword());
+			assertEquals(2, userKey);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/* used by AB to build the "run" record */
+	private void insertSoftwareIds() {
+		// test inserting & retrieving
+		int fredKey;
+		try {
+			fredKey = apolloDbUtils.getSoftwareIdentificationKey(fredSoftwareId);
+			assertEquals(1, fredKey);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		}
+		
+
+		// test inserting new software id
+		int gaiaKey;
+		try {
+			gaiaKey = apolloDbUtils.getSoftwareIdentificationKey(gaiaSoftwareId);
+			assertEquals(2, gaiaKey);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			fail(e.getMessage());
+		
+		}
+	
+	}
+
+	public void testAddTextDataContent() {
+		int key;
+		try {
+			key = apolloDbUtils.addTextDataContent("This is a sample config file!");
+			assertEquals(1, key);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	
+		
+
+	}
+
+	private void insertCfgFile() {
+		// FRED CONFIG FILE
+	}
+
+	public void testGetSoftwareIdentification() {
+		SoftwareIdentification fredSoftwareId;
+		try {
+			fredSoftwareId = apolloDbUtils.getSoftwareIdentification(1);
+			assertEquals("UPitt,PSC,CMU", fredSoftwareId.getSoftwareDeveloper());
+			assertEquals("FRED", fredSoftwareId.getSoftwareName());
+			assertEquals("2.0.1_i", fredSoftwareId.getSoftwareVersion());
+			assertEquals(ApolloSoftwareTypeEnum.SIMULATOR, fredSoftwareId.getSoftwareType());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		} // 2
+																						// ==
+																						// FRED
+		try {
+			SoftwareIdentification gaiaSoftwareId = apolloDbUtils.getSoftwareIdentification(3);
+			assertEquals("PSC", gaiaSoftwareId.getSoftwareDeveloper());
+			assertEquals("GAIA", gaiaSoftwareId.getSoftwareName());
+			assertEquals("1.0", gaiaSoftwareId.getSoftwareVersion());
+			assertEquals(ApolloSoftwareTypeEnum.VISUALIZER, gaiaSoftwareId.getSoftwareType());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		} // 2
+																						// ==
+																						// GAIA
+
+	
+
+		
+	}
+
 //	public void testGetAuthentication() {
-//		Authentication auth = ApolloDbUtils.getAuthentication(1);
+//		Authentication auth = apolloDbUtils.getAuthentication(1);
 //		assertEquals("user1", auth.getRequesterId());
 //		assertEquals("user1pw", auth.getRequesterPassword());
 //
-//		auth = ApolloDbUtils.getAuthentication(2);
+//		auth = apolloDbUtils.getAuthentication(2);
 //		assertEquals("user2", auth.getRequesterId());
 //		assertEquals("user2pw", auth.getRequesterPassword());
 //	}
+
+//	public void testInsertRun() {
+//		int runKey = ApolloDbUtils.getRunKey(tutorial.getRunSimulationMessage());
 //
-////	public void testInsertRun() {
-////		int runKey = ApolloDbUtils.getRunKey(tutorial.getRunSimulationMessage());
-////
-////		assertEquals(1, runKey);
-////		int dataContentKey = insertTextContent();
-////		String dataFormat = "text";
-////		String dataLabel = "config.txt";
-////		String dataType = "configuration file";
-////		String dataSourceSoftware = "translator";
-////		String dataDestinationSoftware = "FRED";
-////
-////		int runDataKey = ApolloDbUtils.addContentToRun(runKey, dataContentKey, dataFormat, dataLabel, dataType, dataSourceSoftware,
-////				dataDestinationSoftware);
-////	}
+//		assertEquals(1, runKey);
+//		int dataContentKey = insertTextContent();
+//		String dataFormat = "text";
+//		String dataLabel = "config.txt";
+//		String dataType = "configuration file";
+//		String dataSourceSoftware = "translator";
+//		String dataDestinationSoftware = "FRED";
 //
-//	/**
-//	 * @param args
-//	 */
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
+//		int runDataKey = ApolloDbUtils.addContentToRun(runKey, dataContentKey, dataFormat, dataLabel, dataType, dataSourceSoftware,
+//				dataDestinationSoftware);
 //	}
+
+	public void testAddSimulationRun() {
+		try {
+			apolloDbUtils.addSimulationRun(tutorial.getRunSimulationMessage());
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
