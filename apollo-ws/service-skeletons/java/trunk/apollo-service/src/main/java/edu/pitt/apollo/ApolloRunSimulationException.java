049156a3fe9abd00e66a5a86ff0455705a111307
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2014-04-04 17:21:09 +0000

    added Apollo run simulation thread to apollo service apollo service runSimulation and getRunStatus methods are implemented

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-service/src/main/java/edu/pitt/apollo/ApolloRunSimulationException.java
@@ -0,0 +1,18 @@
+package edu.pitt.apollo;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Apr 3, 2014
+ * Time: 2:28:16 PM
+ * Class: ApolloRunSimulationException
+ * IDE: NetBeans 6.9.1
+ */
+public class ApolloRunSimulationException extends Exception {
+    
+    public ApolloRunSimulationException(String message) {
+        super(message);
+    }
+    
+}
