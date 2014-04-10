84110335529491ec933a64dbe8d101700c9ff3f3
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2014-04-10 19:47:49 +0000

    updated FluTE to work for 2.0.1 (runSimulation and getStatus)

--- apollo-ws/epidemic-simulator-services/FluTE/trunk/flute-simulator-service/src/main/java/edu/pitt/apollo/flute/utils/RunUtils.java
+++ apollo-ws/epidemic-simulator-services/FluTE/trunk/flute-simulator-service/src/main/java/edu/pitt/apollo/flute/utils/RunUtils.java
@@ -1,106 +1,61 @@
 package edu.pitt.apollo.flute.utils;
 
 import edu.pitt.apollo.FluteSimulatorServiceImpl;
-import edu.pitt.apollo.types._10._28._2013.RunStatus;
-import edu.pitt.apollo.types._10._28._2013.RunStatusEnum;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import java.io.FileNotFoundException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+
 import org.apache.commons.codec.digest.DigestUtils;
+
+import edu.pitt.apollo.types.v2_0_1.MethodCallStatus;
+import edu.pitt.apollo.types.v2_0_1.MethodCallStatusEnum;
 
 public class RunUtils {
 
-//    public static String WORK_DIR = "/Users/jdl50/apollo/work/";
-    public static String WORK_DIR = "/home/jdl/Public/";
-
-    public static synchronized String getNextId() throws IOException {
-
-        // this method is now only called in a static initializer
-        // add 1000 to the IDs to lessen the chance of using the same IDs twice
-        File dir = new File(WORK_DIR);
-        dir.mkdirs();
-        File f = new File(WORK_DIR + "id.txt");
-        if (!f.exists()) {
-            f.createNewFile();
-            FileWriter fw = new FileWriter(f);
-            fw.write("1001");
-            fw.close();
-            return "1001";
-        } else {
-            BufferedReader br = new BufferedReader(new FileReader(f));
-            String line = br.readLine();
-            int nextId = Integer.valueOf(line);
-            nextId++;
-            nextId += 1000;
-            br.close();
-
-            FileWriter fw = new FileWriter(f);
-            fw.write(String.valueOf(nextId));
-            fw.close();
-
-            return String.valueOf(nextId);
-        }
-
-    }
-
-    public static synchronized int writeCurrentId(int id) throws IOException {
-
-        File dir = new File(WORK_DIR);
-        dir.mkdirs();
-        File f = new File(WORK_DIR + "id.txt");
-        if (!f.exists()) {
-            f.createNewFile();
-        }
-        FileWriter fw = new FileWriter(f);
-        fw.write(String.valueOf(id));
-        fw.close();
-
-        return id;
-    }
-
-    public static synchronized String createRunDir(String runId)
+    public static synchronized String createRunDir(String directory)
             throws IOException {
 
-        File dir = new File(WORK_DIR + runId);
+        File dir = new File(directory);
         dir.mkdirs();
 
         return dir.getAbsolutePath();
     }
 
-    public static synchronized String setStarted(String runId) throws IOException {
-
-        String directory = createRunDir(runId);
-
-        //should be md5 hash of run id
-        File started = new File(WORK_DIR + runId + File.separator + "started.txt");
-        started.createNewFile();
-
-        return directory;
-    }
-
-    public static synchronized String getWorkDir(String runId) {
-
-        return WORK_DIR + runId + File.separator;
-    }
-
-    public static synchronized void setFinished(String runId)
+//    public static synchronized String setStarted(String directory) throws IOException {
+//
+//        createRunDir(directory);
+//
+//        //should be md5 hash of run id
+//        File started = new File(directory + File.separator + "started.txt");
+//        started.createNewFile();
+//
+//        return directory;
+//    }
+    public static synchronized void setStatusFile(String directory, MethodCallStatusEnum status)
             throws IOException {
 
-        File finished = new File(WORK_DIR + runId + File.separator + "finished.txt");
+        File finished = new File(directory + File.separator + status.toString().toLowerCase() + ".txt");
         finished.createNewFile();
     }
 
-    public static synchronized void setError(String runId, String msg)
+    public static boolean statusFileExists(String directory, MethodCallStatusEnum status) {
+        return new File(directory + File.separator + status.toString().toLowerCase() + ".txt").exists();
+    }
+
+//    public static synchronized void setFinished(String directory)
+//            throws IOException {
+//
+//        File finished = new File(directory + File.separator + "finished.txt");
+//        finished.createNewFile();
+//    }
+    public static synchronized void setError(String directory, String msg)
             throws IOException {
 
-        File error = new File(WORK_DIR + runId + File.separator + "error.txt");
+        File error = new File(directory + File.separator + "error.txt");
         error.createNewFile();
 
         FileWriter fw = new FileWriter(error);
@@ -108,9 +63,9 @@ public class RunUtils {
         fw.close();
     }
 
-    public static synchronized String getError(String runId) throws IOException {
+    public static synchronized String getError(String directory) throws IOException {
 
-        File error = new File(WORK_DIR + runId + File.separator + "error.txt");
+        File error = new File(directory + File.separator + "error.txt");
 
         BufferedReader br = new BufferedReader(new FileReader(error));
         String errorMsg = br.readLine();
@@ -119,12 +74,9 @@ public class RunUtils {
 
     }
 
-    public static String getResultsString(String runId) throws FileNotFoundException {
+    public static String getResultsString(String directory) throws FileNotFoundException {
 
-//        String runIdHash = RunUtils.getMd5HashFromBytes(runId.getBytes());
-        String runIdHash = RunUtils.getMd5HashFromString(runId);
-
-        File file = new File(WORK_DIR + runIdHash + File.separator + "results.txt");
+        File file = new File(directory + File.separator + "results.txt");
         Scanner scanner = new Scanner(file);
         scanner.nextLine(); // first line is the header
         String results = scanner.nextLine();
@@ -133,63 +85,43 @@ public class RunUtils {
         return results;
     }
 
-    public static RunStatus getStatus(String runId)
+    public static MethodCallStatus getStatus(String directory, int runId)
             throws IOException {
-//        String dirName = getMd5HashFromBytes(runId.getBytes());
-        String dirName = getMd5HashFromString(runId);
-        if (dirName == null) {
-            System.err.println("Directory name from run ID hash was null");
-            return null;
-        }
 
-//        String runIdHash = RunUtils.getMd5HashFromBytes(runId.getBytes());
-        String runIdHash = RunUtils.getMd5HashFromString(runId);
-
-        RunStatus rs = new RunStatus();
-        File error = new File(WORK_DIR + dirName + File.separator + "error.txt");
-        File finished = new File(WORK_DIR + dirName + File.separator + "finished.txt");
-        File started = new File(WORK_DIR + dirName + File.separator + "started.txt");
+        MethodCallStatus status = new MethodCallStatus();
+        File error = new File(directory + File.separator + "error.txt");
+//        File finished = new File(directory + File.separator + "finished.txt");
+//        File started = new File(directory + File.separator + "started.txt");
 
         if (error.exists()) {
-            rs.setMessage(RunUtils.getError(dirName));
-            rs.setStatus(RunStatusEnum.FAILED);
-            return rs;
-        } else if (finished.exists()) {
-            rs.setMessage("Run is complete.");
-            rs.setStatus(RunStatusEnum.COMPLETED);
-            return rs;
-        } else if (started.exists()) {
-            rs.setMessage("Running...");
-            rs.setStatus(RunStatusEnum.RUNNING);
-            return rs;
-        } else if (FluteSimulatorServiceImpl.isRunQueued(runIdHash)) {
-            rs.setMessage("Run is queued");
-            rs.setStatus(RunStatusEnum.QUEUED);
-            return rs;
+            status.setMessage(RunUtils.getError(directory));
+            status.setStatus(MethodCallStatusEnum.FAILED);
+            return status;
+        } else if (statusFileExists(directory, MethodCallStatusEnum.COMPLETED)) {
+            status.setMessage("Run is complete.");
+            status.setStatus(MethodCallStatusEnum.COMPLETED);
+            return status;
+        } else if (statusFileExists(directory, MethodCallStatusEnum.LOG_FILES_WRITTEN)) {
+            status.setMessage("Log files written");
+            status.setStatus(MethodCallStatusEnum.LOG_FILES_WRITTEN);
+            return status;
+        } else if (statusFileExists(directory, MethodCallStatusEnum.RUNNING)) {
+            status.setMessage("Running...");
+            status.setStatus(MethodCallStatusEnum.RUNNING);
+            return status;
+        } else if (FluteSimulatorServiceImpl.isRunQueued(runId)) {
+            status.setMessage("Run is queued");
+            status.setStatus(MethodCallStatusEnum.QUEUED);
+            return status;
         } else {
-            rs.setMessage("Unknown run");
-            rs.setStatus(RunStatusEnum.FAILED);
-            return rs;
+            status.setMessage("Unknown run");
+            status.setStatus(MethodCallStatusEnum.FAILED);
+            return status;
         }
     }
-    
+
     public static String getMd5HashFromString(String string) {
-        
+
         return DigestUtils.md5Hex(string);
-    }
-
-    public static void main(String args[]) throws IOException {
-        System.out.println(RunUtils.getNextId());
-        System.out.println(RunUtils.getNextId());
-        System.out.println(RunUtils.getNextId());
-
-        RunUtils.setStarted("tester");
-        System.out.println(RunUtils.getStatus("tester").getStatus());
-
-        RunUtils.setFinished("tester");
-        System.out.println(RunUtils.getStatus("tester").getStatus());
-
-        RunUtils.setError("tester", "not really an error, just a test.");
-        System.out.println(RunUtils.getStatus("tester").getStatus());
     }
 }
