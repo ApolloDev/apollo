071cc19c5c199cb984dcfbb6164d9eeca20733c7
JohnLevander <jdl50@pitt.edu>
2014-04-11 21:57:51 +0000

    Removed compiler warnings mostly

--- apollo-ws/service-skeletons/java/trunk/visualizer-service/src/main/java/edu/pitt/apollo/timeseriesvisualizer/utilities/TimeSeriesResultSetProcessor.java
+++ apollo-ws/service-skeletons/java/trunk/visualizer-service/src/main/java/edu/pitt/apollo/timeseriesvisualizer/utilities/TimeSeriesResultSetProcessor.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
