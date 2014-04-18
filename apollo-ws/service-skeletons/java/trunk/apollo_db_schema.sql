d59a7f21abed431ef0877c8b262b787444f843ec
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2014-04-18 21:49:44 +0000

    Updated the SEIR WSDL location

--- apollo-ws/service-skeletons/java/trunk/apollo_db_schema.sql
+++ apollo-ws/service-skeletons/java/trunk/apollo_db_schema.sql
@@ -44,7 +44,7 @@ CREATE TABLE software_identification (
 );
 
 INSERT INTO `software_identification` VALUES (1,'UPitt','Translator','1.0','translator','http://localhost:8080/translatorservice201/services/translatorservice?wsdl',1),
-											 (2,'UPitt','SEIR','1.0','simulator','http://localhost:8080/seirsimulatorservice2.0.1/services/seirsimulatorservice?wsdl',1),
+											 (2,'UPitt','SEIR','1.0','simulator','http://betaweb.rods.pitt.edu/seirsimulatorservice2.0.1/services/seirsimulatorservice?wsdl',1),
 											 (3,'UPitt,PSC,CMU','FRED','2.0.1_i','simulator','http://warhol-fred.psc.edu:8094/fred?wsdl',1),
 											 (4,'UPitt','Time Series Visualizer','1.0','visualizer','http://localhost:8080/visualizerservice2.0.1/services/visualizerservice?wsdl',1),
 											 (5,'PSC','GAIA','1.0','visualizer','http://warhol-fred.psc.edu:8094/fred?wsdl',1),
