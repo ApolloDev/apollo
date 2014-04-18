83ed9dd5561890002c182bdff12d098854bf39f2
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2014-04-18 15:13:52 +0000

    More debugging/improvements

--- apollo-ws/service-skeletons/java/trunk/apollo-config-examples/src/main/java/edu/pitt/apollo/examples/TutorialChapter2_ExampleConfig.java
+++ apollo-ws/service-skeletons/java/trunk/apollo-config-examples/src/main/java/edu/pitt/apollo/examples/TutorialChapter2_ExampleConfig.java
@@ -25,6 +25,8 @@ import javax.xml.datatype.XMLGregorianCalendar;
 import edu.pitt.apollo.types.v2_0_1.ApolloPathogenCode;
 import edu.pitt.apollo.types.v2_0_1.ApolloSoftwareTypeEnum;
 import edu.pitt.apollo.types.v2_0_1.Authentication;
+import edu.pitt.apollo.types.v2_0_1.DiseaseOutcomeEnum;
+import edu.pitt.apollo.types.v2_0_1.DiseaseOutcomeWithProbability;
 import edu.pitt.apollo.types.v2_0_1.FixedDuration;
 import edu.pitt.apollo.types.v2_0_1.Infection;
 import edu.pitt.apollo.types.v2_0_1.InfectionAcquisition;
@@ -37,12 +39,17 @@ import edu.pitt.apollo.types.v2_0_1.LocationDefinition;
 import edu.pitt.apollo.types.v2_0_1.PopulationInfectionAndImmunityCensus;
 import edu.pitt.apollo.types.v2_0_1.PopulationInfectionAndImmunityCensusData;
 import edu.pitt.apollo.types.v2_0_1.PopulationInfectionAndImmunityCensusDataCell;
+import edu.pitt.apollo.types.v2_0_1.ProbabilisticParameter;
 import edu.pitt.apollo.types.v2_0_1.RunSimulationMessage;
 import edu.pitt.apollo.types.v2_0_1.SimulatorTimeSpecification;
 import edu.pitt.apollo.types.v2_0_1.SoftwareIdentification;
 import edu.pitt.apollo.types.v2_0_1.UnitOfTimeEnum;
 
 public class TutorialChapter2_ExampleConfig {
+
+	public static final String H1N1_NCBI_TAXON_ID = "114727";
+	public static final String HUMAN_NCBI_TAXON_ID = "9606";
+	public static final String H1N1_VACCINE_ONTOLOGY_ID = "0001599";
 
 	public static Authentication getAuthentication() {
 		Authentication authentication = new Authentication();
@@ -52,26 +59,106 @@ public class TutorialChapter2_ExampleConfig {
 	}
 
 	public static SoftwareIdentification getSoftwareIdentificationForSimulator() {
-		
+
+		 SoftwareIdentification softwareId = new SoftwareIdentification();
+		 softwareId.setSoftwareDeveloper("UPitt,PSC,CMU");
+		 softwareId.setSoftwareName("FRED");
+		 softwareId.setSoftwareVersion("2.0.1_i");
+		 softwareId.setSoftwareType(ApolloSoftwareTypeEnum.SIMULATOR);
 //		SoftwareIdentification softwareId = new SoftwareIdentification();
-//		softwareId.setSoftwareDeveloper("UPitt,PSC,CMU");
-//		softwareId.setSoftwareName("FRED");
-//		softwareId.setSoftwareVersion("2.0.1_i");
+//		softwareId.setSoftwareDeveloper("UPitt");
+//		softwareId.setSoftwareName("SEIR");
+//		softwareId.setSoftwareVersion("1.0");
 //		softwareId.setSoftwareType(ApolloSoftwareTypeEnum.SIMULATOR);
-		SoftwareIdentification softwareId = new SoftwareIdentification();
-		softwareId.setSoftwareDeveloper("UPitt");
-		softwareId.setSoftwareName("SEIR");
-		softwareId.setSoftwareVersion("1.0");
-		softwareId.setSoftwareType(ApolloSoftwareTypeEnum.SIMULATOR);
 		return softwareId;
 	}
 
-	protected SimulatorTimeSpecification getSimulatorTimeSpecification() {
-		SimulatorTimeSpecification timeSpec = new SimulatorTimeSpecification();
-		timeSpec.setRunLength(new BigInteger("100"));
-		timeSpec.setUnitOfTimeForSimulatorTimeStep(UnitOfTimeEnum.DAY);
-		timeSpec.setNumberOfUnitsOfTimeInOneSimulatorTimeStep(1.0);
-		return timeSpec;
+	protected Infection getInfection() {
+		Infection infection = new Infection();
+		infection.setPathogenTaxonId(getPathogen());
+		infection.setHostTaxonId(HUMAN_NCBI_TAXON_ID);
+
+		FixedDuration infectiousPeriod = new FixedDuration();
+		infectiousPeriod.setUnitOfTime(UnitOfTimeEnum.DAY);
+		infectiousPeriod.setValue(6.0);
+		infection.setInfectiousPeriodDuration(infectiousPeriod);
+
+		FixedDuration latentPeriod = new FixedDuration();
+		latentPeriod.setUnitOfTime(UnitOfTimeEnum.DAY);
+		latentPeriod.setValue(2.0);
+		infection.setLatentPeriodDuration(latentPeriod);
+
+		infection.getInfectionAcquisition().add(getInfectionAcquisition());
+
+		return infection;
+	}
+
+	private InfectionAcquisition getInfectionAcquisition() {
+		InfectionAcquisition infectionAcquisition = new InfectionAcquisition();
+
+		ApolloPathogenCode pathId = new ApolloPathogenCode();
+		pathId.setNcbiTaxonId(H1N1_NCBI_TAXON_ID);
+		infectionAcquisition.setPathogenTaxonId(pathId);
+		infectionAcquisition.setSusceptibleHostTaxonId(HUMAN_NCBI_TAXON_ID);
+		infectionAcquisition.setFromInfectiousHost(new InfectionAcquisitionFromInfectiousHost());
+		infectionAcquisition.getFromInfectiousHost().setBasicReproductionNumber(1.3);
+		
+		return infectionAcquisition;
+	}
+
+	protected InfectiousDisease getInfectiousDisease() {
+		InfectiousDisease disease = new InfectiousDisease();
+		disease.setDiseaseId("Influenza");
+		disease.setSpeciesWithDisease(HUMAN_NCBI_TAXON_ID);
+		disease.setCausalPathogen(getPathogen());
+		disease.getDiseaseOutcomesWithProbabilities().add(getProbabilitySymptomaticForDisease());
+		return disease;
+	}
+	
+	private XMLGregorianCalendar getXmlDateGivenYearMonthDay(int year, int month, int day) {
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.DAY_OF_MONTH, month);
+		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+
+		// translate from Java style Calendar to an XML compatible calendar
+		XMLGregorianCalendar scenarioDate = null;
+		try {
+			scenarioDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
+			return scenarioDate;
+		} catch (DatatypeConfigurationException e) {
+			System.out.println("Error!  Unable to set date, error was:" + e.getMessage());
+			System.exit(-1);
+		}
+		return null;
+		
+		
+	}
+
+	protected InfectiousDiseaseScenario getInfectiousDiseaseScenario() {
+		InfectiousDiseaseScenario scenario = new InfectiousDiseaseScenario();
+
+		LocationDefinition definition = new LocationDefinition();
+		definition.setDescription("Allegheny County, Pennsylvania");
+		Location location = new Location();
+		location.setApolloLocationCode("42003");
+		scenario.setLocation(location);
+		scenario.setScenarioDate(getXmlDateGivenYearMonthDay(2009, Calendar.SEPTEMBER, 1));
+		scenario.getInfections().add(getInfection());
+		scenario.getDiseases().add(getInfectiousDisease());
+		scenario.getPopulationInfectionAndImmunityCensuses().add(getPopulationInfectionAndImmunityCensus());
+
+		return scenario;
+	}
+
+	private ApolloPathogenCode getPathogen() {
+		ApolloPathogenCode pathogen = new ApolloPathogenCode();
+		pathogen.setNcbiTaxonId(H1N1_NCBI_TAXON_ID);
+		return pathogen;
 	}
 
 	protected PopulationInfectionAndImmunityCensus getPopulationInfectionAndImmunityCensus() {
@@ -99,11 +186,10 @@ public class TutorialChapter2_ExampleConfig {
 		Location location = new Location();
 		location.setApolloLocationCode("42003");
 		census.setLocation(location);
-		census.setPopulationSpecies("9606"); // homo sapiens
+		census.setPopulationSpecies(HUMAN_NCBI_TAXON_ID); // homo sapiens
 
 		ApolloPathogenCode pathId = new ApolloPathogenCode();
-		pathId.setCladeName("H1N1");
-		pathId.setNcbiTaxonId("114727"); // Influenza A subtype H1N1
+		pathId.setNcbiTaxonId(H1N1_NCBI_TAXON_ID); // Influenza A subtype H1N1
 		census.setPathogen(pathId);
 
 		PopulationInfectionAndImmunityCensusData data = new PopulationInfectionAndImmunityCensusData();
@@ -130,92 +216,16 @@ public class TutorialChapter2_ExampleConfig {
 		return census;
 	}
 
-	protected InfectiousDisease getInfectiousDisease() {
-		InfectiousDisease disease = new InfectiousDisease();
-		disease.setDiseaseId("H1N1");
-		disease.setSpeciesWithDisease("9606"); // homo sapiens
+	private DiseaseOutcomeWithProbability getProbabilitySymptomaticForDisease() {
+		/* set the disease to have a 70% symptomatic rate */
+		ProbabilisticParameter probabilisticParameter = new ProbabilisticParameter();
+		probabilisticParameter.setProbability(0.7);
 
-		ApolloPathogenCode pathId = new ApolloPathogenCode();
-		pathId.setCladeName("H1N1");
-		pathId.setNcbiTaxonId("114727"); // Influenza A subtype H1N1
+		DiseaseOutcomeWithProbability diseaseOutcomeWithProbability = new DiseaseOutcomeWithProbability();
+		diseaseOutcomeWithProbability.setDiseaseOutcome(DiseaseOutcomeEnum.SYMPTOMATIC);
+		diseaseOutcomeWithProbability.setProbability(probabilisticParameter);
 
-		disease.setCausalPathogen(pathId);
-		return disease;
-	}
-
-	protected Infection getInfection() {
-		Infection infection = new Infection();
-
-		ApolloPathogenCode pathId = new ApolloPathogenCode();
-		pathId.setCladeName("H1N1");
-		pathId.setNcbiTaxonId("114727"); // Influenza A subtype H1N1
-		infection.setPathogenTaxonId(pathId);
-
-		infection.setHostTaxonId("9606"); // homo sapiens
-
-		FixedDuration infectiousPeriod = new FixedDuration();
-		infectiousPeriod.setUnitOfTime(UnitOfTimeEnum.DAY);
-		infectiousPeriod.setValue(6.0);
-		infection.setInfectiousPeriodDuration(infectiousPeriod);
-
-		FixedDuration latentPeriod = new FixedDuration();
-		latentPeriod.setUnitOfTime(UnitOfTimeEnum.DAY);
-		latentPeriod.setValue(2.0);
-		infection.setLatentPeriodDuration(latentPeriod);
-
-		infection.getInfectionAcquisition().add(getInfectionAcquisition());
-
-		return infection;
-	}
-
-	private InfectionAcquisition getInfectionAcquisition() {
-		InfectionAcquisition infectionAcquisition = new InfectionAcquisition();
-
-		ApolloPathogenCode pathId = new ApolloPathogenCode();
-		pathId.setCladeName("H1N1");
-		pathId.setNcbiTaxonId("114727"); // Influenza A subtype H1N1
-		infectionAcquisition.setPathogenTaxonId(pathId);
-		infectionAcquisition.setSusceptibleHostTaxonId("9606"); // homo sapiens
-		infectionAcquisition.setFromInfectiousHost(new InfectionAcquisitionFromInfectiousHost());
-		infectionAcquisition.getFromInfectiousHost().setBasicReproductionNumber(1.3);
-		return infectionAcquisition;
-	}
-
-	protected InfectiousDiseaseScenario getInfectiousDiseaseScenario() {
-		InfectiousDiseaseScenario scenario = new InfectiousDiseaseScenario();
-
-		LocationDefinition definition = new LocationDefinition();
-		definition.setDescription("Allegheny County, Pennsylvania");
-		// set the scenario location to Allegheny County
-		Location location = new Location();
-		location.setApolloLocationCode("42003");
-		scenario.setLocation(location);
-
-		// set the scenario date to 2009/09/01
-		GregorianCalendar calendar = new GregorianCalendar();
-		calendar.set(Calendar.YEAR, 2009);
-		calendar.set(Calendar.MONTH, Calendar.SEPTEMBER);
-		calendar.set(Calendar.DAY_OF_MONTH, 1);
-		calendar.set(Calendar.HOUR, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
-
-		// translate from Java style Calendar to an XML compatible calendar
-		XMLGregorianCalendar scenarioDate = null;
-		try {
-			scenarioDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(calendar);
-		} catch (DatatypeConfigurationException e) {
-			System.out.println("Error!  Unable to set date, error was:" + e.getMessage());
-			System.exit(-1);
-		}
-
-		scenario.setScenarioDate(scenarioDate);
-		scenario.getInfections().add(getInfection());
-		scenario.getDiseases().add(getInfectiousDisease());
-		scenario.getPopulationInfectionAndImmunityCensuses().add(getPopulationInfectionAndImmunityCensus());
-
-		return scenario;
+		return diseaseOutcomeWithProbability;
 	}
 
 	public RunSimulationMessage getRunSimulationMessage() {
@@ -225,6 +235,14 @@ public class TutorialChapter2_ExampleConfig {
 		message.setSimulatorIdentification(getSoftwareIdentificationForSimulator());
 		message.setSimulatorTimeSpecification(getSimulatorTimeSpecification());
 		return message;
+	}
+
+	protected SimulatorTimeSpecification getSimulatorTimeSpecification() {
+		SimulatorTimeSpecification timeSpec = new SimulatorTimeSpecification();
+		timeSpec.setRunLength(new BigInteger("100"));
+		timeSpec.setUnitOfTimeForSimulatorTimeStep(UnitOfTimeEnum.DAY);
+		timeSpec.setNumberOfUnitsOfTimeInOneSimulatorTimeStep(1.0);
+		return timeSpec;
 	}
 
 }
