8238631633635f4b95e3a6ecde287f5eb7d0e714
nickmillett <nem41@pitt.edu>
2014-05-09 21:44:49 +0000

    big re-factor to the apollo service to move the service methods into their own classes

---
+++ apollo-ws/service-skeletons/java/trunk/apollo-service/src/main/java/edu/pitt/apollo/apolloservice/methods/content/GetConfigurationFileForSimulationMethod.java
@@ -0,0 +1,60 @@
+package edu.pitt.apollo.apolloservice.methods.content;
+
+import edu.pitt.apollo.apolloservice.database.ApolloDbUtilsContainer;
+import edu.pitt.apollo.apolloservice.translatorservice.TranslatorServiceRecordContainer;
+import edu.pitt.apollo.db.ApolloDatabaseException;
+import edu.pitt.apollo.db.ApolloDbUtils;
+import edu.pitt.apollo.types.v2_0_1.GetConfigurationFileForSimulationResult;
+import edu.pitt.apollo.types.v2_0_1.MethodCallStatus;
+import edu.pitt.apollo.types.v2_0_1.MethodCallStatusEnum;
+import edu.pitt.apollo.types.v2_0_1.RunAndSoftwareIdentification;
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: May 9, 2014
+ * Time: 1:52:25 PM
+ * Class: GetConfigurationFileForSimulationMethod
+ * IDE: NetBeans 6.9.1
+ */
+public class GetConfigurationFileForSimulationMethod {
+
+    public static GetConfigurationFileForSimulationResult getConfigurationFileForSimulation(RunAndSoftwareIdentification runAndSoftwareIdentification) {
+        
+        ApolloDbUtils dbUtils = ApolloDbUtilsContainer.getApolloDbUtils();
+        GetConfigurationFileForSimulationResult result = new GetConfigurationFileForSimulationResult();
+        MethodCallStatus status = new MethodCallStatus();
+        result.setMethodCallStatus(status);
+
+        int runId = Integer.parseInt(runAndSoftwareIdentification.getRunId());
+        Map<String, ByteArrayOutputStream> map;
+        try {
+            map = dbUtils.getDataContentForSoftware(
+                    runId,
+                    dbUtils.getSoftwareIdentificationKey(TranslatorServiceRecordContainer.getTranslatorSoftwareIdentification()),
+                    dbUtils.getSoftwareIdentificationKey(runAndSoftwareIdentification.getSoftwareId()));
+        } catch (ApolloDatabaseException ex) {
+            status.setStatus(MethodCallStatusEnum.FAILED);
+            status.setMessage(ex.getMessage());
+            return result;
+        }
+
+        for (String label : map.keySet()) {
+            ByteArrayOutputStream stream = map.get(label);
+            if (label.equals("config.txt")) {
+                String content = stream.toString();
+                result.setConfigurationFile(content);
+            } else if (label.equals("verbose.html")) {
+                String content = stream.toString();
+                result.setConfigurationFileInHtmlFormat(content);
+            }
+        }
+
+        status.setStatus(MethodCallStatusEnum.COMPLETED);
+
+        return result;
+    }
+}
