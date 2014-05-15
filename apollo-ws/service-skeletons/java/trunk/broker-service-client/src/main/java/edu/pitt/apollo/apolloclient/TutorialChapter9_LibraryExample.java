9d6a3cfdc75be7c715d846071d8b5066db4bfd24
john.d.levander@gmail.com <jdl50@pitt.edu>
2014-05-15 20:30:26 +0000

    Updating the tutorial code for clarity and readability

--- apollo-ws/service-skeletons/java/trunk/broker-service-client/src/main/java/edu/pitt/apollo/apolloclient/TutorialChapter9_LibraryExample.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service-client/src/main/java/edu/pitt/apollo/apolloclient/TutorialChapter9_LibraryExample.java
@@ -3,8 +3,8 @@ package edu.pitt.apollo.apolloclient;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
-import edu.pitt.apollo.examples.TutorialChapter2_ExampleConfig;
-import edu.pitt.apollo.examples.TutorialChapter8_RunSimulationWithVaccinationControlStrategyConfig;
+import edu.pitt.apollo.examples.ExampleRunSimulationMessageFactory;
+import edu.pitt.apollo.examples.ExampleUseOfVaccinationControlStrategy;
 import edu.pitt.apollo.types.v2_0_1.AddLibraryItemResult;
 import edu.pitt.apollo.types.v2_0_1.Authentication;
 import edu.pitt.apollo.types.v2_0_1.DiseaseSurveillanceTriggerDefinition;
@@ -20,17 +20,18 @@ import edu.pitt.apollo.types.v2_0_1.RunSimulationMessage;
 import edu.pitt.apollo.types.v2_0_1.TriggerDefinition;
 import edu.pitt.apollo.types.v2_0_1.Vaccination;
 
-public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample {
+public class TutorialChapter9_LibraryExample extends
+		AbstractTutorialWebServiceClient {
 
 	public String infectionUuid = "";
 	public String infectiousDiseaseScenarioUuid = "";
 	public String vaccinationControlStrategyUuid = "";
-	
-	public TutorialChapter8_RunSimulationWithVaccinationControlStrategyConfig chapter8Config;
+
+	public ExampleUseOfVaccinationControlStrategy chapter8Config;
 
 	public TutorialChapter9_LibraryExample() throws MalformedURLException {
 		super();
-		chapter8Config = new TutorialChapter8_RunSimulationWithVaccinationControlStrategyConfig();
+		chapter8Config = new ExampleUseOfVaccinationControlStrategy();
 	}
 
 	protected Authentication getAuthentication() {
@@ -41,18 +42,19 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 	}
 
 	public void saveItems() {
-		
-		RunSimulationMessage runSimulationMessage = chapter8Config.getRunSimulationMessage();
-		
 
-		InfectiousDiseaseScenario infectiousDiseaseScenario = runSimulationMessage.getInfectiousDiseaseScenario();
+		RunSimulationMessage runSimulationMessage = chapter8Config.getRunSimulationMessage();
+
+		InfectiousDiseaseScenario infectiousDiseaseScenario = runSimulationMessage
+				.getInfectiousDiseaseScenario();
 		ArrayList<String> labels = new ArrayList<String>();
 		labels.add("tutorial");
 
 		Infection infection = infectiousDiseaseScenario.getInfections().get(0);
 
 		AddLibraryItemResult result = getPort().addLibraryItem(getAuthentication(), infection,
-				"The Infection instance of the InfectiousDiseaseScenario from the tutorial..", "Tutorial", "Infection", labels);
+				"The Infection instance of the InfectiousDiseaseScenario from the tutorial..", "Tutorial",
+				"Infection", labels);
 		infectionUuid = result.getUuid();
 		System.out.println("Uuid returned for newly saved Infection instance: " + result.getUuid());
 
@@ -60,16 +62,20 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 				.getInfectiousDiseaseControlStrategies().get(0);
 
 		result = getPort().addLibraryItem(getAuthentication(), vaccinationControlStrategy,
-				"The Vaccination Control Strategy used in the tutorial", "Tutorial", "InfectiousDiseaseControlStrategy", labels);
+				"The Vaccination Control Strategy used in the tutorial", "Tutorial",
+				"InfectiousDiseaseControlStrategy", labels);
 
 		vaccinationControlStrategyUuid = result.getUuid();
-		System.out.println("Uuid returned for newly saved VaccinationControlStrategy instance: " + result.getUuid());
+		System.out.println("Uuid returned for newly saved VaccinationControlStrategy instance: "
+				+ result.getUuid());
 
 		result = getPort().addLibraryItem(getAuthentication(), infectiousDiseaseScenario,
-				"The Infectious Disease Scenario used in the tutorial", "Tutorial", "InfectiousDiseaseScenario", labels);
+				"The Infectious Disease Scenario used in the tutorial", "Tutorial",
+				"InfectiousDiseaseScenario", labels);
 
 		infectiousDiseaseScenarioUuid = result.getUuid();
-		System.out.println("Uuid returned for newly saved InfectionDiseaseScenario instance: " + result.getUuid());
+		System.out.println("Uuid returned for newly saved InfectionDiseaseScenario instance: "
+				+ result.getUuid());
 		System.out.println();
 
 	}
@@ -77,7 +83,8 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 	public void loadInfection() {
 		System.out.println("Retrieving Infection instance from Library using Uuid: " + infectionUuid + "\n");
 		GetLibraryItemResult infectionItem = getPort().getLibraryItem(infectionUuid);
-		Infection infection = (Infection) infectionItem.getCuratedLibraryItemContainer().getApolloIndexableItem();
+		Infection infection = (Infection) infectionItem.getCuratedLibraryItemContainer()
+				.getApolloIndexableItem();
 		System.out.println("Infection instance retrieved from the library!  Here are the attributes:");
 		System.out.println("\tHost Taxon ID: " + infection.getHostTaxonId());
 		System.out.println("\tPathogen Taxon ID: " + infection.getPathogenTaxonId().getNcbiTaxonId());
@@ -89,8 +96,10 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 
 		InfectionAcquisition infectionAcquisition = infection.getInfectionAcquisition().get(0);
 		System.out.println("\tInfectionAcquisition:");
-		System.out.println("\t\tSusceptible Host Taxon ID: " + infectionAcquisition.getSusceptibleHostTaxonId());
-		System.out.println("\t\tPathogen Taxon ID: " + infectionAcquisition.getPathogenTaxonId().getNcbiTaxonId());
+		System.out.println("\t\tSusceptible Host Taxon ID: "
+				+ infectionAcquisition.getSusceptibleHostTaxonId());
+		System.out.println("\t\tPathogen Taxon ID: "
+				+ infectionAcquisition.getPathogenTaxonId().getNcbiTaxonId());
 		if (infectionAcquisition.getFromInfectiousHost() != null)
 			System.out.println("\t\tBasic Reproduction Number: "
 					+ infectionAcquisition.getFromInfectiousHost().getBasicReproductionNumber());
@@ -98,7 +107,8 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 	}
 
 	public void loadVaccinationControlStrategy() {
-		GetLibraryItemResult vaccinationControlStrategyItem = getPort().getLibraryItem(vaccinationControlStrategyUuid);
+		GetLibraryItemResult vaccinationControlStrategyItem = getPort().getLibraryItem(
+				vaccinationControlStrategyUuid);
 		IndividualTreatmentControlStrategy vaccinationControlStrategy = (IndividualTreatmentControlStrategy) vaccinationControlStrategyItem
 				.getCuratedLibraryItemContainer().getApolloIndexableItem();
 		System.out.println("VaccinationControlStrategy instance retrieved from the library:");
@@ -106,12 +116,16 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 		System.out.println("\tCompliance: " + vaccinationControlStrategy.getCompliance().getProbability());
 		if (vaccinationControlStrategy.getControlStrategyResponseDelay() instanceof FixedDuration) {
 			System.out.println("\tResponse Delay: "
-					+ ((FixedDuration) vaccinationControlStrategy.getControlStrategyResponseDelay()).getValue());
+					+ ((FixedDuration) vaccinationControlStrategy.getControlStrategyResponseDelay())
+							.getValue());
 		}
 		for (TriggerDefinition trigger : vaccinationControlStrategy.getControlStrategyStartTime()) {
 			DiseaseSurveillanceTriggerDefinition triggerDefinition = (DiseaseSurveillanceTriggerDefinition) trigger;
-			System.out.println("\tControl Strategy Starts when : " +
-					 triggerDefinition.getReactiveControlStrategyOperator().toString() + " " + triggerDefinition.getReactiveControlStrategyThreshold() + " " + triggerDefinition.getReactiveControlStrategyTest() + " " + triggerDefinition.getUnitOfMeasureForThreshold().toString());
+			System.out.println("\tControl Strategy Starts when : "
+					+ triggerDefinition.getReactiveControlStrategyOperator().toString() + " "
+					+ triggerDefinition.getReactiveControlStrategyThreshold() + " "
+					+ triggerDefinition.getReactiveControlStrategyTest() + " "
+					+ triggerDefinition.getUnitOfMeasureForThreshold().toString());
 
 		}
 
@@ -123,12 +137,13 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 	public InfectiousDiseaseScenario loadInfectiousDiseaseScenario() {
 		System.out.println("Retrieving InfectiousDiseaseScenario instance from Library using Uuid: "
 				+ infectiousDiseaseScenarioUuid + "\n");
-		GetLibraryItemResult infectionDiseaseScenarioLibraryItem = getPort().getLibraryItem(infectiousDiseaseScenarioUuid);
+		GetLibraryItemResult infectionDiseaseScenarioLibraryItem = getPort().getLibraryItem(
+				infectiousDiseaseScenarioUuid);
 		InfectiousDiseaseScenario infectiousDiseaseScenario = (InfectiousDiseaseScenario) infectionDiseaseScenarioLibraryItem
 				.getCuratedLibraryItemContainer().getApolloIndexableItem();
 
-		InfectionAcquisition infectionAcquisition = infectiousDiseaseScenario.getInfections().get(0).getInfectionAcquisition()
-				.get(0);
+		InfectionAcquisition infectionAcquisition = infectiousDiseaseScenario.getInfections().get(0)
+				.getInfectionAcquisition().get(0);
 		if (infectionAcquisition.getFromInfectiousHost() != null) {
 			System.out.println("The existing Basic Reproduction Number in the InfectionDiseaseScenario is: "
 					+ infectionAcquisition.getFromInfectiousHost().getBasicReproductionNumber());
@@ -138,7 +153,8 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 
 	private InfectiousDiseaseScenario setR0(InfectiousDiseaseScenario infectiousDiseaseScenario, Double value) {
 		System.out.println("Changing the Basic Reproduction Number to 1.7...");
-		InfectionAcquisition infectionAcquisition = infectiousDiseaseScenario.getInfections().get(0).getInfectionAcquisition().get(0);
+		InfectionAcquisition infectionAcquisition = infectiousDiseaseScenario.getInfections().get(0)
+				.getInfectionAcquisition().get(0);
 		infectionAcquisition.getFromInfectiousHost().setBasicReproductionNumber(1.7);
 		return infectiousDiseaseScenario;
 	}
@@ -152,21 +168,27 @@ public class TutorialChapter9_LibraryExample extends TutorialChapter8_RunSimulat
 		tutorialChapter9.saveItems();
 		tutorialChapter9.loadInfection();
 		tutorialChapter9.loadVaccinationControlStrategy();
-		InfectiousDiseaseScenario infectiousDiseaseScenario = tutorialChapter9.loadInfectiousDiseaseScenario();
-		InfectiousDiseaseScenario infectiousDiseaseScenarioWithHighR0 = tutorialChapter9.setR0(infectiousDiseaseScenario, 1.7);
+		InfectiousDiseaseScenario infectiousDiseaseScenario = tutorialChapter9
+				.loadInfectiousDiseaseScenario();
+		InfectiousDiseaseScenario infectiousDiseaseScenarioWithHighR0 = tutorialChapter9.setR0(
+				infectiousDiseaseScenario, 1.7);
 
 		TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample tutorialChapter8 = new TutorialChapter8_RunSimulationWithVaccinationControlStrategyExample();
-		RunSimulationMessage runSimulationMessageWithNormalR0 = tutorialChapter9.chapter8Config.getRunSimulationMessage();
+		RunSimulationMessage runSimulationMessageWithNormalR0 = tutorialChapter9.chapter8Config
+				.getRunSimulationMessage();
 		RunAndSoftwareIdentification normalR0runAndSoftwareId = tutorialChapter8
 				.runSimulationAndDisplayResults(runSimulationMessageWithNormalR0);
 
-		RunSimulationMessage runSimulationMessageWithHighR0 = tutorialChapter9.chapter8Config.getRunSimulationMessage();
+		RunSimulationMessage runSimulationMessageWithHighR0 = tutorialChapter9.chapter8Config
+				.getRunSimulationMessage();
 		runSimulationMessageWithHighR0.setInfectiousDiseaseScenario(infectiousDiseaseScenarioWithHighR0);
 		RunAndSoftwareIdentification highR0RunAndSoftwareId = tutorialChapter8
 				.runSimulationAndDisplayResults(runSimulationMessageWithHighR0);
 
-		tutorialChapter8.createIncidenceVisualizationForMultipleSimulations(TutorialChapter2_ExampleConfig.getAuthentication(), normalR0runAndSoftwareId.getRunId(),
-				highR0RunAndSoftwareId.getRunId());
+		tutorialChapter8.createIncidenceVisualizationForMultipleSimulations(ExampleRunSimulationMessageFactory
+				.getAuthentication(ExampleRunSimulationMessageFactory.REQUESTER_ID,
+						ExampleRunSimulationMessageFactory.REQUESTER_PASSWORD), normalR0runAndSoftwareId
+				.getRunId(), highR0RunAndSoftwareId.getRunId());
 	}
 
 }
