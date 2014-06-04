a2686bb17f0654105c3a6011978e56222ee22035
shawntbrown@gmail.com <shawntbrown@gmail.com@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2014-06-04 17:31:44 +0000

    Updated service to the 2.0.1 Apollo XSD and improved logging functionality.  The code now has a dependency on sqlite.

--- apollo-ws/epidemic-simulator-services/psc-simulator-service/trunk/SimulatorService.py
+++ apollo-ws/epidemic-simulator-services/psc-simulator-service/trunk/SimulatorService.py
@@ -12,7 +12,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
 # License for the specific language governing permissions and limitations under
 # the License.
-from SimulatorService_v2_0_services import runSimulationRequest
+from SimulatorService_v2_0_1_services import runSimulationRequest
 '''
 Created on Nov 27, 2012
 
@@ -22,12 +22,12 @@ This is an example GenericEpidemicModelService implementation.
 '''
 
 from ZSI.wstools import logging
-from SimulatorService_v2_0_services_server import SimulatorService_v2_0
+from SimulatorService_v2_0_1_services_server import SimulatorService_v2_0_1
 from ZSI.ServiceContainer import AsServer
-from SimulatorService_v2_0_services_types import *
+from SimulatorService_v2_0_1_services_types import *
 from ApolloFactory import *
 from ApolloUtils import *
-from SimulatorService_v2_0_services import *
+from SimulatorService_v2_0_1_services import *
 import os,sys
 import shutil
 import random
@@ -36,163 +36,175 @@ import paramiko
 import simWS
 from logger import Log
 from commission import SSHConn
-from TranslatorService_v2_0_services import *
-import urllib2
+from statusDB import StatusDB
+from apollo import ApolloDB
+
+#from TranslatorService_v2_0_1_services import *
+#import urllib2
 
 connections = {} 
+statusDB = StatusDB()
+class SimulatorWebService(SimulatorService_v2_0_1):
+    _wsdl = "".join(open("simulatorservice201.wsdl").readlines())
+        
+    factory = ApolloFactory()
+    utils = ApolloUtils()
+    logger = Log(simWS.configuration['local']['logFile'])
+    logger.start()
 
-class SimulatorWebService(SimulatorService_v2_0):
-        _wsdl = "".join(open("simulatorservice20.wsdl").readlines())
-        
-        factory = ApolloFactory()
-        utils = ApolloUtils()
-	logger = Log(simWS.configuration['local']['logFile'])
-        logger.start()
-	
-        #def soap_getSupportedLocations(self, ps, **kw):
-        #    response = SimulatorService_v2_0.soap_getSupportedLocations(self, ps, **kw)
-        #    
-        #    spl = self.factory.new_SupportedPopulationLocation()
-        #    
-        #    spl._populationLocation = "42003"
-        #    spl._populationLocationDescription = "Allegheny County"
-        #    
-        #    response._supportedPopluationLocations.append(spl)
-        #    return response
-        
-        #this method checks on the status of a job, indicated by the jobs runId
-        def soap_getRunStatus(self, ps, **kw):
-            response = SimulatorService_v2_0.soap_getRunStatus(self, ps, **kw)
+    #statusDB = StatusDB()
+
+   #this method checks on the status of a job, indicated by the jobs runId
+    def soap_getRunStatus(self, ps, **kw):
+        response = SimulatorService_v2_0_1.soap_getRunStatus(self, ps, **kw)
             #get the run id of the job 
-            request = ps.Parse(getRunStatusRequest.typecode)
-            runId = request._runId
-	    print "Looking up run status of %s"%runId
-	    ### First, check the log file to see if this status has been logged
-	    logs = {}
-	    with open("./service_log.txt","rb") as f:
-		for line in f:
-		    lineSplit = line.strip().split(":")
-		    logs[lineSplit[0]] = lineSplit[1]
+        request = ps.Parse(getRunStatusRequest.typecode)
+        runId = str(request._runId)
+        print "Looking up run status of %s"%runId
+	status,message = statusDB.getEntry(runId)
+        print "Status for %s is %s"%(runId,status)
+	goCheckList = ["queued","running","unknown","logfilewritten"]
+        errorList = ['LOGERROR']
+        	
+        ### First, check the log file to see if this status has been logged
+        if status in goCheckList:
+            print "checking "+ str(connections.keys())
+            if str(request._runId) not in connections.keys():
+                status = "failed"
+                message = "Simulation Service doesn't know anything about runId %s"%str(request._runId)
+                statusDB.updateEntry(runId,status,message)
+            else:
+		pbsId = statusDB.getPBSId(runId)
+                status, message = connections[runId].getStatus(runId,pbsId)
+                print "Parsing status of %s"%status
+                if status in errorList: status = 'failed'
+                statusDB.updateEntry(runId,status.lower(),message)
 
-	    if runId in logs.keys():
-		status = logs[runId]
-                message = "This was read from the service cache"
-		print "Returning " + str(status) + " " + message
-            else:	   	    
-		if request._runId not in connections.keys():
-			status = "failed"
-			message = "Simulation Service doesn't know anything about runId %s"%str(request._runId)
-		else:
-                	status, message = connections[request._runId].getStatus(runId)
-			if status == "COMPLETED" or status == "FAILED":
-				with open('./service_log.txt','ab') as f:
-					f.write('%s:%s\n'%(runId,status))
-
-            response._runStatus = self.factory.new_MethodCallStatus()
-            response._runStatus._status = status.lower()
-            response._runStatus._message = message
+        response._runStatus = self.factory.new_MethodCallStatus()
+        response._runStatus._status = status.lower()
+        response._runStatus._message = message
             
+        return response
+
+        # this method runs an epidemic model
+    def soap_runSimulation(self, ps, **kw):
+        try:
+            response = SimulatorService_v2_0_1.soap_runSimulation(self, ps, **kw)
+            request = ps.Parse(runSimulationRequest.typecode)
+            runId = request._simulationRunId
+            statusDB.updateEntry(runId,'staging')
+            self.logger.update("SVC_APL_RESQ_RECV")
+        except Exception as e:
+            self.logger.update("SVC_APL_RESQ_RECV_FAILED", message="%s" % str(e))
+            #response._runId = self.logger.pollStatusCode()
+            response._message = self.logger.pollStatus()[1]
             return response
-
-        #this method runs an epidemic model
-        def soap_runSimulation(self, ps, **kw):
-            try:
-                response = SimulatorService_v2_0.soap_runSimulation(self, ps, **kw)
-                request = ps.Parse(runSimulationRequest.typecode)
-                self.logger.update("SVC_APL_RESQ_RECV")
-            except Exception as e:
-                self.logger.update("SVC_APL_RESQ_RECV_FAILED",message="%s"%str(e))
-                response._runId = self.logger.pollStatusCode()
-                response._message = self.logger.pollStatus()[1]
-                return response
-            #extract the epidemic model input object from the request
-	    
-	    ## Parse and Translate the Apollo message First
-            try:
-                cfg = request._runSimulationMessage
-                transService = TranslatorService_v2_0Locator().getTranslatorServiceEI(url="http://research.rods.pitt.edu"+\
-                                                                                        "/translatorservice2.0/services/"+\
-                                                                                        "translatorservice")	    
-                transReq = translateRunSimulationMessage()
-                transReq._runSimulationMessage = cfg
-                transResp = transService.translateRunSimulationMessage(transReq)
-            
-            
-                idPrefix = cfg._simulatorIdentification._softwareDeveloper +\
+ 
+        # Parse and Translate the Apollo message First
+        try:
+            cfg = request._runSimulationMessage
+            idPrefix = cfg._simulatorIdentification._softwareDeveloper +\
                        "_" + cfg._simulatorIdentification._softwareName +\
                        "_" + cfg._simulatorIdentification._softwareVersion + "_"
-                
-                self.logger.update("SVC_APL_TRANS_RECV",message="%s"%str(idPrefix))
-            except Exception as e:
-                self.logger.update("SVC_APL_TRANS_FAILED",message="%s"%str(e))
-                response._runId = self.logger.pollStatusCode()
-                response._message = self.logger.pollStatus()[1]
-                return response
+                 
             
-            try:
-                confId = simWS.configuration['simulators']['mappings'][idPrefix]
-		simConf = simWS.configuration['simulators'][confId]
-                conn = SSHConn(self.logger,machineName_=simConf['defaultMachine'][0])
-                self.logger.update("SRV_SSH_CONN_SUCCESS",message="%s"%str(idPrefix))
-            except Exception as e:
-                self.logger.update("SVC_SSH_CONN_FAILED",message="%s"%str(e))
-                response._runId = self.logger.pollStatusCode()
-                response._message = self.logger.pollStatus()[1]
-                return response
-            
-            ### Make a random directory name so that multiple calls can be made
-            
-            try:
-                    randID = random.randint(0,100000)
-                    tempDirName = "%s/fred.tmp.%d"%(simWS.configuration["local"]["scratchDir"],randID)
-                    os.mkdir(tempDirName)
-                    self.logger.update("SVC_TMPDIR_SUCCESS",message="%s"%tempDirName)
-            except Exception as e:
-                    self.logger.update("SVC_TMPDIR_FAILED",message="%s"%str(e))
-                    #print("Cannot make temporary simulation directory %s"%(tempDirName))
-                    response._runId = self.logger.pollStatusCode()
-                    response._message = self.logger.pollStatus()[1]
-                    return response
-
-	    ### Get a list of the files
-	    try:
-		    fileList = [x for x in urllib2.urlopen(transResp._methodCallStatus._message).read().split('\n') if x != '']
-		    print str(fileList)
-		    fileDict = {}
-		    for fileName in fileList:
-			    fileDict[fileName] = urllib2.urlopen(transResp._methodCallStatus._message + "/" + fileName).read()
-
-		    self.logger.update("SVC_FILELIST_RECIEVED")
-	    except Exception as e:
-		    self.logger.update("SVC_FILELIST_FAILED",message="%s"%str(e))
-		    response._runId = self.logger.pollStatusCode()
-		    response._message = self.logger.pollStatus()[1]
-		    return response
-        
-	    for fileName,content in fileDict.items():
-		    with open(tempDirName+"/"+fileName,"wb") as f:
-			    f.write("%s"%content)
-        
-	    remoteScr = 'fred.tmp.%s'%str(randID)
-            conn._mkdir(remoteScr)
-
-	    for fileName,content in fileDict.items():
-		    conn.sendFile(tempDirName + "/" + fileName, remoteScr + "/" + fileName)
-	    
-	    ### we do not need temp directory anymore
-	    shutil.rmtree(tempDirName)
-	    
-
-	    returnVal = conn.submitJob(randID,idPrefix,idPrefix)
-            #print returnVal
-
-	    ### parse the run ID
-	    response._runId = idPrefix + str(returnVal)      
-	    
-	    ### Add this connection to the dictionary of connections
-	    connections[response._runId] = conn
+            self.logger.update("SVC_APL_TRANS_RECV",message="%s"%str(idPrefix))
+        except Exception as e:
+            self.logger.update("SVC_APL_TRANS_FAILED",message="%s"%str(e))
+            response._runId = self.logger.pollStatusCode()
+            response._message = self.logger.pollStatus()[1]
+            statusDB.updateEntry(runId,"ERROR",str(self.logger.pollStatusCode()) + ":"\
+                                 + str(self.logger.pollStatus()[1]))
             return response
 
+        # Create the current connector
+        
+        try:
+            confId = simWS.configuration['simulators']['mappings'][idPrefix]
+            simConf = simWS.configuration['simulators'][confId]
+            conn = SSHConn(self.logger,machineName_=simConf['defaultMachine'][0])
+            connections[str(runId)] = conn
+            self.logger.update("SRV_SSH_CONN_SUCCESS",message="%s"%str(idPrefix))
+        except Exception as e:
+            self.logger.update("SVC_SSH_CONN_FAILED",message="%s"%str(e))
+            response._runId = self.logger.pollStatusCode()
+            response._message = self.logger.pollStatus()[1]
+            return response
+
+        
+        # Make a random directory name so that multiple calls can be made
+             
+        try:
+            randID = random.randint(0,100000)
+            tempDirName = "%s/fred.tmp.%d"%(simWS.configuration["local"]["scratchDir"],randID)
+            os.mkdir(tempDirName)
+            self.logger.update("SVC_TMPDIR_SUCCESS",message="%s"%tempDirName)
+        except Exception as e:
+            self.logger.update("SVC_TMPDIR_FAILED",message="%s"%str(e))
+            #print("Cannot make temporary simulation directory %s"%(tempDirName))
+            #response._runId = self.logger.pollStatusCode()
+            response._message = self.logger.pollStatus()[1]
+            return response
+
+        # Get the files from the Apollo DB
+        try:
+            apolloDB = ApolloDB(logger_=self.logger)
+            apolloDB.connect()
+            translatorServiceId = apolloDB.getTranslatorServiceKey()
+            simulatorServiceId = apolloDB.getSimulatorServiceKey(cfg._simulatorIdentification._softwareDeveloper,
+                                                                 cfg._simulatorIdentification._softwareName,
+                                                                 cfg._simulatorIdentification._softwareVersion)
+
+            
+            simulatorInputFileDict = apolloDB.getSimulationInputFilesForRunId(runId,translatorServiceId,simulatorServiceId)
+            apolloDB.close()
+            for fileName,content in simulatorInputFileDict.items():
+                print fileName
+                with open(tempDirName+"/"+fileName,"wb") as f:
+                    f.write("%s"%content)
+
+            self.logger.update("SVC_FILELIST_RECIEVED")
+        except Exception as e:
+            self.logger.update("SVC_FILELIST_FAILED",message="%s"%str(e))
+            response._runId = self.logger.pollStatusCode()
+            response._message = self.logger.pollStatus()[1]
+            return response
+
+        try:
+            remoteScr = 'fred.tmp.%s'%str(randID)
+            conn._mkdir(remoteScr)
+ 
+            for fileName,content in simulatorInputFileDict.items():
+                conn.sendFile(tempDirName + "/" + fileName, remoteScr + "/" + fileName)
+
+            ### we do not need temp directory anymore
+            shutil.rmtree(tempDirName)
+            
+            self.logger.update("SVC_FILE_SEND_SUCCESS",message="%s"%tempDirName)
+        except Exception as e:
+            self.logger.update("SVC_FILE_SEND_FAILED",message="%s"%e)
+            response._runId = self.logger.pollStatusCode()
+            response._message = self.logger.pollStatus()[1]
+            return response
+
+        try:
+            (jobType,returnVal) = conn.submitJob(randID,runId,idPrefix)
+	    print "JobType = %s returnVal = %s"%(jobType,str(returnVal))
+	    if jobType == "pbs":
+		statusDB.updatePBSId(runId,"pbs",returnVal) 
+            statusDB.updateEntry(runId,'queued',"%s"%returnVal)
+            self.logger.update("SVC_SUBMIT_JOB_SUCCESS",message="%s"%returnVal)
+        except Exception as e:
+            self.logger.update("SVC_SUBMIT_JOB_FAILED",message="%s"%str(e))
+            response._runId = self.logger.pollStatusCode()
+            response._message = self.logger.pollStatus()[1]
+            return response
+
+        
+        response._runId = str(idPrefix) + str(runId)
+        response._message = "%s has successfully been processed by runSimulation"%str(runId)
+        return response
+
 #run a webserver on 8087
-AsServer(port=8094, services=[SimulatorWebService('fred'), ])
+AsServer(port=8045, services=[SimulatorWebService('fred'), ])
 
