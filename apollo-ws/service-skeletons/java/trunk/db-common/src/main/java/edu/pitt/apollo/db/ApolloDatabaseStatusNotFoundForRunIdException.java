3b0f1f27b059d52de13fefd928514c0efd75d3ff
nickmillett <nem41@pitt.edu>
2014-07-08 15:39:28 +0000

    Implemented getRunStatus in the Apollo broker added special exception to ApolloDbUtils to indicate no status was found for a given run

---
+++ apollo-ws/service-skeletons/java/trunk/db-common/src/main/java/edu/pitt/apollo/db/ApolloDatabaseStatusNotFoundForRunIdException.java
@@ -0,0 +1,17 @@
+package edu.pitt.apollo.db;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Jul 8, 2014
+ * Time: 11:26:49 AM
+ * Class: ApolloDatabaseStatusNotFoundForRunIdException
+ */
+public class ApolloDatabaseStatusNotFoundForRunIdException extends ApolloDatabaseException {
+
+    public ApolloDatabaseStatusNotFoundForRunIdException(String message) {
+        super(message);
+    }
+    
+}
