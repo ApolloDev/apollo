51f6a9d79adc64d6a6c5768cff774745532084e2
shawntbrown <shawntbrown@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2014-07-28 17:08:42 +0000

    Updated the Simulation Services to conform to the 2.0.2 apollo XSD and made the following improvement:
    1. PBS running now catches all errors from the remote simulation through manipulation of the PBS stderr files.
    2. The status of a run is now checked through pinging the Apollo Database.
    3. The Apollo database module will now be deployed separately.  There is a value in simWS.configuration['local'] to signify the location of this module.
    4. The software now uses the latest version of ZSI which is 2.1_a1 and the depends has been updated as such.
    5. Removed lots of extraneous printing.
    6. The SQLite statusDB has been deprecated and removed.

--- apollo-ws/epidemic-simulator-services/psc-simulator-service/trunk/gen.sh
+++ apollo-ws/epidemic-simulator-services/psc-simulator-service/trunk/gen.sh
@@ -1,3 +1,1 @@
-wsdl2py --complexType --lazy -f simulatorservice201.wsdl
-wsdl2dispatch -f simulatorservice201.wsdl
-ln -sf SimulatorService.py SimulatorService-2.0.1.py
+wsdl2py --complexType --lazy simulatorservice202.wsdl
