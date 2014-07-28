8c414504b28b52ca6716b66eb014fbec6ea6af7f
shawntbrown <shawntbrown@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2014-07-28 17:54:11 +0000

    This updates the Visualization Services to conform to the Apollo XSD 2.0.2 and makes the following improvements:
    1. Status of the visualizer service is now handled through the Apollo Database
    2. The apollo module is no longer a part of this distribution, it is a separate one, and there is a flag in the configuration to describe where it is at.
    3. There is now a visWS.py module to describe the local configuration, which is analogous to the SimulatorService.

---
+++ apollo-ws/visualizer-services/GAIA/trunk/visWS.py
@@ -0,0 +1,14 @@
+configuration = {
+    'local':{
+        'statusDirectory':'/home/gaia/tmp',
+        'tempDirectory':'/home/gaia/tmp',
+        'outputDirectory':'/var/www/GAIA/',
+        'URL':'http://gaia.pha.psc.edu/GAIA/',
+        'serverPort':8092,
+        'logFile':'/usr/local/packages/GAIA-Apollo-v2.0.2/gaiaService.202.log',
+        'apolloDBName':'test',
+        'wsdlFile':'/usr/local/packages/GAIA-Apollo-v2.0.2/visualizerservice202.wsdl',
+        'apolloPyDir':'/usr/local/packages/Apollo-Python'
+        }
+    }
+ 
