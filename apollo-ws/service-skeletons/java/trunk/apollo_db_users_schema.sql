ea3eb84eecc29222c044d09888aa7c3c39e47494
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2014-07-30 14:27:13 +0000

    changed library service wsdl location in LibraryMethod added authentication/authorization support

--- apollo-ws/service-skeletons/java/trunk/apollo_db_users_schema.sql
+++ apollo-ws/service-skeletons/java/trunk/apollo_db_users_schema.sql
@@ -6,8 +6,9 @@ DROP TABLE IF EXISTS role_description;
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
   requester_id VARCHAR(255) NOT NULL,
-  requester_password VARCHAR(255) NOT NULL,
-  requester_email VARCHAR(255) NOT NULL,
+  hash_of_user_password_and_salt VARCHAR(255) NOT NULL,
+  salt VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
   PRIMARY KEY(id)
 );
 
@@ -18,15 +19,24 @@ CREATE TABLE roles (
   PRIMARY KEY(id)
 );
 
+INSERT INTO roles VALUES (1, 'Can run SEIR without privileged request');
+INSERT INTO roles VALUES (2, 'Can run SEIR with privileged request');
+INSERT INTO roles VALUES (3, 'Can run FRED without privileged request');
+INSERT INTO roles VALUES(4, 'Can run FRED with privileged request');
+
 
 CREATE TABLE role_description (
   role_id INT REFERENCES roles(id),
   software_id INT REFERENCES software_identification(id),
-  can_run_simulator BIT,
+  can_run_software BIT,
   allow_privileged_request BIT,
   CONSTRAINT role_description UNIQUE(role_id, software_id)
 );
 
+INSERT INTO role_description VALUES(1, 2, 1, 0);
+INSERT INTO role_description VALUES(2, 2, 1, 1);
+INSERT INTO role_description VALUES(3, 3, 1, 0);
+INSERT INTO role_description VALUES(4, 3, 1, 1);
 
 CREATE TABLE user_roles (
   user_id INT NOT NULL REFERENCES users(id),
