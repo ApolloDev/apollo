01cb210d9c1849d1e3a905ea2a5e91a3fc0626e6
nickmillett <nem41@pitt.edu>
2014-07-30 14:27:13 +0000

    changed library service wsdl location in LibraryMethod added authentication/authorization support

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunMethod.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunMethod.java
@@ -1,10 +1,15 @@
 package edu.pitt.apollo.apolloservice.methods.run;
 
+import edu.pitt.apollo.apolloservice.database.DatabaseAccessor;
+import edu.pitt.apollo.apolloservice.error.ApolloServiceErrorHandler;
 import java.math.BigInteger;
 
 import edu.pitt.apollo.db.ApolloDatabaseException;
+import edu.pitt.apollo.types.v2_0_2.Authentication;
 import edu.pitt.apollo.types.v2_0_2.MethodCallStatus;
 import edu.pitt.apollo.types.v2_0_2.MethodCallStatusEnum;
+import edu.pitt.apollo.types.v2_0_2.SoftwareIdentification;
+import java.io.IOException;
 
 /**
  *
@@ -17,14 +22,67 @@ import edu.pitt.apollo.types.v2_0_2.MethodCallStatusEnum;
  */
 public abstract class RunMethod {
 
-    protected static boolean isRunFailed(BigInteger runId) throws ApolloDatabaseException {
-        MethodCallStatus status = GetRunStatusMethod.getRunStatus(runId);
-        MethodCallStatusEnum statusEnum = status.getStatus();
+	private final Authentication authentication;
+	private final SoftwareIdentification softwareIdentification;
+	protected DatabaseAccessor databaseAccessor;
+	
+	public RunMethod(Authentication authentication, SoftwareIdentification softwareIdentification) {
+		this.authentication = authentication;
+		this.softwareIdentification = softwareIdentification;
+	}
 
-        if (statusEnum.equals(MethodCallStatusEnum.FAILED)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	protected static final boolean isRunFailed(BigInteger runId) throws ApolloDatabaseException {
+		MethodCallStatus status = GetRunStatusMethod.getRunStatus(runId);
+		MethodCallStatusEnum statusEnum = status.getStatus();
+
+		return statusEnum.equals(MethodCallStatusEnum.FAILED);
+	}
+
+	protected final BigInteger userAuthenticated(DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
+		boolean userAuthenticated = dbAccessor.authenticateUser(authentication);
+		if (!userAuthenticated) {
+			try {
+				long runId = ApolloServiceErrorHandler.writeErrorWithErrorId("You are not registered to use Apollo Web Services.");
+				return new BigInteger(Long.toString(runId));
+			} catch (IOException e) {
+				System.err.println("IOException writing error file: "
+					+ e.getMessage());
+				return ApolloServiceErrorHandler.FATAL_ERROR_CODE;
+			}
+		}
+
+		return BigInteger.ZERO;
+	}
+
+	protected final BigInteger userAuthorizedForCachedResults(DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
+		boolean userAuthorized = dbAccessor.authorizeUserForSoftwareCacheData(authentication, softwareIdentification);
+		if (!userAuthorized) {
+			try {
+				long runId = ApolloServiceErrorHandler.writeErrorWithErrorId("You are not authorized to use the specified software.");
+				return new BigInteger(Long.toString(runId));
+			} catch (IOException e) {
+				System.err.println("IOException writing error file: "
+					+ e.getMessage());
+				return ApolloServiceErrorHandler.FATAL_ERROR_CODE;
+			}
+		}
+
+		return BigInteger.ZERO;
+	}
+
+	protected final BigInteger userAuthorizedToRunSoftware(DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
+		boolean userAuthorized = dbAccessor.authorizeUserForRunningSoftware(authentication, softwareIdentification);
+		if (!userAuthorized) {
+			try {
+				long runId = ApolloServiceErrorHandler.writeErrorWithErrorId("You are not authorized to run the specified software, only view cached results.");
+				return new BigInteger(Long.toString(runId));
+			} catch (IOException e) {
+				System.err.println("IOException writing error file: "
+					+ e.getMessage());
+				return ApolloServiceErrorHandler.FATAL_ERROR_CODE;
+			}
+		}
+
+		return BigInteger.ZERO;
+	}
 }
