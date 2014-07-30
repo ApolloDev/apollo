a391a4f160c04dfe79ff61296538470631ffeecb
john.d.levander@gmail.com <jdl50@pitt.edu>
2014-07-30 17:01:47 +0000

    Changed POMS so that we can use the maven release plugin to change version numbers Updated the broker WSdl to return a "RunResult" from calls to runSimulaton and runVisualization

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunMethod.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunMethod.java
@@ -12,77 +12,52 @@ import edu.pitt.apollo.types.v2_0_2.SoftwareIdentification;
 import java.io.IOException;
 
 /**
- *
- * Author: Nick Millett
- * Email: nick.millett@gmail.com
- * Date: May 8, 2014
- * Time: 11:52:26 AM
- * Class: RunMethod
- * IDE: NetBeans 6.9.1
+ * 
+ * Author: Nick Millett Email: nick.millett@gmail.com Date: May 8, 2014 Time:
+ * 11:52:26 AM Class: RunMethod IDE: NetBeans 6.9.1
  */
 public abstract class RunMethod {
 
 	private final Authentication authentication;
 	private final SoftwareIdentification softwareIdentification;
 	protected DatabaseAccessor databaseAccessor;
-	
-	public RunMethod(Authentication authentication, SoftwareIdentification softwareIdentification) {
+
+	public RunMethod(Authentication authentication,
+			SoftwareIdentification softwareIdentification) {
 		this.authentication = authentication;
 		this.softwareIdentification = softwareIdentification;
 	}
 
-	protected static final boolean isRunFailed(BigInteger runId) throws ApolloDatabaseException {
+	protected static final boolean isRunFailed(BigInteger runId)
+			throws ApolloDatabaseException {
 		MethodCallStatus status = GetRunStatusMethod.getRunStatus(runId);
 		MethodCallStatusEnum statusEnum = status.getStatus();
 
 		return statusEnum.equals(MethodCallStatusEnum.FAILED);
 	}
 
-	protected final BigInteger userAuthenticated(DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
-		boolean userAuthenticated = dbAccessor.authenticateUser(authentication);
-		if (!userAuthenticated) {
-			try {
-				long runId = ApolloServiceErrorHandler.writeErrorWithErrorId("You are not registered to use Apollo Web Services.");
-				return new BigInteger(Long.toString(runId));
-			} catch (IOException e) {
-				System.err.println("IOException writing error file: "
-					+ e.getMessage());
-				return ApolloServiceErrorHandler.FATAL_ERROR_CODE;
-			}
-		}
-
-		return BigInteger.ZERO;
+	protected final boolean authenticateUser(DatabaseAccessor dbAccessor)
+			throws ApolloDatabaseException {
+		boolean userSuccessfulyAuthenticated = dbAccessor
+				.authenticateUser(authentication);
+		return userSuccessfulyAuthenticated;
 	}
 
-	protected final BigInteger userAuthorizedForCachedResults(DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
-		boolean userAuthorized = dbAccessor.authorizeUserForSoftwareCacheData(authentication, softwareIdentification);
-		if (!userAuthorized) {
-			try {
-				long runId = ApolloServiceErrorHandler.writeErrorWithErrorId("You are not authorized to use the specified software.");
-				return new BigInteger(Long.toString(runId));
-			} catch (IOException e) {
-				System.err.println("IOException writing error file: "
-					+ e.getMessage());
-				return ApolloServiceErrorHandler.FATAL_ERROR_CODE;
-			}
-		}
+	protected final boolean userAuthorizedForCachedResults(
+			DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
+		boolean userAuthorizedForCachedResults = dbAccessor
+				.authorizeUserForSoftwareCacheData(authentication,
+						softwareIdentification);
+		return userAuthorizedForCachedResults;
 
-		return BigInteger.ZERO;
 	}
 
-	protected final BigInteger userAuthorizedToRunSoftware(DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
-		boolean userAuthorized = dbAccessor.authorizeUserForRunningSoftware(authentication, softwareIdentification);
-		if (!userAuthorized) {
-			try {
-				long runId = ApolloServiceErrorHandler.writeErrorWithErrorId("You are not authorized to run the specified software, only view cached results.");
-				return new BigInteger(Long.toString(runId));
-			} catch (IOException e) {
-				System.err.println("IOException writing error file: "
-					+ e.getMessage());
-				return ApolloServiceErrorHandler.FATAL_ERROR_CODE;
-			}
-		}
+	protected final boolean userAuthorizedToRunSoftware(
+			DatabaseAccessor dbAccessor) throws ApolloDatabaseException {
+		boolean userAuthorizedToRunSoftware = dbAccessor.authorizeUserForRunningSoftware(
+				authentication, softwareIdentification);
+		return userAuthorizedToRunSoftware;
+		
 
-		return BigInteger.ZERO;
 	}
 }
