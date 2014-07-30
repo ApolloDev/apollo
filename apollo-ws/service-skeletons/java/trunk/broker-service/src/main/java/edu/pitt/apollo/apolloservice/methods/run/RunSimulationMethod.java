ab81d45a97d93cc0d8b780594e1238e9f4876b9f
JohnLevander <jdl50@pitt.edu>
2014-07-30 17:01:47 +0000

    Changed POMS so that we can use the maven release plugin to change version numbers Updated the broker WSdl to return a "RunResult" from calls to runSimulaton and runVisualization

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunSimulationMethod.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunSimulationMethod.java
@@ -7,69 +7,103 @@ import edu.pitt.apollo.apolloservice.database.DatabaseAccessorForRunningSimulati
 import edu.pitt.apollo.apolloservice.error.ApolloServiceErrorHandler;
 import edu.pitt.apollo.apolloservice.thread.RunSimulationThread;
 import edu.pitt.apollo.db.ApolloDatabaseException;
+import edu.pitt.apollo.types.v2_0_2.MethodCallStatus;
+import edu.pitt.apollo.types.v2_0_2.MethodCallStatusEnum;
+import edu.pitt.apollo.types.v2_0_2.RunResult;
 import edu.pitt.apollo.types.v2_0_2.RunSimulationMessage;
 
 /**
- *
- * Author: Nick Millett
- * Email: nick.millett@gmail.com
- * Date: May 8, 2014
- * Time: 11:50:15 AM
- * Class: RunSimulationMethod
- * IDE: NetBeans 6.9.1
+ * 
+ * Author: Nick Millett Email: nick.millett@gmail.com Date: May 8, 2014 Time:
+ * 11:50:15 AM Class: RunSimulationMethod IDE: NetBeans 6.9.1
  */
 public class RunSimulationMethod extends RunMethod {
 
 	private final RunSimulationMessage runSimulationMessage;
 
 	public RunSimulationMethod(RunSimulationMessage runSimulationMessage) {
-		super(runSimulationMessage.getAuthentication(), runSimulationMessage.getSimulatorIdentification());
+		super(runSimulationMessage.getAuthentication(), runSimulationMessage
+				.getSimulatorIdentification());
 		this.runSimulationMessage = runSimulationMessage;
-		databaseAccessor = new DatabaseAccessorForRunningSimulations(runSimulationMessage);
+		databaseAccessor = new DatabaseAccessorForRunningSimulations(
+				runSimulationMessage);
 	}
 
-	public BigInteger runSimulation() {
+	private RunResult getUnauthorizedSoftwareResult() {
+		RunResult runResult = new RunResult();
+		runResult.setRunId(ApolloServiceErrorHandler.FATAL_ERROR_CODE);
+		runResult.getMethodCallStatus().setStatus(
+				MethodCallStatusEnum.AUTHENTICATION_FAILURE);
+		runResult.getMethodCallStatus().setMessage(
+				"You are not authorized to view results for the "
+						+ runSimulationMessage.getSimulatorIdentification()
+								.getSoftwareName()
+						+ " "
+						+ runSimulationMessage.getSimulatorIdentification()
+								.getSoftwareType().toString().toLowerCase()
+						+ ".");
+		return runResult;
+	}
 
+	public RunResult runSimulation() {
+		RunResult runResult = new RunResult();
+		MethodCallStatus methodCallStatus = new MethodCallStatus();
+		runResult.setMethodCallStatus(methodCallStatus);
 		try {
-			BigInteger userAuthenticated = userAuthenticated(databaseAccessor);
-			if (userAuthenticated.intValue() != 0) {
-				return userAuthenticated;
+			boolean userSuccessfullyAuthenticated = authenticateUser(databaseAccessor);
+
+			if (!userSuccessfullyAuthenticated) {
+				runResult.setRunId(ApolloServiceErrorHandler.FATAL_ERROR_CODE);
+				runResult.getMethodCallStatus().setStatus(
+						MethodCallStatusEnum.AUTHENTICATION_FAILURE);
+				runResult.getMethodCallStatus().setMessage(
+						"Invalid username or password.");
+				return runResult;
 			}
 
-			BigInteger userAuthorizedForCachedRun = userAuthorizedForCachedResults(databaseAccessor);
-			if (userAuthorizedForCachedRun.intValue() != 0) {
-				return userAuthorizedForCachedRun;
+			boolean userAuthorizedForCachedRun = userAuthorizedForCachedResults(databaseAccessor);
+			if (!userAuthorizedForCachedRun) {
+				return getUnauthorizedSoftwareResult();
 			}
 
-			BigInteger runId = databaseAccessor.getCachedRunIdFromDatabaseOrNull();
+			BigInteger runId = databaseAccessor
+					.getCachedRunIdFromDatabaseOrNull();
 			if (runId != null) {
 				if (!isRunFailed(runId)) {
-					return runId;
+					runResult.setRunId(runId);
+					runResult.getMethodCallStatus().setStatus(
+							MethodCallStatusEnum.CALLED_SIMULATOR);
+					runResult.getMethodCallStatus().setMessage(
+							"This run is already running!");
+					return runResult;
 				}
 			}
 
-			BigInteger userAuthorizedToRunSoftware = userAuthorizedToRunSoftware(databaseAccessor);
-			if (userAuthorizedToRunSoftware.intValue() != 0) {
-				return userAuthorizedToRunSoftware;
+			boolean userAuthorizedToRunSoftware = userAuthorizedToRunSoftware(databaseAccessor);
+			if (!userAuthorizedToRunSoftware) {
+				return getUnauthorizedSoftwareResult();
 			}
 
 			if (isRunFailed(runId)) {
 				databaseAccessor.removeAllDataAssociatedWithRunId(runId);
 			}
 			runId = databaseAccessor.insertRunIntoDatabase();
+			runResult.setRunId(runId);
+			runResult.getMethodCallStatus().setStatus(
+					MethodCallStatusEnum.STAGING);
+			runResult.getMethodCallStatus().setMessage(
+					"Apollo Broker is handling the run request.");
 
 			new RunSimulationThread(runId, runSimulationMessage).start();
 
-			return runId;
+			return runResult;
 		} catch (ApolloDatabaseException ex) {
-			try {
-				long runId = ApolloServiceErrorHandler.writeErrorWithErrorId(ex.getMessage());
-				return new BigInteger(Long.toString(runId));
-			} catch (IOException e) {
-				System.err.println("IOException writing error file: "
-					+ e.getMessage());
-				return ApolloServiceErrorHandler.FATAL_ERROR_CODE;
-			}
+
+			runResult.setRunId(ApolloServiceErrorHandler.FATAL_ERROR_CODE);
+			runResult.getMethodCallStatus().setMessage(ex.getMessage());
+			runResult.getMethodCallStatus().setStatus(
+					MethodCallStatusEnum.FAILED);
+			return runResult;
 		}
 	}
 }
