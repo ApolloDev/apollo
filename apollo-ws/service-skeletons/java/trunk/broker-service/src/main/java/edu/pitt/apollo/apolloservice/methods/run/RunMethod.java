063744f40d6ac3a6fe972c28ddb7ee44f087a733
nickmillett <nem41@pitt.edu>
2014-08-01 14:14:20 +0000

    removed apollo_db_users_schema.sql and merged it with the apollo_db_schema.sql combined the run simulation and run visualization methods into a single run method implemented authentication and authorization checking when running a simulation or visualization

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunMethod.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/run/RunMethod.java
@@ -1,31 +1,60 @@
 package edu.pitt.apollo.apolloservice.methods.run;
 
 import edu.pitt.apollo.apolloservice.database.DatabaseAccessor;
+import edu.pitt.apollo.apolloservice.database.DatabaseAccessorFactory;
 import edu.pitt.apollo.apolloservice.error.ApolloServiceErrorHandler;
+import edu.pitt.apollo.apolloservice.exception.UnrecognizedMessageTypeException;
+import edu.pitt.apollo.apolloservice.thread.RunApolloServiceThread;
+import edu.pitt.apollo.apolloservice.thread.RunApolloServiceThreadFactory;
 import java.math.BigInteger;
 
 import edu.pitt.apollo.db.ApolloDatabaseException;
 import edu.pitt.apollo.types.v2_0_2.Authentication;
 import edu.pitt.apollo.types.v2_0_2.MethodCallStatus;
 import edu.pitt.apollo.types.v2_0_2.MethodCallStatusEnum;
+import edu.pitt.apollo.types.v2_0_2.RunResult;
 import edu.pitt.apollo.types.v2_0_2.SoftwareIdentification;
-import java.io.IOException;
 
 /**
  * 
  * Author: Nick Millett Email: nick.millett@gmail.com Date: May 8, 2014 Time:
  * 11:52:26 AM Class: RunMethod IDE: NetBeans 6.9.1
  */
-public abstract class RunMethod {
+public class RunMethod {
 
 	private final Authentication authentication;
 	private final SoftwareIdentification softwareIdentification;
 	protected DatabaseAccessor databaseAccessor;
+	protected RunApolloServiceThread runApolloServiceThread;
+	private final Object message;
 
 	public RunMethod(Authentication authentication,
-			SoftwareIdentification softwareIdentification) {
-		this.authentication = authentication;
+			SoftwareIdentification softwareIdentification, Object message) {
+
+		this.authentication = new Authentication();
+		this.authentication.setRequesterId(authentication.getRequesterId());
+		this.authentication.setRequesterPassword(authentication.getRequesterPassword());
+
 		this.softwareIdentification = softwareIdentification;
+		this.message = message;
+	}
+
+	protected final RunResult getUnauthorizedSoftwareResult() {
+		RunResult runResult = new RunResult();
+		MethodCallStatus methodCallStatus = new MethodCallStatus();
+		runResult.setMethodCallStatus(methodCallStatus);
+
+		runResult.setRunId(ApolloServiceErrorHandler.FATAL_ERROR_CODE);
+		runResult.getMethodCallStatus().setStatus(
+				MethodCallStatusEnum.AUTHENTICATION_FAILURE);
+		runResult.getMethodCallStatus().setMessage(
+				"You are not authorized to view results for the "
+				+ softwareIdentification.getSoftwareName()
+				+ " "
+				+ softwareIdentification
+				.getSoftwareType().toString().toLowerCase()
+				+ ".");
+		return runResult;
 	}
 
 	protected static final boolean isRunFailed(BigInteger runId)
@@ -57,7 +86,87 @@ public abstract class RunMethod {
 		boolean userAuthorizedToRunSoftware = dbAccessor.authorizeUserForRunningSoftware(
 				authentication, softwareIdentification);
 		return userAuthorizedToRunSoftware;
-		
+	}
 
+	public final RunResult run() {
+		RunResult runResult = new RunResult();
+		MethodCallStatus methodCallStatus = new MethodCallStatus();
+		runResult.setMethodCallStatus(methodCallStatus);
+
+		try {
+			runApolloServiceThread = RunApolloServiceThreadFactory.getRunApolloServiceThread(message);
+			runApolloServiceThread.setAuthenticationPasswordFieldToBlank();
+			
+			databaseAccessor = DatabaseAccessorFactory.getDatabaseAccessor(message, authentication);
+		} catch (UnrecognizedMessageTypeException ex) {
+			runResult.setRunId(ApolloServiceErrorHandler.FATAL_ERROR_CODE);
+			runResult.getMethodCallStatus().setMessage(ex.getMessage());
+			runResult.getMethodCallStatus().setStatus(
+					MethodCallStatusEnum.FAILED);
+			return runResult;
+		}
+
+		try {
+			boolean userSuccessfullyAuthenticated = authenticateUser(databaseAccessor);
+
+			if (!userSuccessfullyAuthenticated) {
+				runResult.setRunId(ApolloServiceErrorHandler.FATAL_ERROR_CODE);
+				runResult.getMethodCallStatus().setStatus(
+						MethodCallStatusEnum.AUTHENTICATION_FAILURE);
+				runResult.getMethodCallStatus().setMessage(
+						"Invalid username");
+				return runResult;
+			}
+
+			boolean userAuthorizedForCachedRun = userAuthorizedForCachedResults(databaseAccessor);
+			if (!userAuthorizedForCachedRun) {
+				return getUnauthorizedSoftwareResult();
+			}
+
+			BigInteger runId = databaseAccessor
+					.getCachedRunIdFromDatabaseOrNull();
+
+			if (runId != null) {
+				if (!isRunFailed(runId)) {
+					runResult.setRunId(runId);
+					runResult.getMethodCallStatus().setStatus(
+							MethodCallStatusEnum.RUNNING);
+					runResult.getMethodCallStatus().setMessage(
+							"This run is already running!");
+					return runResult;
+				}
+
+			}
+
+			boolean userAuthorizedToRunSoftware = userAuthorizedToRunSoftware(databaseAccessor);
+			if (!userAuthorizedToRunSoftware) {
+				return getUnauthorizedSoftwareResult();
+			}
+
+			if (runId != null) {
+				if (isRunFailed(runId)) {
+					databaseAccessor.removeAllDataAssociatedWithRunId(runId);
+				}
+			}
+
+			runId = databaseAccessor.insertRunIntoDatabase();
+			runResult.setRunId(runId);
+			runResult.getMethodCallStatus().setStatus(
+					MethodCallStatusEnum.STAGING);
+			runResult.getMethodCallStatus().setMessage(
+					"Apollo Broker is handling the run request.");
+
+			runApolloServiceThread.setRunId(runId);
+			runApolloServiceThread.start();
+
+			return runResult;
+		} catch (ApolloDatabaseException ex) {
+
+			runResult.setRunId(ApolloServiceErrorHandler.FATAL_ERROR_CODE);
+			runResult.getMethodCallStatus().setMessage(ex.getMessage());
+			runResult.getMethodCallStatus().setStatus(
+					MethodCallStatusEnum.FAILED);
+			return runResult;
+		}
 	}
 }
