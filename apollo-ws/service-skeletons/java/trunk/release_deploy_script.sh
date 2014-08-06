b6ea42769d44dcd4b20c2c6651b4225ad44114e6
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2014-08-06 22:30:54 +0000

    updated poms to use maven shade plugin. this allows the creation of a jar with all dependencies included but some troublesome files excluded from META-INF updated the release deploy script to deploy the uber-jars created by the maven shade plugin instead of the jars with dependencies

--- apollo-ws/service-skeletons/java/trunk/release_deploy_script.sh
+++ apollo-ws/service-skeletons/java/trunk/release_deploy_script.sh
@@ -12,35 +12,35 @@ mvn release:prepare -DreleaseVersion=$release_version -DdevelopmentVersion="$dev
 
 mvn release:perform -Dgoals=deploy -DuseReleaseProfile=false
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=broker-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/broker-service/target/broker-service-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=broker-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/broker-service/target/broker-service-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=broker-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/broker-service-client/target/broker-service-client-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=broker-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/broker-service-client/target/broker-service-client-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=config-examples -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/config-examples/target/config-examples-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=config-examples -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/config-examples/target/config-examples-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=db-common -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/db-common/target/db-common-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=db-common -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/db-common/target/db-common-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=library-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/library-service/target/library-service-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=library-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/library-service/target/library-service-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=library-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/library-service-client/target/library-service-client-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=library-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/library-service-client/target/library-service-client-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=services-common -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/services-common/target/services-common-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=services-common -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/services-common/target/services-common-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=simulator-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/simulator-service/target/simulator-service-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=simulator-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/simulator-service/target/simulator-service-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=simulator-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/simulator-service-client/target/simulator-service-client-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=simulator-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/simulator-service-client/target/simulator-service-client-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=syntheticpopulation-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/syntheticpopulation-service/target/syntheticpopulation-service-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=syntheticpopulation-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/syntheticpopulation-service/target/syntheticpopulation-service-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=syntheticpopulation-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/syntheticpopulation-service-client/target/syntheticpopulation-service-client-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=syntheticpopulation-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/syntheticpopulation-service-client/target/syntheticpopulation-service-client-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=translator-service-skeleton -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/translator-service-skeleton/target/translator-service-skeleton-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=translator-service-skeleton -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/translator-service-skeleton/target/translator-service-skeleton-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=visualizer-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/visualizer-service/target/visualizer-service-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=visualizer-service -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/visualizer-service/target/visualizer-service-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=visualizer-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/visualizer-service-client/target/visualizer-service-client-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=visualizer-service-client -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/visualizer-service-client/target/visualizer-service-client-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
-mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=xsd-types -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/xsd-types/target/xsd-types-$release_version-jar-with-dependencies.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
+mvn deploy:deploy-file -DgroupId=edu.pitt.apollo -DartifactId=xsd-types -Dversion=$release_version -Dpackaging=jar -Dfile=./target/checkout/xsd-types/target/xsd-types-$release_version.jar -DrepositoryId=research.rods.pitt.edu -Durl=scp://research.rods.pitt.edu:/home/jdl/repo/
 
 mvn clean
 
