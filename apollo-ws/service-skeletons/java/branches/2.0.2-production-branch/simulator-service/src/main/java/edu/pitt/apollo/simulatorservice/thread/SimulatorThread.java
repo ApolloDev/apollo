f9f0ecd08ab626dc64134605f285fd5954c7eae2
nickmillett <nem41@pitt.edu>
2014-08-18 16:52:36 +0000

    updated Time-series visualizer to output newly_deceased and prophylactics_given images for the Anthrax simulator integrated latest XSD changes from trunk updated the db schema with the treatment.png image now output by the TSV improved the SimulatorThread by incorporating some repeated code from sub-classes

--- apollo-ws/service-skeletons/java/branches/2.0.2-production-branch/simulator-service/src/main/java/edu/pitt/apollo/simulatorservice/thread/SimulatorThread.java
+++ apollo-ws/service-skeletons/java/branches/2.0.2-production-branch/simulator-service/src/main/java/edu/pitt/apollo/simulatorservice/thread/SimulatorThread.java
@@ -24,227 +24,243 @@ import java.util.Map;
  */
 public abstract class SimulatorThread extends Thread {
 
-    protected RunSimulationMessage message;
-    protected final BigInteger runId;
-    private final boolean useFile;
-    private final boolean useDatabase;
-    protected final ApolloDbUtils dbUtils;
-    private final SoftwareIdentification translatorSoftwareId;
-    protected static SoftwareIdentification visualizerId;
+	protected RunSimulationMessage message;
+	protected final BigInteger runId;
+	private final boolean useFile;
+	private final boolean useDatabase;
+	protected final ApolloDbUtils dbUtils;
+	private final SoftwareIdentification translatorSoftwareId;
+	protected static SoftwareIdentification visualizerId;
 
-    public SimulatorThread(ApolloDbUtils dbUtils, BigInteger runId, SoftwareIdentification translatorSoftwareId,
-            boolean useFile, boolean useDatabase) {
-        super();
-        this.runId = runId;
-        this.useFile = useFile;
-        this.useDatabase = useDatabase;
-        this.dbUtils = dbUtils;
-        this.translatorSoftwareId = translatorSoftwareId;
+	public SimulatorThread(ApolloDbUtils dbUtils, BigInteger runId, SoftwareIdentification translatorSoftwareId,
+			boolean useFile, boolean useDatabase) {
+		super();
+		this.runId = runId;
+		this.useFile = useFile;
+		this.useDatabase = useDatabase;
+		this.dbUtils = dbUtils;
+		this.translatorSoftwareId = translatorSoftwareId;
 
-        loadRunSimulationMessage();
-    }
+		loadRunSimulationMessage();
+	}
 
-    public BigInteger getRunId() {
-        return runId;
-    }
+	public BigInteger getRunId() {
+		return runId;
+	}
 
-    protected abstract void runSimulator() throws IOException;
+	protected abstract void runSimulator() throws IOException;
 
-    protected abstract void storeFileOutputToDatabase(String location);
+	protected abstract void storeFileOutputToDatabase(String location);
 
-    protected abstract void storeTimeSeriesOutputToDatabase(String location);
+	protected abstract void storeTimeSeriesOutputToDatabase(String location);
 
-    protected abstract void storeLocalFileOutput();
+	protected abstract void storeLocalFileOutput();
 
-    @Override
-    public void run() {
+	@Override
+	public void run() {
 
-        try {
+		try {
 
-            if (message == null) {
-                return;
-            }
-            setRunStartedStatus();
-            runSimulator();
-            storeOutput();
-            setRunCompletedStatus();
-        } catch (IOException ex) {
-            System.err.println("IOException running simulator: " + ex.getMessage());
-        } finally {
-            finalizeRun();
-        }
+			if (message == null) {
+				return;
+			}
+			setRunStartedStatus();
+			runSimulator();
+			storeOutput();
+			setRunCompletedStatus();
+		} catch (IOException ex) {
+			System.err.println("IOException running simulator: " + ex.getMessage());
+		} finally {
+			finalizeRun();
+		}
 
-    }
+	}
 
-    private void loadRunSimulationMessage() {
-        try {
-            message = dbUtils.getRunSimulationMessageForRun(runId);
-            if (message == null) {
-                updateStatus(MethodCallStatusEnum.FAILED, "The runSimulationMessage obtained from the database was null");
-            }
+	private void loadRunSimulationMessage() {
+		try {
+			message = dbUtils.getRunSimulationMessageForRun(runId);
+			if (message == null) {
+				updateStatus(MethodCallStatusEnum.FAILED, "The runSimulationMessage obtained from the database was null");
+			}
 
-        } catch (ApolloDatabaseException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, ex.getMessage());
-        } catch (IOException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, ex.getMessage());
-        }
-    }
+		} catch (ApolloDatabaseException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, ex.getMessage());
+		} catch (IOException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, ex.getMessage());
+		}
+	}
 
-    protected void updateStatus(MethodCallStatusEnum statusEnum, String message) {
-        RunUtils.updateStatus(dbUtils, runId, statusEnum, message);
-    }
+	protected void updateStatus(MethodCallStatusEnum statusEnum, String message) {
+		RunUtils.updateStatus(dbUtils, runId, statusEnum, message);
+	}
 
-    protected void storeOutput() throws IOException {
-        if (useDatabase) {
-            // get location
-            Location location = message.getInfectiousDiseaseScenario().getLocation();
-            String locationString;
-            if (location.getApolloLocationCode() != null) {
-                locationString = location.getApolloLocationCode();
-            } else {
-                // else use first location included
-                locationString = location.getLocationDefinition().getLocationsIncluded().get(0);
-            }
-            storeFileOutputToDatabase(locationString);
-            updateStatus(MethodCallStatusEnum.LOG_FILES_WRITTEN, "The simulator log files were written");
+	protected void storeOutput() throws IOException {
+		if (useDatabase) {
+			// get location
+			Location location = message.getInfectiousDiseaseScenario().getLocation();
+			String locationString;
+			if (location.getApolloLocationCode() != null) {
+				locationString = location.getApolloLocationCode();
+			} else {
+				// else use first location included
+				locationString = location.getLocationDefinition().getLocationsIncluded().get(0);
+			}
+			storeFileOutputToDatabase(locationString);
+			updateStatus(MethodCallStatusEnum.LOG_FILES_WRITTEN, "The simulator log files were written");
 
-            storeTimeSeriesOutputToDatabase(locationString);
-        }
-        if (useFile) {
-            storeLocalFileOutput();
-        }
-    }
+			storeTimeSeriesOutputToDatabase(locationString);
+		}
+		if (useFile) {
+			storeLocalFileOutput();
+		}
+	}
 
-    private void setRunCompletedStatus() {
-        updateStatus(MethodCallStatusEnum.COMPLETED, "The run is complete");
-    }
-    
-    private void setRunStartedStatus() {
-        updateStatus(MethodCallStatusEnum.RUNNING, "The simulator is running");
-    }
+	private void setRunCompletedStatus() {
+		updateStatus(MethodCallStatusEnum.COMPLETED, "The run is complete");
+	}
 
-    private void finalizeRun() {
-        SimulatorServiceQueue.simulatorThreadFinished();
-        SimulatorServiceQueue.startQueuedSimulatorThreads();
-    }
+	private void setRunStartedStatus() {
+		updateStatus(MethodCallStatusEnum.RUNNING, "The simulator is running");
+	}
 
-    protected final void addTextDataContentForSeries(String content, String seriesName) throws IOException {
-        int dataContentKey;
-        try {
-            dataContentKey = dbUtils.addTextDataContent(content);
-        } catch (ClassNotFoundException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "ClassNotFoundException attempting to add text data "
-                    + "content for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-            return;
-        } catch (SQLException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "SQLException attempting to add text data "
-                    + "content for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-            return;
-        } catch (ApolloDatabaseException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseException attempting to add text data "
-                    + "content for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-            return;
-        }
+	private void finalizeRun() {
+		SimulatorServiceQueue.simulatorThreadFinished();
+		SimulatorServiceQueue.startQueuedSimulatorThreads();
+	}
 
-        int runDataDescriptionId;
-        try {
-            runDataDescriptionId = dbUtils.getRunDataDescriptionId(ApolloDbUtils.DbContentDataFormatEnum.TEXT, seriesName + ".txt",
-                    ApolloDbUtils.DbContentDataType.SIMULATOR_LOG_FILE, message.getSimulatorIdentification(),
-                    visualizerId);
-        } catch (ApolloDatabaseKeyNotFoundException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseKeyNotFoundException attempting to get run data "
-                    + "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-            return;
-        } catch (ClassNotFoundException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "ClassNotFoundException attempting to get run data "
-                    + "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-            return;
-        } catch (SQLException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "SQLException attempting to get run data "
-                    + "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-            return;
-        } catch (ApolloDatabaseException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseException attempting to get run data "
-                    + "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-            return;
-        }
+	protected final void addTextDataContentForSeries(double[] series, String seriesName, String locationString) throws IOException {
 
-        try {
-            dbUtils.associateContentWithRunId(runId, dataContentKey, runDataDescriptionId);
-        } catch (ClassNotFoundException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "ClassNotFoundException attempting to associate "
-                    + "content with run ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-        } catch (SQLException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "SQLException attempting to associate "
-                    + "content with run ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-        } catch (ApolloDatabaseKeyNotFoundException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseKeyNotFoundException attempting to associate "
-                    + "content with run ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
-        }
-    }
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		try {
+			for (int i = 0; i < series.length; i++) {
+				String line = "US" + locationString + " " + series[i] + " " + i + ":1" + "\n";
+				stream.write(line.getBytes());
+			}
+		} catch (IOException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "IOException attempting to write " + seriesName + " series "
+					+ "to ByteArrayOutputStream for run " + runId + ": " + ex.getMessage());
+		}
+		addTextDataContentForSeries(stream.toString(), seriesName);
 
-    protected final String getConfigurationFileContent() throws IOException {
-        // first get the configuration file from the database
-        Map<String, ByteArrayOutputStream> map;
-        try {
-            int translatorKey = dbUtils.getSoftwareIdentificationKey(translatorSoftwareId);
-            int simulatorKey = dbUtils.getSoftwareIdentificationKey(message.getSimulatorIdentification());
-            map = dbUtils.getDataContentForSoftware(runId,
-                    translatorKey, simulatorKey);
-            // update this
-        } catch (ApolloDatabaseException ex) {
-            updateStatus(MethodCallStatusEnum.FAILED, ex.getMessage());
-            return null;
-        }
+	}
 
-        String configurationFileContent = null;
-        for (String label : map.keySet()) {
-            if (label.equals("config.txt")) {
-                configurationFileContent = map.get(label).toString();
-                break;
-            }
-        }
+	private void addTextDataContentForSeries(String content, String seriesName) throws IOException {
+		int dataContentKey;
+		try {
+			dataContentKey = dbUtils.addTextDataContent(content);
+		} catch (ClassNotFoundException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "ClassNotFoundException attempting to add text data "
+					+ "content for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+			return;
+		} catch (SQLException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "SQLException attempting to add text data "
+					+ "content for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+			return;
+		} catch (ApolloDatabaseException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseException attempting to add text data "
+					+ "content for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+			return;
+		}
 
-        if (configurationFileContent == null) {
-            updateStatus(MethodCallStatusEnum.FAILED, "No label \"config.txt\" was found in the data content map"
-                    + " for run " + runId);
-            return null;
-        }
+		int runDataDescriptionId;
+		try {
+			runDataDescriptionId = dbUtils.getRunDataDescriptionId(ApolloDbUtils.DbContentDataFormatEnum.TEXT, seriesName + ".txt",
+					ApolloDbUtils.DbContentDataType.SIMULATOR_LOG_FILE, message.getSimulatorIdentification(),
+					visualizerId);
+		} catch (ApolloDatabaseKeyNotFoundException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseKeyNotFoundException attempting to get run data "
+					+ "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+			return;
+		} catch (ClassNotFoundException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "ClassNotFoundException attempting to get run data "
+					+ "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+			return;
+		} catch (SQLException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "SQLException attempting to get run data "
+					+ "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+			return;
+		} catch (ApolloDatabaseException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseException attempting to get run data "
+					+ "description ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+			return;
+		}
 
-        return configurationFileContent;
-    }
+		try {
+			dbUtils.associateContentWithRunId(runId, dataContentKey, runDataDescriptionId);
+		} catch (ClassNotFoundException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "ClassNotFoundException attempting to associate "
+					+ "content with run ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+		} catch (SQLException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "SQLException attempting to associate "
+					+ "content with run ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+		} catch (ApolloDatabaseKeyNotFoundException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, "ApolloDatabaseKeyNotFoundException attempting to associate "
+					+ "content with run ID for series " + seriesName + " for run " + runId + ": " + ex.getMessage());
+		}
+	}
 
-    private static void loadTimeSeriesVisualizerSoftwareIdentification() {
+	protected final String getConfigurationFileContent() throws IOException {
+		// first get the configuration file from the database
+		Map<String, ByteArrayOutputStream> map;
+		try {
+			int translatorKey = dbUtils.getSoftwareIdentificationKey(translatorSoftwareId);
+			int simulatorKey = dbUtils.getSoftwareIdentificationKey(message.getSimulatorIdentification());
+			map = dbUtils.getDataContentForSoftware(runId,
+					translatorKey, simulatorKey);
+			// update this
+		} catch (ApolloDatabaseException ex) {
+			updateStatus(MethodCallStatusEnum.FAILED, ex.getMessage());
+			return null;
+		}
 
-        System.out.println("Loading time series visualizer software identification");
-        try {
-            ApolloDbUtils dbUtils = new ApolloDbUtils(new File(SimulatorServiceImpl.getDatabasePropertiesFilename()));
+		String configurationFileContent = null;
+		for (String label : map.keySet()) {
+			if (label.equals("config.txt")) {
+				configurationFileContent = map.get(label).toString();
+				break;
+			}
+		}
 
-            Map<Integer, ServiceRegistrationRecord> softwareIdMap = dbUtils.getRegisteredSoftware();
-            for (Integer id : softwareIdMap.keySet()) {
-                SoftwareIdentification softwareId = softwareIdMap.get(id).getSoftwareIdentification();
-                if (softwareId.getSoftwareName().toLowerCase().equals("time series visualizer")) {
-                    visualizerId = softwareIdMap.get(id).getSoftwareIdentification();
-                    break;
-                }
+		if (configurationFileContent == null) {
+			updateStatus(MethodCallStatusEnum.FAILED, "No label \"config.txt\" was found in the data content map"
+					+ " for run " + runId);
+			return null;
+		}
 
-            }
+		return configurationFileContent;
+	}
 
-        } catch (ClassNotFoundException ex) {
-            throw new ExceptionInInitializerError("ClassNotFoundException attempting to load the time series visualizer software ID: "
-                    + ex.getMessage());
-        } catch (IOException ex) {
-            throw new ExceptionInInitializerError("IOException attempting to load the time series visualizer software ID: " + ex.getMessage());
-        } catch (SQLException ex) {
-            throw new ExceptionInInitializerError("SQLException attempting to load the time series visualizer software ID: " + ex.getMessage());
-        }
+	private static void loadTimeSeriesVisualizerSoftwareIdentification() {
 
-        if (visualizerId == null) {
-            throw new ExceptionInInitializerError("Could not find the time series visualizer software id in the list of registered services");
-        }
-    }
+		System.out.println("Loading time series visualizer software identification");
+		try {
+			ApolloDbUtils dbUtils = new ApolloDbUtils(new File(SimulatorServiceImpl.getDatabasePropertiesFilename()));
 
-    static {
-        loadTimeSeriesVisualizerSoftwareIdentification();
-    }
+			Map<Integer, ServiceRegistrationRecord> softwareIdMap = dbUtils.getRegisteredSoftware();
+			for (Integer id : softwareIdMap.keySet()) {
+				SoftwareIdentification softwareId = softwareIdMap.get(id).getSoftwareIdentification();
+				if (softwareId.getSoftwareName().toLowerCase().equals("time series visualizer")) {
+					visualizerId = softwareIdMap.get(id).getSoftwareIdentification();
+					break;
+				}
+
+			}
+
+		} catch (ClassNotFoundException ex) {
+			throw new ExceptionInInitializerError("ClassNotFoundException attempting to load the time series visualizer software ID: "
+					+ ex.getMessage());
+		} catch (IOException ex) {
+			throw new ExceptionInInitializerError("IOException attempting to load the time series visualizer software ID: " + ex.getMessage());
+		} catch (SQLException ex) {
+			throw new ExceptionInInitializerError("SQLException attempting to load the time series visualizer software ID: " + ex.getMessage());
+		}
+
+		if (visualizerId == null) {
+			throw new ExceptionInInitializerError("Could not find the time series visualizer software id in the list of registered services");
+		}
+	}
+
+	static {
+		loadTimeSeriesVisualizerSoftwareIdentification();
+	}
 }
