63183d61b8064d4b6351cd954005575cf52ce0d5
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2014-10-01 15:39:28 +0000

    added missing chart type classes for visualizer

---
+++ apollo-ws/service-skeletons/java/trunk/visualizer-service/src/main/java/edu/pitt/apollo/timeseriesvisualizer/types/ChartTypeEnum.java
@@ -0,0 +1,14 @@
+package edu.pitt.apollo.timeseriesvisualizer.types;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Sep 19, 2014
+ * Time: 11:28:30 AM
+ * Class: ChartTypeEnum
+ */
+public enum ChartTypeEnum {
+
+	INFECTION_STATES, DISEASE_STATES, INCIDENCE, TREATMENT, NEWLY_DECEASED
+}
