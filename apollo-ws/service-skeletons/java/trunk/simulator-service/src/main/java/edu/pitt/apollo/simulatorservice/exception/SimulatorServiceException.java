8b5f01399d14e698608edbd14f7c746458bfc6c3
nickmillett <nem41@pitt.edu>
2014-10-20 19:43:18 +0000

    a few small fixes/improvements to SimulatorThread

---
+++ apollo-ws/service-skeletons/java/trunk/simulator-service/src/main/java/edu/pitt/apollo/simulatorservice/exception/SimulatorServiceException.java
@@ -0,0 +1,17 @@
+package edu.pitt.apollo.simulatorservice.exception;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Oct 20, 2014
+ * Time: 11:53:51 AM
+ * Class: SimulatorServiceException
+ */
+public class SimulatorServiceException extends Exception {
+
+	public SimulatorServiceException(String message) {
+		super(message);
+	}
+	
+}
