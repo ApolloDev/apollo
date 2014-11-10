40cd38f2470f238195548d57562a7dfacf67287a
nickmillett <nem41@pitt.edu>
2014-11-10 22:35:00 +0000

    added new library methods updated library schema updated library db utils

--- apollo-ws/service-skeletons/java/trunk/library-service/src/main/java/edu/pitt/apollo/libraryservice/methods/AddReviewerCommentMethod.java
+++ apollo-ws/service-skeletons/java/trunk/library-service/src/main/java/edu/pitt/apollo/libraryservice/methods/AddReviewerCommentMethod.java
@@ -3,9 +3,8 @@ package edu.pitt.apollo.libraryservice.methods;
 import edu.pitt.apollo.db.ApolloDatabaseException;
 import edu.pitt.apollo.db.LibraryDbUtils;
 import edu.pitt.apollo.db.LibraryUserRoleTypeEnum;
-import edu.pitt.apollo.library_service_types.v2_1_0.AddOrUpdateLibraryItemContainerMessage;
-import edu.pitt.apollo.library_service_types.v2_1_0.AddOrUpdateLibraryItemContainerResult;
-import edu.pitt.apollo.library_service_types.v2_1_0.LibraryItemContainer;
+import edu.pitt.apollo.library_service_types.v2_1_0.AddReviewerCommentMessage;
+import edu.pitt.apollo.library_service_types.v2_1_0.AddReviewerCommentResult;
 import edu.pitt.apollo.services_common.v2_1_0.Authentication;
 import edu.pitt.apollo.services_common.v2_1_0.MethodCallStatus;
 import edu.pitt.apollo.services_common.v2_1_0.MethodCallStatusEnum;
@@ -14,33 +13,31 @@ import edu.pitt.apollo.services_common.v2_1_0.MethodCallStatusEnum;
  *
  * Author: Nick Millett
  * Email: nick.millett@gmail.com
- * Date: Aug 13, 2014
- * Time: 3:41:53 PM
- * Class: AddLibraryItemMethod
+ * Date: Nov 7, 2014
+ * Time: 11:21:20 AM
+ * Class: AddReviewerCommentMethod
  */
-public class AddLibraryItemMethod {
+public class AddReviewerCommentMethod {
 
-	public static AddOrUpdateLibraryItemContainerResult addLibraryItem(LibraryDbUtils dbUtils,
-			AddOrUpdateLibraryItemContainerMessage message) {
-
+	public static AddReviewerCommentResult addReviewerComment(LibraryDbUtils dbUtils, AddReviewerCommentMessage message) {
+		
 		Authentication authentication = message.getAuthentication();
 		String uri = message.getUri();
 		String comment = message.getComment();
-		LibraryItemContainer item = message.getLibraryItemContainer();
-		
-		AddOrUpdateLibraryItemContainerResult result = new AddOrUpdateLibraryItemContainerResult();
+		int version = message.getVersion();
+
+		AddReviewerCommentResult result = new AddReviewerCommentResult();
 		MethodCallStatus status = new MethodCallStatus();
 		result.setStatus(status);
-		
+
 		try {
-			boolean userAuthorized = dbUtils.authorizeUser(authentication, LibraryUserRoleTypeEnum.COMMITTER);
+			boolean userAuthorized = dbUtils.authorizeUser(authentication, LibraryUserRoleTypeEnum.REVIEWER);
 			if (userAuthorized) {
-				int version = dbUtils.addLibraryItem(uri, item, authentication, comment);
-				result.setVersion(version);
+				dbUtils.addReviewerComment(uri, version, comment, authentication);
 				status.setStatus(MethodCallStatusEnum.COMPLETED);
 			} else {
 				status.setStatus(MethodCallStatusEnum.AUTHENTICATION_FAILURE);
-				status.setMessage("You are not authorized to add items to the library.");
+				status.setMessage("You are not authorized to add reviewer comments.");
 			}
 
 		} catch (ApolloDatabaseException ex) {
@@ -50,4 +47,5 @@ public class AddLibraryItemMethod {
 
 		return result;
 	}
+
 }
