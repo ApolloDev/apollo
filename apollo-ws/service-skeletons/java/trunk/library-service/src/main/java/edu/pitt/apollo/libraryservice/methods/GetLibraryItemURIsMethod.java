989ef7dd67495de38b649224f821789748bcae1d
nickmillett <nem41@pitt.edu>
2014-11-12 19:47:04 +0000

    added some unique constraints to the library db schema changed how objects are serialized in the library db utils added get URIs method to library db utils

--- apollo-ws/service-skeletons/java/trunk/library-service/src/main/java/edu/pitt/apollo/libraryservice/methods/GetLibraryItemURIsMethod.java
+++ apollo-ws/service-skeletons/java/trunk/library-service/src/main/java/edu/pitt/apollo/libraryservice/methods/GetLibraryItemURIsMethod.java
@@ -1,28 +1,42 @@
 package edu.pitt.apollo.libraryservice.methods;
 
+import edu.pitt.apollo.db.ApolloDatabaseException;
+import edu.pitt.apollo.db.LibraryDbUtils;
+import edu.pitt.apollo.db.LibraryUserRoleTypeEnum;
 import edu.pitt.apollo.library_service_types.v2_1_0.GetLibraryItemURIsMessage;
 import edu.pitt.apollo.library_service_types.v2_1_0.GetLibraryItemURIsResult;
+import edu.pitt.apollo.services_common.v2_1_0.Authentication;
 import edu.pitt.apollo.services_common.v2_1_0.MethodCallStatus;
 import edu.pitt.apollo.services_common.v2_1_0.MethodCallStatusEnum;
 
 public class GetLibraryItemURIsMethod {
 
-	public static GetLibraryItemURIsResult getLibraryItemURIs(
+	public static GetLibraryItemURIsResult getLibraryItemURIs(LibraryDbUtils dbUtils,
 			GetLibraryItemURIsMessage getLibraryItemURIsMessage) {
-		// query = "select all URIs from the db";
+
+		Authentication authentication = getLibraryItemURIsMessage.getAuthentication();
 		String itemType = getLibraryItemURIsMessage.getItemType();
-		if (itemType != null) {
-			// query = " where item_type = "+itemType;
-		}
+
 		//execute query
 		GetLibraryItemURIsResult result = new GetLibraryItemURIsResult();
 		MethodCallStatus status = new MethodCallStatus();
-		status.setMessage("");
-		status.setStatus(MethodCallStatusEnum.COMPLETED);
 		result.setStatus(status);
-		
-		//for each result...
-		result.getURIs().add("//result");
+
+		try {
+			boolean userAuthorized = dbUtils.authorizeUser(authentication, LibraryUserRoleTypeEnum.READONLY);
+			if (userAuthorized) {
+				result.getURIs().addAll(dbUtils.getURIs(itemType));
+				status.setMessage("");
+				status.setStatus(MethodCallStatusEnum.COMPLETED);
+			} else {
+				status.setStatus(MethodCallStatusEnum.AUTHENTICATION_FAILURE);
+				status.setMessage("You are not authorized to get item URIs from the library.");
+			}
+		} catch (ApolloDatabaseException ex) {
+			status.setStatus(MethodCallStatusEnum.FAILED);
+			status.setMessage(ex.getMessage());
+		}
+
 		return result;
 	}
 
