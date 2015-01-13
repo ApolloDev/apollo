c97f10961095ba06ec05455309d76eca82b2b170
JohnLevander <JohnLevander@users.noreply.github.com>
2015-01-13 16:11:23 +0000

    Tagging 2.1.0

---
+++ apollo-ws/service-skeletons/java/tags/2.1.0/broker-service/src/main/java/edu/pitt/apollo/apolloservice/database/DatabaseAccessorFactory.java
@@ -0,0 +1,30 @@
+package edu.pitt.apollo.apolloservice.database;
+
+import edu.pitt.apollo.apolloservice.exception.UnrecognizedMessageTypeException;
+import edu.pitt.apollo.services_common.v2_1_0.Authentication;
+import edu.pitt.apollo.simulator_service_types.v2_1_0.RunSimulationMessage;
+import edu.pitt.apollo.visualizer_service_types.v2_1_0.RunVisualizationMessage;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Jul 30, 2014
+ * Time: 3:46:22 PM
+ * Class: DatabaseAccessorFactory
+ */
+public class DatabaseAccessorFactory {
+	
+	public static DatabaseAccessor getDatabaseAccessor(Object message, Authentication authentication) throws UnrecognizedMessageTypeException {
+		
+		if (message instanceof RunSimulationMessage) {
+			return new DatabaseAccessorForRunningSimulations((RunSimulationMessage) message, authentication);
+		} else if (message instanceof RunVisualizationMessage) {
+			return new DatabaseAccessorForRunningVisualizations((RunVisualizationMessage) message, authentication);
+		} else {
+			throw new UnrecognizedMessageTypeException("Unrecognized message type in DatabaseAccessorFactory");
+		}
+		
+	}
+	
+}
