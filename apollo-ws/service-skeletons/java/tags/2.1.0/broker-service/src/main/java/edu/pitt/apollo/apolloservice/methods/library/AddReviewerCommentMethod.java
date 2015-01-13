43908f01570968d02e73d3726f7b48243b3dcc69
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2015-01-13 16:11:23 +0000

    Tagging 2.1.0

---
+++ apollo-ws/service-skeletons/java/tags/2.1.0/broker-service/src/main/java/edu/pitt/apollo/apolloservice/methods/library/AddReviewerCommentMethod.java
@@ -0,0 +1,20 @@
+package edu.pitt.apollo.apolloservice.methods.library;
+
+import edu.pitt.apollo.library_service_types.v2_1_0.AddReviewerCommentMessage;
+import edu.pitt.apollo.library_service_types.v2_1_0.AddReviewerCommentResult;
+
+/**
+ *
+ * Author: Nick Millett
+ * Email: nick.millett@gmail.com
+ * Date: Dec 12, 2014
+ * Time: 4:47:12 PM
+ * Class: AddReviewerCommentMethod
+ */
+public class AddReviewerCommentMethod extends LibraryMethod {
+
+	public static AddReviewerCommentResult addReviewerComment(AddReviewerCommentMessage addReviewerCommentToLibraryItemMessage) {
+		return getLibraryServicePort().addReviewerCommentToLibraryItem(addReviewerCommentToLibraryItemMessage);
+	}
+	
+}
