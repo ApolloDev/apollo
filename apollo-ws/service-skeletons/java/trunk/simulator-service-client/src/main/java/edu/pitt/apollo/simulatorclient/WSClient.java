f1d2d257cb84dc7d97856174adc600303dc4f735
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2015-01-16 18:45:11 +0000

    made a small simulator client just for testing connections to wsdls

--- apollo-ws/service-skeletons/java/trunk/simulator-service-client/src/main/java/edu/pitt/apollo/simulatorclient/WSClient.java
+++ apollo-ws/service-skeletons/java/trunk/simulator-service-client/src/main/java/edu/pitt/apollo/simulatorclient/WSClient.java
@@ -12,14 +12,28 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package edu.pitt.apollo.simulatorclient;
 
+import edu.pitt.apollo.service.simulatorservice.v3_0_0.SimulatorServiceEI;
+import edu.pitt.apollo.service.simulatorservice.v3_0_0.SimulatorServiceV300;
+import edu.pitt.apollo.services_common.v3_0_0.MethodCallStatus;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.namespace.QName;
 
 public class WSClient {
 
+	public static final String WSDL_LOC = "http://gaia.pha.psc.edu:13500/pscsimu?wsdl";
+	public static final QName SERVICE = new QName("http://service.apollo.pitt.edu/simulatorservice/v3_0_0/", "SimulatorService_v3.0.0");
+
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
+		SimulatorServiceV300 simulatorService = new SimulatorServiceV300(new URL(WSDL_LOC), SERVICE);
+		SimulatorServiceEI port = simulatorService.getSimulatorServiceEndpoint();
+
+		MethodCallStatus result = port.runSimulation(BigInteger.ONE);
+		System.out.println(result.getStatus());
+		
 	}
 }
