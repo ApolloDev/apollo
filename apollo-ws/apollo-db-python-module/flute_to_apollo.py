bdbd0c6c7921cfeaedacda86d162e8efd7a7796b
shawntbrown@gmail.com <shawntbrown@gmail.com@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2015-01-21 14:38:06 +0000

    Updated all DB conversion scripts to take command line arguments for database flags.  Updated fred_to_apollo.py to use the new block level tracking.

--- apollo-ws/apollo-db-python-module/flute_to_apollo.py
+++ apollo-ws/apollo-db-python-module/flute_to_apollo.py
@@ -1,14 +1,14 @@
-import os,sys,string
+import os, sys, string
 import math
 import optparse
 import apollo
 import time
-import glob,hashlib
+import glob, hashlib
 import datetime
 import random
-from multiprocessing import Process,Queue
+from multiprocessing import Process, Queue
 
-def parseFips(state,county,tract):
+def parseFips(state, county, tract):
     fipsString = ""
     if len(str(state)) == 1:
         fipsString = "0" + str(state)
@@ -25,8 +25,13 @@ def parseFips(state,county,tract):
     fipsString += str(tract)
     return fipsString
 
+def statusUpdate(status, message):
+    statusFile = './starttime'
+    with open(statusFile, "wb") as f:
+        f.write("%s %s" % (status, message))
+
 def error_exit(message):
-	#statusUpdate("LOGERROR",message)
+	# statusUpdate("LOGERROR",message)
 	sys.stderr.write(message)
 	sys.exit(1)
 
@@ -35,14 +40,25 @@ if __name__ == '__main__':
     %prog [--help]
     """)
     
-    parser.add_option("-d","--debug",action="store_true")
-    parser.add_option("-i","--runId",type="string",
+    parser.add_option("-d", "--debug", action="store_true")
+    parser.add_option("-i", "--runId", type="string",
 		      help="The Apollo RunId for this call")
     
-    opts,args=parser.parse_args()
+    parser.add_option("-H", "--dbHost", type="string", default="warhol-fred.psc.edu")
+    parser.add_option("-D", "--dbName", type="string", default="test")
+    parser.add_option("-U", "--dbUser", type="string", default="apolloint")
+    parser.add_option("-P", "--dbPword", type="string", default="int4p0llo")
+    parser.add_option("-n", "--no_db", action="store_true", default=False, 
+                      help='(Debugging) Flag prevents from writing to the database, but still need database to run')
+
+    opts, args = parser.parse_args()
     
     try:
-        apolloDB = apollo.ApolloDB(host_="warhol-fred.psc.edu",dbname_="test")
+        apolloDB = apollo.ApolloDB(host_=opts.dbHost,
+                                   dbname_=opts.dbName,
+                                   user_=opts.dbUser,
+                                   password_=opts.dbPword)
+
         apolloDB.connect()
         
     except Exception as e:
@@ -51,12 +67,12 @@ if __name__ == '__main__':
     tractList = []
     simulationRunId = opts.runId
 
-    ### Parse the Tracts that you need to know
+    # ## Parse the Tracts that you need to know
     try:
-        with open("Tracts0","rb") as f:
+        with open("Tracts0", "rb") as f:
             for line in f.readlines()[1:]:
                 lineSplit = line.split(",")
-                tractList.append(parseFips(lineSplit[1],lineSplit[2],lineSplit[3]))
+                tractList.append(parseFips(lineSplit[1], lineSplit[2], lineSplit[3]))
     except Exception as e:
         error_exit(str(e))
                                
@@ -66,11 +82,11 @@ if __name__ == '__main__':
         logFiles = glob.glob('Log*')
 
         
-        ## First find the number of days that this job was run for
+        # # First find the number of days that this job was run for
         numberOfDays = 0
         for logFile in logFiles:
-            with open(logFile,"rb") as f:
-            ## get the last line
+            with open(logFile, "rb") as f:
+            # # get the last line
                 lastLineSplit = f.readlines()[-1].split(",")
                 if int(lastLineSplit[0]) > numberOfDays: numberOfDays = int(lastLineSplit[0])
                 
@@ -79,8 +95,8 @@ if __name__ == '__main__':
         
         for logFile in logFiles:
             
-            with open(logFile,'rb') as f:
-                ### skip Header
+            with open(logFile, 'rb') as f:
+                # ## skip Header
                 isHead = False
                 for line in f:
                     if not isHead:
@@ -88,58 +104,66 @@ if __name__ == '__main__':
                     else:
                         lineSplit = line.split(",")
                         newInfectsVal = sum(float(x) for x in lineSplit[12:16])
-                        #print lineSplit[1] +" " + lineSplit[0]
+                        # print lineSplit[1] +" " + lineSplit[0]
                         newInfectsPerDayPerTract[int(lineSplit[1])][int(lineSplit[0])] += newInfectsVal
             
-        ### Now make them an average
-        for i in range(0,len(newInfectsPerDayPerTract)):
-            for j in range(0,len(newInfectsPerDayPerTract[i])):
+        # ## Now make them an average
+        for i in range(0, len(newInfectsPerDayPerTract)):
+            for j in range(0, len(newInfectsPerDayPerTract[i])):
                 newInfectsPerDayPerTract[i][j] /= float(len(logFiles))
                         
         
-        ### Create the output string
+        # ## Create the output string
         outStrList = []
-        for i in range(0,len(newInfectsPerDayPerTract)):
-            for j in range(0,len(newInfectsPerDayPerTract[i])):
-                outStrList.append('US%s %d %d:1\n'%(tractList[i],int(math.ceil(newInfectsPerDayPerTract[i][j])),j))
+        for i in range(0, len(newInfectsPerDayPerTract)):
+            for j in range(0, len(newInfectsPerDayPerTract[i])):
+                outStrList.append('US%s %d %d:1\n' % (tractList[i], int(math.ceil(newInfectsPerDayPerTract[i][j])), j))
         
         
         outStr = ''.join(outStrList)
-	with open('out.log','wb') as f:
-	    	f.write(outStr)
+    	with open('out.log', 'wb') as f:
+    	    f.write(outStr)
+        
         m = hashlib.md5()
         m.update(outStr)
         m5hash = m.hexdigest() 
-        bufferSize=len(outStr)
+        bufferSize = len(outStr)
         count = 0
         runDataContentId = -1
-        hashvar = apolloDB.checkMD5HashExistence(m5hash)
-        if hashvar > -1:
-            runDataContentId = hashvar
-        else:
-        	SQLString = 'INSERT INTO run_data_content (text_content, md5_hash_of_content) values ("%s","%s")'%(outStr,m5hash)
-        	apolloDB.query(SQLString)
-        	runDataContentId = apolloDB.insertID()
-        softIdTS =    apolloDB.getSoftwareIdentificationId(name_="Time Series Visualizer",version_="1.0")
-        softIdGAIA =  apolloDB.getSoftwareIdentificationId(name_="GAIA",version_="1.0")
-        softIdFLUTE = apolloDB.getSoftwareIdentificationId(name_="FluTE",version_="1.15")
-        runDataDescriptionIdTS   = apolloDB.getRunDataDescriptionId(label_="newly_exposed.txt",
+        if opts.no_db is False:
+            hashvar = apolloDB.checkMD5HashExistence(m5hash)
+            if hashvar > -1:
+                runDataContentId = hashvar
+            else:
+                SQLString = 'INSERT INTO run_data_content (text_content, md5_hash_of_content) values ("%s","%s")' % (outStr, m5hash)
+                apolloDB.query(SQLString)
+                runDataContentId = apolloDB.insertID()
+        softIdTS = apolloDB.getSoftwareIdentificationId(name_="Time Series Visualizer", version_="1.0")
+        softIdGAIA = apolloDB.getSoftwareIdentificationId(name_="GAIA", version_="1.0")
+        softIdFLUTE = apolloDB.getSoftwareIdentificationId(name_="FluTE", version_="1.15")
+        runDataDescriptionIdTS = apolloDB.getRunDataDescriptionId(label_="newly_exposed.txt",
                                                                     source_software_=softIdFLUTE,
 								    destination_software_=softIdTS)
         runDataDescriptionIdGAIA = apolloDB.getRunDataDescriptionId(label_="newly_exposed.txt",
                                                                     source_software_=softIdFLUTE,
                                                                     destination_software_=softIdGAIA)
-        SQLString = 'INSERT INTO run_data(run_Id,description_id,content_id) values ("%s","%d","%d")'%(simulationRunId,
-                                                                                                      runDataDescriptionIdTS,
-                                                                                                      runDataContentId)
-        apolloDB.query(SQLString)
+        SQLString = 'INSERT INTO run_data(run_Id,description_id,content_id) values ("%s","%d","%d")' % (simulationRunId,
+                                                                                                        runDataDescriptionIdTS,
+                                                                                                        runDataContentId)
+        if opts.no_db is False:
+            apolloDB.query(SQLString)
         
-        SQLString = 'INSERT INTO run_data(run_Id,description_id,content_id) values ("%s","%d","%d")'%(simulationRunId,
-                                                                                                      runDataDescriptionIdGAIA,
-                                                                                                      runDataContentId)
-        apolloDB.query(SQLString)
-
-        apolloDB.close() 
+        SQLString = 'INSERT INTO run_data(run_Id,description_id,content_id) values ("%s","%d","%d")' % (simulationRunId,
+                                                                                                        runDataDescriptionIdGAIA,
+                                                                                                        runDataContentId)
+        if opts.no_db is False:                                                                                          
+            apolloDB.query(SQLString)
+        apolloDB.close()
+        statusUpdate("LOG_FILES_WRITTEN", "%s" % datetime.datetime.now().strftime("%a %b %d %H:%M:%S EDT %Y"))    
+            # ## Insert data into database
+            
+            # with open('test.txt','wb') as f:
+            #    f.write("%s"%(''.join(outStr)))          
                               
     except Exception as e:
         error_exit(str(e))
