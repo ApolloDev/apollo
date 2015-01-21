e21fe3db9094537cb6c6838e923155cd6406b1db
shawntbrown@gmail.com <shawntbrown@gmail.com@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2015-01-21 14:38:06 +0000

    Updated all DB conversion scripts to take command line arguments for database flags.  Updated fred_to_apollo.py to use the new block level tracking.

--- apollo-ws/apollo-db-python-module/apollo.py
+++ apollo-ws/apollo-db-python-module/apollo.py
@@ -1,11 +1,11 @@
-import os,sys,string
-#import simWS
+import os, sys, string
+# import simWS
 import MySQLdb
 import MySQLdb.cursors
 from logger import Log
 
-### STB To Do Make this variable from the SimWS file
-#from SimulatorService_v2_0_2_types import *
+# ## STB To Do Make this variable from the SimWS file
+# from SimulatorService_v2_0_2_types import *
 
 _runTable = "run"
 _populationAxisTable = "population_axis"
@@ -16,8 +16,8 @@ _softwareIdentificationTable = "software_identification"
 
 class ApolloDB:
 	
-	def __init__(self,host_="warhol-fred.psc.edu",
-		 user_="apolloext",dbname_="apollo201",password_=None,logger_=None):
+	def __init__(self, host_="warhol-fred.psc.edu",
+		 user_="apolloext", dbname_="apollo201", password_=None, logger_=None):
 
 		self._host = host_
 		self._user = user_
@@ -33,12 +33,12 @@ class ApolloDB:
 		else:
 			self.logger = logger_
 
-		self.stateToPopulationDict = {'S':'susceptible','E':'exposed','I':'infectious',
-									  'R':'recovered','C':'newly exposed','V':'received vaccine control measure',
-									  'Av':'received antiviral control measure','ScCl':'school that is closed'}
+		self.stateToPopulationDict = {'S':'susceptible', 'E':'exposed', 'I':'infectious',
+									  'R':'recovered', 'C':'newly exposed', 'V':'received vaccine control measure',
+									  'Av':'received antiviral control measure', 'ScCl':'school that is closed'}
 
-		self.stateToDataFileDict = {'S':'susceptible.txt','E':'exposed.txt','I':'infectious.txt',
-									'R':'recovered.txt','C':'newly_exposed.txt','V':'vacc_administered.txt',
+		self.stateToDataFileDict = {'S':'susceptible.txt', 'E':'exposed.txt', 'I':'infectious.txt',
+									'R':'recovered.txt', 'C':'newly_exposed.txt', 'V':'vacc_administered.txt',
 									'Av':'av_administered.txt'}
 	def connect(self):
 		if self._conn is not None:
@@ -46,7 +46,7 @@ class ApolloDB:
 			return
 		try:
 			if self._password is None:
-				self._conn = MySQLdb.connect(host=self._host,user=self._user,db=self._dbname)
+				self._conn = MySQLdb.connect(host=self._host, user=self._user, db=self._dbname)
 			else:
 				self._conn = MySQLdb.connect(host=self._host,
 											 user=self._user,
@@ -71,7 +71,7 @@ class ApolloDB:
 
 		self._conn.close()
 
-	def query(self,SQLString):
+	def query(self, SQLString):
 		if self._conn is None:
 			raise RuntimeError("A connection to the Apollo database "\
 							   "has not been made before queury is made")
@@ -79,7 +79,7 @@ class ApolloDB:
 		try:
 			self._cursor.execute(SQLString)
 		except Exception as e:
-			self.logger.update('DB_QUERY_FAILED',message="%s,%s"%(str(e),SQLString))
+			self.logger.update('DB_QUERY_FAILED', message="%s,%s" % (str(e), SQLString))
 			raise
 		
 		rows = self._cursor.fetchall()
@@ -90,7 +90,7 @@ class ApolloDB:
 
 	def _populationAxis(self):
 		populationAxisDict = {}
-		SQLStatement = "SELECT * from %s"%_populationAxisTable
+		SQLStatement = "SELECT * from %s" % _populationAxisTable
 
 		result = self.query(SQLStatement)
 
@@ -101,89 +101,89 @@ class ApolloDB:
 
 	def getRuns(self):
 		runList = []
-		SQLStatement = "SELECT * from %s"%_runTable
+		SQLStatement = "SELECT * from %s" % _runTable
 		result = self.query(SQLStatement)
 		for row in result:
 			runList.append(row['label'])
 		return runList
 
 	def getTranslatorServiceKey(self):
-		SQLStatement = 'SELECT * from %s where service_type = "translator"'%(_softwareIdentificationTable)
+		SQLStatement = 'SELECT * from %s where service_type = "translator"' % (_softwareIdentificationTable)
 		result = self.query(SQLStatement)
 		row = result[0]
 		return row['id']
 	
-	def getSoftwareIdentificationId(self,name_="SEIR",version_="3.0"):
-		SQLStatement = 'SELECT id from %s where name = "%s" and version = "%s"'%(_softwareIdentificationTable,name_,version_)
+	def getSoftwareIdentificationId(self, name_="SEIR", version_="3.0"):
+		SQLStatement = 'SELECT id from %s where name = "%s" and version = "%s"' % (_softwareIdentificationTable, name_, version_)
 		result = self.query(SQLStatement)
 		if len(result) == 0:
-			self.logger.update('DB_QUERY_NO_RESULTS',message=SQLStatement)
+			self.logger.update('DB_QUERY_NO_RESULTS', message=SQLStatement)
 			return -1
 		return result[0]['id']
 	
 	
 	
 	
-	def getSoftwareIdentificationForRunId(self,runId):
+	def getSoftwareIdentificationForRunId(self, runId):
 		SQLStatement = 'SELECT t1.name,t1.developer,t1.version from software_identification t1, '\
-					   + 'run t2 where t1.id = t2.software_id and t2.id = %s'%str(runId)
+					   + 'run t2 where t1.id = t2.software_id and t2.id = %s' % str(runId)
 
 		result = self.query(SQLStatement)
 		if len(result) == 0:
-			self.logger.update('DB_QUERY_NO_RESULTS',message=SQLStatement)
-			return ('unknown','Could not find runId: %s in the ApolloDB')
+			self.logger.update('DB_QUERY_NO_RESULTS', message=SQLStatement)
+			return ('unknown', 'Could not find runId: %s in the ApolloDB')
 		if len(result) > 1:
-			self.logger.update('DB_QUERY_MORE_THAN_ONE',messsage=SQLStatement)
+			self.logger.update('DB_QUERY_MORE_THAN_ONE', messsage=SQLStatement)
 			raise
 
-		return (result[0]['name'],result[0]['developer'],result[0]['version'])
+		return (result[0]['name'], result[0]['developer'], result[0]['version'])
 	
 	def getSimulatorServiceKey(self, developer, name, version):
 		SQLStatement = 'SELECT * from %s where developer = "%s" and name = "%s"  and version = "%s"'\
-			%(_softwareIdentificationTable, developer, name, version)
+			% (_softwareIdentificationTable, developer, name, version)
 		result = self.query(SQLStatement)
 		if len(result) == 0:
-			self.logger.update('DB_QUERY_NO_RESULTS',message=SQLStatement)
+			self.logger.update('DB_QUERY_NO_RESULTS', message=SQLStatement)
 			raise
 		
 		return result[0]['id']
 	
-	def getRunDataDescriptionId(self,label_,source_software_=3,destination_software_=4,format_="TEXT",type_="SIMULATOR_LOG_FILE"):
+	def getRunDataDescriptionId(self, label_, source_software_=3, destination_software_=4, format_="TEXT", type_="SIMULATOR_LOG_FILE"):
 		try:
 			SQLStatement = 'SELECT v.run_data_description_id FROM run_data_description_view v WHERE '\
-				+ 'v.format = "%s" AND v.label = "%s" and v.type = "%s" and v.source_software = %s '%(format_,label_,type_,str(source_software_))\
-				+ 'and v.destination_software = %s'%(str(destination_software_))
+				+ 'v.format = "%s" AND v.label = "%s" and v.type = "%s" and v.source_software = %s ' % (format_, label_, type_, str(source_software_))\
+				+ 'and v.destination_software = %s' % (str(destination_software_))
 				
 			result = self.query(SQLStatement)
 			if len(result) > 1:
-				raise RuntimeError("getRunDataDescription returned more than one entry "+\
-									   "for (%s,%d,%d,%s,%s)"%(label_,source_software_,destination_software_,format_,type_))
+				raise RuntimeError("getRunDataDescription returned more than one entry " + \
+									   "for (%s,%d,%d,%s,%s)" % (label_, source_software_, destination_software_, format_, type_))
 			
 			return int(result[0]["run_data_description_id"])
 		except Exception as e:
 			print str(e)
 			raise e
 		
-	def getRunDataContentFromRunIdAndLabel(self,simulationRunId_,label_,source_software_=3,
-										   destination_software_=4,format_="TEXT",type_="SIMULATOR_LOG_FILE"):
+	def getRunDataContentFromRunIdAndLabel(self, simulationRunId_, label_, source_software_=3,
+										   destination_software_=4, format_="TEXT", type_="SIMULATOR_LOG_FILE"):
 		
 		try:
-			runDataDescriptionId = self.getRunDataDescriptionId(label_,source_software_,destination_software_,format_,type_)
+			runDataDescriptionId = self.getRunDataDescriptionId(label_, source_software_, destination_software_, format_, type_)
 			
-			### First get the content id
-			SQLStatement = 'SELECT content_id from run_data where run_Id = "%s" and description_Id = "%s"'%(simulationRunId_,runDataDescriptionId)
+			# ## First get the content id
+			SQLStatement = 'SELECT content_id from run_data where run_Id = "%s" and description_Id = "%s"' % (simulationRunId_, runDataDescriptionId)
 			result = self.query(SQLStatement)
 			
 			if len(result) > 1:
 				raise RuntimeError('getRunDataContentFromRunIdAndLabel returned more than one entry '\
-									   + 'for the content_id for (%s,%s)'%(simulationRunId_,label_))
+									   + 'for the content_id for (%s,%s)' % (simulationRunId_, label_))
 			if len(result) == 0:
 				return ""
 			
 			contentId = result[0]['content_id']
 			
-			### Now get the file content
-			SQLStatement = 'SELECT text_content from run_data_content where id = "%s"'%str(contentId)
+			# ## Now get the file content
+			SQLStatement = 'SELECT text_content from run_data_content where id = "%s"' % str(contentId)
 
 			result = self.query(SQLStatement)
 
@@ -195,10 +195,10 @@ class ApolloDB:
 		except Exception as e:
 			raise e
 
-	def checkMD5HashExistence(self,md5hash):
+	def checkMD5HashExistence(self, md5hash):
 		try:
 			
-			SQLStatement = 'SELECT * from run_data_content where md5_hash_of_content = "%s"'%(md5hash)
+			SQLStatement = 'SELECT * from run_data_content where md5_hash_of_content = "%s"' % (md5hash)
 			result = self.query(SQLStatement)
 			if len(result) > 0:
 				return result[0]['id']
@@ -208,27 +208,27 @@ class ApolloDB:
 		except Exception as e:
 			raise e
 
-	def getSimulatorIdFromRunId(self,simulationRunId_):
+	def getSimulatorIdFromRunId(self, simulationRunId_):
 		try:
-			SQLStatement = 'SELECT software_id from run where id = "%s"'%str(simulationRunId_)
+			SQLStatement = 'SELECT software_id from run where id = "%s"' % str(simulationRunId_)
 			result = self.query(SQLStatement)
 			if len(result) > 1:
-				raise RuntimeError("getSimulatorIdFromRunId: more than one run with simulationRunId %s"%str(simulationRunId_))
+				raise RuntimeError("getSimulatorIdFromRunId: more than one run with simulationRunId %s" % str(simulationRunId_))
 			return result[0]["software_id"]
 		except Exception as e:
 			raise e
 
 	
-	def getSimulationInputFilesForRunId(self,runId,translatorKey,simulatorKey):
+	def getSimulationInputFilesForRunId(self, runId, translatorKey, simulatorKey):
 		SQLStatement = 'SELECT rddv.label, rdc.text_content FROM run_data_content rdc, run_data rd, '\
-					   + 'run_data_description_view rddv WHERE rd.content_id = rdc.id AND rd.run_id = %s '%str(runId)\
-					   + 'AND rddv.run_data_description_id = rd.description_id AND rddv.source_software = %s '%str(translatorKey)\
-					   + 'AND rddv.destination_software = %s'%str(simulatorKey)
+					   + 'run_data_description_view rddv WHERE rd.content_id = rdc.id AND rd.run_id = %s ' % str(runId)\
+					   + 'AND rddv.run_data_description_id = rd.description_id AND rddv.source_software = %s ' % str(translatorKey)\
+					   + 'AND rddv.destination_software = %s' % str(simulatorKey)
 		
 		result = self.query(SQLStatement)
 		if len(result) == 0:
-			self.logger.update('DB_QUERY_NO_RESULTS',message=SQLStatement)
-			return ('unknown','Could not find runId: %s in the ApolloDB'%str(runId))
+			self.logger.update('DB_QUERY_NO_RESULTS', message=SQLStatement)
+			return ('unknown', 'Could not find runId: %s in the ApolloDB' % str(runId))
 
 		fileDict = {}
 		for row in result:
@@ -236,19 +236,19 @@ class ApolloDB:
 
 		return fileDict
 		
-	def getRunStatus(self,runId):
+	def getRunStatus(self, runId):
 		SQLStatement = 'SELECT t2.status,t1.message from run_status t1, run_status_description t2 '\
-					   + 'where t1.run_id = %s and t2.id = t1.status_id'%str(runId)
+					   + 'where t1.run_id = %s and t2.id = t1.status_id' % str(runId)
 		result = self.query(SQLStatement)
 		if len(result) == 0:
-			self.logger.update('DB_QUERY_NO_RESULTS',message=SQLStatement)
-			return ('unknown','Could not find runId: %s in the ApolloDB'%str(runId))
+			self.logger.update('DB_QUERY_NO_RESULTS', message=SQLStatement)
+			return ('unknown', 'Could not find runId: %s in the ApolloDB' % str(runId))
 
 		if len(result) > 1:
-			self.logger.update('DB_QUERY_MORE_THAN_ONE',messsage=SQLStatement)
-			return ('failed','More than in entry for runID: %s in the ApolloDB'%str(runId))
+			self.logger.update('DB_QUERY_MORE_THAN_ONE', messsage=SQLStatement)
+			return ('failed', 'More than in entry for runID: %s in the ApolloDB' % str(runId))
 
-		return (result[0]['status'],result[0]['message'])
+		return (result[0]['status'], result[0]['message'])
 
 	def getAllowedStatus(self):
 		SQLStatement = "SELECT status from run_status_description"
@@ -281,123 +281,35 @@ def main():
 	print "Testing connection to Apollo database"
 	apolloDB.connect()
 	print "Connection to Database successful"
-	#runList = apolloDB.getRuns()
-	#print "Keys: Translator %d FRED Simulator %d"%\
-	#	  (apolloDB.getTranslatorServiceKey(),\
-	#	   apolloDB.getSimulatorServiceKey('UPitt,PSC,CMU','FRED','2.0.1_i'))
-	#fileDict = apolloDB.getSimulationInputFilesForRunId('74',1,3)
-	#for fileName,content in fileDict.items():
-	#	print "%s"%fileName
+	# runList = apolloDB.getRuns()
+	# print "Keys: Translator %d FRED Simulator %d"%\
+	# 	  (apolloDB.getTranslatorServiceKey(),\
+	# 	   apolloDB.getSimulatorServiceKey('UPitt,PSC,CMU','FRED','2.0.1_i'))
+	# fileDict = apolloDB.getSimulationInputFilesForRunId('74',1,3)
+	# for fileName,content in fileDict.items():
+	# 	print "%s"%fileName
 	print str(apolloDB.getAllowedStatus())
 	print str(apolloDB.getRunStatus(2418))
 	print str(apolloDB.getSoftwareIdentificationForRunId(14))
-	apolloDB.setRunStatus(2418,"failed","my Message")
+	apolloDB.setRunStatus(2418, "failed", "my Message")
 	print str(apolloDB.getRunStatus(2418))
-	apolloDB.setRunStatus(2418,"completed","Completed at Thu, 10 Jul 2014 14:48:12 -0400")
+	apolloDB.setRunStatus(2418, "completed", "Completed at Thu, 10 Jul 2014 14:48:12 -0400")
 	print str(apolloDB.getRunStatus(14))
 			  
 	print "Closing connection to database"
 	apolloDB.close()
 	print "Closing successful"
 
-	#print "RunList 0: " + runList[0]
-	#apolloOut = ApolloSimulatorOutput(runList[0])
-	#apolloOut.getNewlyInfectedTimeSeriesWithinLocation("42003")
-	#apolloOut.printSimulatedPopulations()
+	# print "RunList 0: " + runList[0]
+	# apolloOut = ApolloSimulatorOutput(runList[0])
+	# apolloOut.getNewlyInfectedTimeSeriesWithinLocation("42003")
+	# apolloOut.printSimulatedPopulations()
 
-### Deprecated Class
-class _ApolloSimulatorOutput:
-	def __init__(self,runID_,logger_=None):
-		self.runID = runID_
-		self.logger = logger_
-		### here are the structures used to define the total output of an Apollo data unit
-
-		### Initiate the database
-		try:
-			self.apolloDB = ApolloDB(logger_=self.logger)
-			self.apolloDB.connect()
-		except:
-			raise
-		
-		### Get the ID from the database for this runID
-		SQLStatement = 'SELECT * from '+_runTable+' WHERE label = "' + self.runID + '"'
-		try:
-			self._dbRunIndex = self.apolloDB.query(SQLStatement)[0]['id']
-		except:
-			self.logger.update('DB_NOID_FOUND')
-			raise
-
-	def printSimulatedPopulations(self):
-		print "Simulated Populations for run: " + str(self.runID)
-		for population,id in self.simulatedPopulationsDict.items():
-			print population + ": id = " + str(id)
-
-	def getMaxDayfromTimeSeriesForRunID(self):
-		results = self.apolloDB.query('select max(time_step) from time_series '\
-									  +'where run_id = %d'%(self._dbRunIndex))
-		return int(results[0]['max(time_step)'])
-	
-	def getNewlyInfectedTimeSeriesForBlocks(self):
-		tsDict = {}
-		maxTime = self.getMaxDayfromTimeSeriesForRunID() 
-		SQLStatement = 'select substring(p.label,18) as incits, t.time_step, t.pop_count '\
-				  + 'from time_series t, simulated_population p '\
-				  + 'where t.run_id = %d and t.population_id = p.id and p.label like '%(self._dbRunIndex)\
-				  + '"newly exposed in %%" and length(substring(p.label,18)) > 6'
-		
-		try:
-			results = self.apolloDB.query(SQLStatement)
-		except:
-			self.logger.update('DB_POP_FAILED')
-			raise
-		
-		if len(results) == 0:
-			self.logger.update('DB_POP_ZERO')
-			raise
-		
-		for row in results:
-			if tsDict.has_key(row['incits']) is False:
-				tsDict[row['incits']] = [0 for x in range(0,maxTime+1)]
-			tsDict[row['incits']][int(row['time_step'])] = int(row['pop_count'])
-		return tsDict
-
-	def _getNewlyInfectedTimeSeriesWithinLocation(self,location):
-		### STB. This function is deprecated
-		tsDict = {}
-		fipsTranslate = {}
-		### get the populationAxis value for "location"
-		LocationAxisID = self.apolloDB._populationAxis()['location']
-		### Get distance populations
-		SQLStatement = 'SELECT population_id,value from simulated_population_axis_value '\
-		 	' where value like "%'+str(location) + '%"'
-		popResults = self.apolloDB.query(SQLStatement)
-
-		for row in popResults:
-			fipsTranslate[row['population_id']] = row['value']
-		### Get all of the Results for this time series
-		SQLStatement = 'select * from time_series where run_id = "'+str(self._dbRunIndex)+'" '\
-			  +'and population_id in (select id from (select p.id '\
-			  +'from simulated_population p, simulated_population_axis_value v, '\
-			  +'population_axis a where p.id = v.population_id and '\
-			  +'v.value like "newly exposed" and v.axis_id = a.id and '\
-			  +'a.label like "disease_state" and p.label like "% '+location+'%") as x)'\
-			  +' order by population_id, time_step'
-		tsResults = self.apolloDB.query(SQLStatement)
-		#print SQLStatement
-		#print "Length = " + str(len(tsResults))
-		for row in tsResults:
-			## Get the fips code for this population
-			fipsCode = fipsTranslate[row['population_id']]
-			if tsDict.has_key(fipsCode) is False:
-				tsDict[fipsCode] = []
-			tsDict[fipsCode].append(row['pop_count'])
-
-		return tsDict
 ############
 # Main hook
 ############
 
-if __name__=="__main__":
+if __name__ == "__main__":
 	main()
 
 	
