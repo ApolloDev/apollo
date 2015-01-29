27070d2d2778d9ec1e9a151270bfc1048b03cf4d
shawntbrown <shawntbrown@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2015-01-29 17:15:36 +0000

    Updated this service to Apollo Version 3.0

--- apollo-ws/visualizer-services/GAIA/trunk/visWS.py
+++ apollo-ws/visualizer-services/GAIA/trunk/visWS.py
@@ -4,10 +4,13 @@ configuration = {
         'tempDirectory':'/home/gaia/tmp',
         'outputDirectory':'/var/www/GAIA/',
         'URL':'http://gaia.pha.psc.edu/GAIA/',
-        'serverPort':8092,
-        'logFile':'/usr/local/packages/GAIA-Apollo-v2.0.2/gaiaService.202.log',
-        'apolloDBName':'test',
-        'wsdlFile':'/usr/local/packages/GAIA-Apollo-v2.0.2/visualizerservice202.wsdl',
+        'serverPort':13501,
+        'logFile':'/usr/local/packages/GAIA-Apollo-v3.0/gaiaService.303.log',
+	'apolloDBName':'apollo_300_snapshot',
+        'apolloDBHost':'warhol-fred.psc.edu',
+        'apolloDBUser':'apolloint',
+        'apolloDBPword':'int4p011o',
+        'wsdlFile':'/usr/local/packages/GAIA-Apollo-v3.0/visualizerservice300.wsdl',
         'apolloPyDir':'/usr/local/packages/Apollo-Python'
         }
     }
