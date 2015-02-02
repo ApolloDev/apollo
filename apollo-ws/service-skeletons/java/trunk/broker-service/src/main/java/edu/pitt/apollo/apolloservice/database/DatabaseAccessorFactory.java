506c42eded5670c02782f018c775bb63df06e97a
nickmillett <nem41@pitt.edu>
2015-02-02 23:08:11 +0000

    added data service

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/database/DatabaseAccessorFactory.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/database/DatabaseAccessorFactory.java
@@ -2,6 +2,8 @@ package edu.pitt.apollo.apolloservice.database;
 
 import edu.pitt.apollo.apollo_service_types.v3_0_0.RunSimulationsMessage;
 import edu.pitt.apollo.apolloservice.exception.UnrecognizedMessageTypeException;
+import edu.pitt.apollo.data_service_types.v3_0_0.GetOutputFilesURLAsZipMessage;
+import edu.pitt.apollo.data_service_types.v3_0_0.GetOutputFilesURLsMessage;
 import edu.pitt.apollo.services_common.v3_0_0.Authentication;
 import edu.pitt.apollo.simulator_service_types.v3_0_0.RunSimulationMessage;
 import edu.pitt.apollo.visualizer_service_types.v3_0_0.RunVisualizationMessage;
@@ -13,10 +15,9 @@ import edu.pitt.apollo.visualizer_service_types.v3_0_0.RunVisualizationMessage;
  */
 public class DatabaseAccessorFactory {
 
-	public static DatabaseAccessor getDatabaseAccessor(Object message,
-			Authentication authentication)
-			throws UnrecognizedMessageTypeException {
-		
+	public static DatabaseAccessor getDatabaseAccessor(Object message, Authentication authentication) throws UnrecognizedMessageTypeException {
+
+
 		if (message instanceof RunSimulationMessage) {
 			((RunSimulationMessage) message).getAuthentication()
 					.setRequesterPassword("");
@@ -32,6 +33,10 @@ public class DatabaseAccessorFactory {
 					.setRequesterPassword("");
 			return new DatabaseAccessorForRunningVisualizations(
 					(RunVisualizationMessage) message, authentication);
+		} else if (message instanceof GetOutputFilesURLsMessage) {
+			return new DatabaseAccessorForRunningDataService((GetOutputFilesURLsMessage) message, authentication);
+		} else if (message instanceof GetOutputFilesURLAsZipMessage) {
+			return new DatabaseAccessorForRunningDataService((GetOutputFilesURLAsZipMessage) message, authentication);
 		} else {
 			throw new UnrecognizedMessageTypeException(
 					"Unrecognized message type in DatabaseAccessorFactory");
