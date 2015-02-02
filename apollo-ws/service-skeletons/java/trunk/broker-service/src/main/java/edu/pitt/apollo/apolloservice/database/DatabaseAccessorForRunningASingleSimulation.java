4acbf383e07f971ee052fd57920c4e5b22486b0f
john.d.levander@gmail.com <john.d.levander@gmail.com@72930db8-7f83-2d6d-414c-35dd9ce70ea4>
2015-02-02 20:35:47 +0000

    Added batch functionality.

---
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/database/DatabaseAccessorForRunningASingleSimulation.java
@@ -0,0 +1,38 @@
+package edu.pitt.apollo.apolloservice.database;
+
+import java.math.BigInteger;
+
+import edu.pitt.apollo.apolloservice.translatorservice.TranslatorServiceRecordContainer;
+import edu.pitt.apollo.db.ApolloDbUtils;
+import edu.pitt.apollo.db.exceptions.ApolloDatabaseException;
+import edu.pitt.apollo.services_common.v3_0_0.Authentication;
+import edu.pitt.apollo.simulator_service_types.v3_0_0.RunSimulationMessage;
+
+public class DatabaseAccessorForRunningASingleSimulation extends
+		DatabaseAccessorForRunningSimulations {
+
+	public DatabaseAccessorForRunningASingleSimulation(
+			RunSimulationMessage runSimulationMessage,
+			Authentication authentication) {
+		super(authentication,
+				runSimulationMessage.getSimulatorIdentification(),
+				RunSimulationMessage.class);
+		this.runMessage = runSimulationMessage;
+	}
+
+	@Override
+	public BigInteger[] insertRunIntoDatabase(
+			BigInteger memberOfSimulationGroupIdOrNull)
+			throws ApolloDatabaseException {
+		int md5CollisionId = dbUtils.getHighestMD5CollisionIdForRun(runMessage) + 1;
+
+		BigInteger[] runIdsimulationGroupId = dbUtils.addSimulationRun(
+				runMessage,
+				memberOfSimulationGroupIdOrNull, md5CollisionId,
+				((RunSimulationMessage) runMessage)
+						.getSimulatorIdentification(),
+				TranslatorServiceRecordContainer
+						.getTranslatorSoftwareIdentification(), authentication);
+		return runIdsimulationGroupId;
+	}
+}
