5d690c452791ae1138b3c2dd8786b608a41837de
john.d.levander <jdl50@pitt.edu>
2015-02-11 19:48:34 +0000

    More updates for batch service

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/database/DatabaseAccessor.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/database/DatabaseAccessor.java
@@ -3,18 +3,17 @@ package edu.pitt.apollo.apolloservice.database;
 import edu.pitt.apollo.db.ApolloDbUtils;
 import edu.pitt.apollo.db.exceptions.ApolloDatabaseException;
 import edu.pitt.apollo.services_common.v3_0_0.Authentication;
+import edu.pitt.apollo.services_common.v3_0_0.RunIdentificationAndLabel;
 import edu.pitt.apollo.services_common.v3_0_0.SoftwareIdentification;
+
 import java.math.BigInteger;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  *
- * Author: Nick Millett
- * Email: nick.millett@gmail.com
- * Date: May 7, 2014
- * Time: 2:26:02 PM
- * Class: DatabaseAccessor
- * IDE: NetBeans 6.9.1
+ * Author: Nick Millett Email: nick.millett@gmail.com Date: May 7, 2014 Time:
+ * 2:26:02 PM Class: DatabaseAccessor IDE: NetBeans 6.9.1
  */
 public abstract class DatabaseAccessor {
 
@@ -30,50 +29,75 @@ public abstract class DatabaseAccessor {
 		this.authentication = authentication;
 	}
 
-	public void removeAllDataAssociatedWithRunId(BigInteger runId) throws ApolloDatabaseException {
+	public void removeAllDataAssociatedWithRunId(BigInteger runId)
+			throws ApolloDatabaseException {
 		dbUtils.removeRunData(runId);
 	}
 
-	public boolean authenticateUser(Authentication authentication) throws ApolloDatabaseException {
+	public boolean authenticateUser(Authentication authentication)
+			throws ApolloDatabaseException {
 		return dbUtils.authenticateUser(authentication);
 	}
 
-	public boolean authorizeUserForSoftwareCacheData(Authentication authentication, SoftwareIdentification softwareIdentification) throws ApolloDatabaseException {
+	public boolean authorizeUserForSoftwareCacheData(
+			Authentication authentication,
+			SoftwareIdentification softwareIdentification)
+			throws ApolloDatabaseException {
 		boolean requestRunSimulator = false;
-		return dbUtils.authorizeUser(authentication, softwareIdentification, requestRunSimulator);
+		return dbUtils.authorizeUser(authentication, softwareIdentification,
+				requestRunSimulator);
 	}
 
-	public boolean authorizeUserForRunningSoftware(Authentication authentication, SoftwareIdentification softwareIdentification) throws ApolloDatabaseException {
+	public boolean authorizeUserForRunningSoftware(
+			Authentication authentication,
+			SoftwareIdentification softwareIdentification)
+			throws ApolloDatabaseException {
 		boolean requestRunSimulator = true;
-		return dbUtils.authorizeUser(authentication, softwareIdentification, requestRunSimulator);
+		return dbUtils.authorizeUser(authentication, softwareIdentification,
+				requestRunSimulator);
 	}
 
-	protected final boolean isRunIdAssociatedWithMatchingRunMessage(String targetRunMessageAsJson,
-			BigInteger runIdAssociatedWithRunMessageHash) throws ApolloDatabaseException {
+	protected final boolean isRunIdAssociatedWithMatchingRunMessage(
+			String targetRunMessageAsJson,
+			BigInteger runIdAssociatedWithRunMessageHash)
+			throws ApolloDatabaseException {
 
-		String runSimulationMessageAssociatedWithRunIdAsJson
-				= getRunMessageAssociatedWithRunIdAsJsonOrNull(runIdAssociatedWithRunMessageHash);
+		String runSimulationMessageAssociatedWithRunIdAsJson = getRunMessageAssociatedWithRunIdAsJsonOrNull(runIdAssociatedWithRunMessageHash);
 
 		if (runSimulationMessageAssociatedWithRunIdAsJson == null) {
 			throw new ApolloDatabaseException(
 					"There was no run_simulation_message.json content for run ID "
-					+ runIdAssociatedWithRunMessageHash);
+							+ runIdAssociatedWithRunMessageHash);
 		}
 
-		return targetRunMessageAsJson.equals(runSimulationMessageAssociatedWithRunIdAsJson);
+		return targetRunMessageAsJson
+				.equals(runSimulationMessageAssociatedWithRunIdAsJson);
 	}
 
-	public List<BigInteger> getRunIdsAssociatedWithRun(BigInteger runId) throws ApolloDatabaseException {
+	public List<BigInteger> getRunIdsAssociatedWithRun(BigInteger runId)
+			throws ApolloDatabaseException {
 		return dbUtils.getRunIdsForBatch(runId);
 	}
-	
-	public abstract BigInteger getCachedRunIdFromDatabaseOrNull() throws ApolloDatabaseException;
 
-	protected abstract String getRunMessageAssociatedWithRunIdAsJsonOrNull(BigInteger runId) throws ApolloDatabaseException;
+	public void addRunIdsToSimulationGroup(
+			BigInteger simulationGroupId,
+			List<RunIdentificationAndLabel> simulationRunIdentificationsAndLabels) throws ClassNotFoundException, SQLException {
+		dbUtils.addRunIdsToSimulationGroup(simulationGroupId,
+				simulationRunIdentificationsAndLabels);
+	}
 
-	public abstract BigInteger[] insertRunIntoDatabase(BigInteger memberOfSimulationGroupIdOrNull) throws ApolloDatabaseException;
+	public abstract BigInteger getCachedRunIdFromDatabaseOrNull()
+			throws ApolloDatabaseException;
 
-	public BigInteger getSimulationGroupIdForRun(BigInteger runId) throws ApolloDatabaseException {
+	protected abstract String getRunMessageAssociatedWithRunIdAsJsonOrNull(
+			BigInteger runId) throws ApolloDatabaseException;
+
+	public abstract BigInteger[] insertRunIntoDatabase(
+			BigInteger memberOfSimulationGroupIdOrNull)
+			throws ApolloDatabaseException;
+
+	public BigInteger getSimulationGroupIdForRun(BigInteger runId)
+			throws ApolloDatabaseException {
 		return dbUtils.getSimulationGroupIdForRun(runId);
 	}
 }
