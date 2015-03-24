75cda772b1b313b8f529e7042e4751eb26f4d1fb
john.d.levander@gmail.com <JohnLevander@users.noreply.github.com>
2015-03-24 15:20:23 +0000

    Performance improvements

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/translatorservice/TranslatorServiceAccessor.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/translatorservice/TranslatorServiceAccessor.java
@@ -29,11 +29,38 @@ import java.sql.SQLException;
  */
 public class TranslatorServiceAccessor {
 
+
+    static TranslatorServiceEI translatorPortSingleton = null;
+    private static  TranslatorServiceEI getTranslatorPort(BigInteger runId) throws IOException {
+        if (translatorPortSingleton != null)
+            return translatorPortSingleton;
+
+        ServiceRegistrationRecord translatorServiceRecord = TranslatorServiceRecordContainer.getTranslatorServiceRegistrationRecord();
+
+        try {
+            translatorPortSingleton = new TranslatorServiceV300(new URL(translatorServiceRecord.getUrl())).getTranslatorServiceEndpoint();
+            return translatorPortSingleton;
+        } catch (WebServiceException e) {
+            ApolloServiceErrorHandler.writeErrorToErrorFile("WebServiceException attempting to get the translator port for runId " + runId
+                            + ". URL was " + translatorServiceRecord.getUrl() + ". Error message was: " + e.getMessage(),
+                    runId);
+            return null;
+        } catch (MalformedURLException ex) {
+            ApolloServiceErrorHandler.writeErrorToErrorFile("MalformedURLEXception attempting to get the translator port for runId " + runId
+                            + ". URL was " + translatorServiceRecord.getUrl() + ". Error message was: " + ex.getMessage(),
+                    runId);
+            return null;
+        }
+
+    }
+
     public static boolean runTranslatorAndReturnIfRunWasSuccessful(BigInteger runId, ApolloDbUtils dbUtils) throws IOException {
         ServiceRegistrationRecord translatorServiceRecord = TranslatorServiceRecordContainer.getTranslatorServiceRegistrationRecord();
         TranslatorServiceEI translatorPort;
         try {
-            translatorPort = new TranslatorServiceV300(new URL(translatorServiceRecord.getUrl())).getTranslatorServiceEndpoint();
+            translatorPort = getTranslatorPort(runId);
+            if (translatorPort == null)
+                return false;
         } catch (WebServiceException e) {
             ApolloServiceErrorHandler.writeErrorToErrorFile("WebServiceException attempting to get the translator port for runId " + runId
                     + ". URL was " + translatorServiceRecord.getUrl() + ". Error message was: " + e.getMessage(),
@@ -47,12 +74,12 @@ public class TranslatorServiceAccessor {
         }
 
         // disable chunking for ZSI
-        Client client = ClientProxy.getClient(translatorPort);
-        HTTPConduit http = (HTTPConduit) client.getConduit();
-        HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
-        httpClientPolicy.setConnectionTimeout(36000);
-        httpClientPolicy.setAllowChunking(false);
-        http.setClient(httpClientPolicy);
+//        Client client = ClientProxy.getClient(translatorPort);
+//        HTTPConduit http = (HTTPConduit) client.getConduit();
+//        HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+//        httpClientPolicy.setConnectionTimeout(36000);
+//        httpClientPolicy.setAllowChunking(false);
+//        http.setClient(httpClientPolicy);
 
         try {
             translatorPort.translateRun(runId);
