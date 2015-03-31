7ae84bf719e8767ec245fd4a6ef88d4bb2c0290a
nick.millett@gmail.com <nickmillett@users.noreply.github.com>
2015-03-31 22:55:19 +0000

    took out unused file specifications from apollo_db_schema updated ApolloDbUtils to create a connection and close it in every method, also to not throw SQLExceptions and ClassNotFoundExceptions anymore moved ApolloServiceConstants to services-common

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/translatorservice/TranslatorServiceRecordContainer.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/translatorservice/TranslatorServiceRecordContainer.java
@@ -4,8 +4,8 @@ import edu.pitt.apollo.apolloservice.database.ApolloDbUtilsContainer;
 import edu.pitt.apollo.db.ApolloDbUtils;
 import edu.pitt.apollo.services_common.v3_0_0.ServiceRegistrationRecord;
 import edu.pitt.apollo.services_common.v3_0_0.SoftwareIdentification;
-import java.sql.SQLException;
 import java.util.Map;
+import edu.pitt.apollo.db.exceptions.ApolloDatabaseException;
 
 /**
  *
@@ -18,42 +18,39 @@ import java.util.Map;
  */
 public class TranslatorServiceRecordContainer {
 
-    private static final ServiceRegistrationRecord TRANSLATOR_SERVICE_RECORD;
+	private static final ServiceRegistrationRecord TRANSLATOR_SERVICE_RECORD;
 
-    static {
-        ApolloDbUtils dbUtils = ApolloDbUtilsContainer.getApolloDbUtils();
+	static {
+		ApolloDbUtils dbUtils = ApolloDbUtilsContainer.getApolloDbUtils();
 
-        ServiceRegistrationRecord tempTranslatorServiceRecord = null;
-        try {
-            Map<Integer, ServiceRegistrationRecord> softwareIdMap = dbUtils.getRegisteredSoftware();
-            for (Integer id : softwareIdMap.keySet()) {
-                SoftwareIdentification softwareId = softwareIdMap.get(id).getSoftwareIdentification();
-                if (softwareId.getSoftwareName().toLowerCase().equals("translator")) {
-                    tempTranslatorServiceRecord = softwareIdMap.get(id);
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            throw new ExceptionInInitializerError("ClassNotFoundException attempting to load the translator service record: "
-                    + ex.getMessage());
-        } catch (SQLException ex) {
-            throw new ExceptionInInitializerError("SQLException attempting to load the translator service record: "
-                    + ex.getMessage());
-        }
+		ServiceRegistrationRecord tempTranslatorServiceRecord = null;
+		try {
+			Map<Integer, ServiceRegistrationRecord> softwareIdMap = dbUtils.getRegisteredSoftware();
+			for (Integer id : softwareIdMap.keySet()) {
+				SoftwareIdentification softwareId = softwareIdMap.get(id).getSoftwareIdentification();
+				if (softwareId.getSoftwareName().toLowerCase().equals("translator")) {
+					tempTranslatorServiceRecord = softwareIdMap.get(id);
+					break;
+				}
+			}
+		} catch (ApolloDatabaseException ex) {
+			throw new ExceptionInInitializerError("ApolloDatabaseException attempting to load the translator service record: "
+					+ ex.getMessage());
+		}
 
-        if (tempTranslatorServiceRecord == null) {
-            throw new ExceptionInInitializerError("Translator ServiceRegistrationRecord object could not be initialized");
-        }
+		if (tempTranslatorServiceRecord == null) {
+			throw new ExceptionInInitializerError("Translator ServiceRegistrationRecord object could not be initialized");
+		}
 
-        TRANSLATOR_SERVICE_RECORD = tempTranslatorServiceRecord;
+		TRANSLATOR_SERVICE_RECORD = tempTranslatorServiceRecord;
 
-    }
+	}
 
-    public static SoftwareIdentification getTranslatorSoftwareIdentification() {
-        return TRANSLATOR_SERVICE_RECORD.getSoftwareIdentification();
-    }
+	public static SoftwareIdentification getTranslatorSoftwareIdentification() {
+		return TRANSLATOR_SERVICE_RECORD.getSoftwareIdentification();
+	}
 
-    public static ServiceRegistrationRecord getTranslatorServiceRegistrationRecord() {
-        return TRANSLATOR_SERVICE_RECORD;
-    }
+	public static ServiceRegistrationRecord getTranslatorServiceRegistrationRecord() {
+		return TRANSLATOR_SERVICE_RECORD;
+	}
 }
