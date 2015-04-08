b35acac6539086c59c674b174304ff9d43882921
JDL50 <jdl50@pitt.edu>
2015-04-08 18:34:20 -0400

    adding a thread pool to the staging code

--- apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/thread/RunApolloServiceThread.java
+++ apollo-ws/service-skeletons/java/trunk/broker-service/src/main/java/edu/pitt/apollo/apolloservice/thread/RunApolloServiceThread.java
@@ -1,13 +1,11 @@
 package edu.pitt.apollo.apolloservice.thread;
 
-import edu.pitt.apollo.apolloservice.database.ApolloDbUtilsContainer;
-import edu.pitt.apollo.db.ApolloDbUtils;
-import java.math.BigInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigInteger;
+
 /**
- *
  * Author: Nick Millett
  * Email: nick.millett@gmail.com
  * Date: Jul 30, 2014
@@ -16,18 +14,14 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class RunApolloServiceThread extends Thread {
 
-	static Logger logger = LoggerFactory.getLogger(RunApolloServiceThread.class);
-	protected static final ApolloDbUtils dbUtils;
-	protected BigInteger runId;
+    static Logger logger = LoggerFactory.getLogger(RunApolloServiceThread.class);
+    protected BigInteger runId;
 
-	public RunApolloServiceThread(BigInteger runId) {
-		this.runId = runId;
-	}
+    public RunApolloServiceThread(BigInteger runId) {
+        this.runId = runId;
+    }
 
-	public abstract void setAuthenticationPasswordFieldToBlank();
+    public abstract void setAuthenticationPasswordFieldToBlank();
 
-	static {
-		dbUtils = ApolloDbUtilsContainer.getApolloDbUtils();
-	}
 
 }
