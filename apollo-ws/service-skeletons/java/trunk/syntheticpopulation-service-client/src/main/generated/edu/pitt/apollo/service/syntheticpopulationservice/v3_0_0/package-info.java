da8ce56cd4b527d5c57fc6983f0dbadc8fce85e0
TPS23 <tps23@pitt.edu>
2015-05-15 13:01:11 -0400

    Removed generated source files

--- apollo-ws/service-skeletons/java/trunk/syntheticpopulation-service-client/src/main/generated/edu/pitt/apollo/service/syntheticpopulationservice/v3_0_0/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.apollo.pitt.edu/syntheticpopulationservice/v3_0_0/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package edu.pitt.apollo.service.syntheticpopulationservice.v3_0_0;
