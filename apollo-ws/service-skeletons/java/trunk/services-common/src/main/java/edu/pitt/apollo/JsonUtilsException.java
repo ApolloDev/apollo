3223e191050c9fee00b23314239218d4d258efe7
JohnLevander <jdl50@pitt.edu>
2015-05-18 17:15:29 -0400

    Still developing the batch functionality.  Solved a major bug but there is still work to be done.

---
+++ apollo-ws/service-skeletons/java/trunk/services-common/src/main/java/edu/pitt/apollo/JsonUtilsException.java
@@ -0,0 +1,26 @@
+package edu.pitt.apollo;
+
+/**
+ * Created by jdl50 on 5/15/15.
+ */
+public class JsonUtilsException extends Exception {
+    public JsonUtilsException() {
+        super();
+    }
+
+    public JsonUtilsException(String message) {
+        super(message);
+    }
+
+    public JsonUtilsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public JsonUtilsException(Throwable cause) {
+        super(cause);
+    }
+
+    protected JsonUtilsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
