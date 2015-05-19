42513879f3aecb2a99fd5d512ccf2f49a15106f2
JohnLevander <jdl50@pitt.edu>
2015-05-19 11:39:28 -0400

    Trying to get this to compile

--- apollo-ws/service-skeletons/java/trunk/data-service/src/main/java/edu/pitt/apollo/dataservice/methods/GetSoftwareIdentificationKeyForRunMethod.java
+++ apollo-ws/service-skeletons/java/trunk/data-service/src/main/java/edu/pitt/apollo/dataservice/methods/GetSoftwareIdentificationKeyForRunMethod.java
@@ -3,7 +3,7 @@ package edu.pitt.apollo.dataservice.methods;
 import edu.pitt.apollo.data_service_types.v3_0_0.*;
 import edu.pitt.apollo.db.ApolloDbUtils;
 import edu.pitt.apollo.db.exceptions.ApolloDatabaseException;
-import edu.pitt.apollo.service.dataservice.v3_0_0.GetSoftwareIdentificationKeyForRun;
+//import edu.pitt.apollo.service.dataservice.v3_0_0.GetSoftwareIdentificationKeyForRun;
 import edu.pitt.apollo.services_common.v3_0_0.MethodCallStatus;
 import edu.pitt.apollo.services_common.v3_0_0.MethodCallStatusEnum;
 import edu.pitt.apollo.services_common.v3_0_0.SoftwareIdentification;
