6fc76b2756af4d2d699462a60812f9e5e5783497
JohnLevander <jdl50@pitt.edu>
2015-06-02 15:46:08 -0400

    Adding a simple test for the rest service

--- apollo-ws/service-skeletons/java/trunk/data-service-client/src/main/java/edu/pitt/apollo/dataserviceclient/BestRestTestClient.java
+++ apollo-ws/service-skeletons/java/trunk/data-service-client/src/main/java/edu/pitt/apollo/dataserviceclient/BestRestTestClient.java
@@ -1,7 +1,23 @@
 package edu.pitt.apollo.dataserviceclient;
 
+import edu.pitt.apollo.DataServiceImpl;
+import edu.pitt.apollo.db.exceptions.ApolloDatabaseException;
+import edu.pitt.apollo.examples.runsimulationmessages.ExampleInfectiousDiseaseScenario;
+import edu.pitt.apollo.simulator_service_types.v3_0_0.RunSimulationMessage;
+
 /**
  * Created by jdl50 on 6/2/15.
  */
 public class BestRestTestClient {
+
+    private static final String REST_DATA_SERVICE_TEST_CLIENT_PROPERTIES_FILE = "data_service_test_client.properties";
+
+    public static void main(String[] args) {
+        RunSimulationMessage runSimulationMessage =
+                ApolloServiceTypeFactory.getMinimalistRunSimulationMessage(ApolloServiceTypeFactory.SimulatorIdentificationEnum.FRED);
+
+        DataServiceImpl dataService = new DataServiceImpl();
+        dataService.insertRun(runSimulationMessage, "");
+
+    }
 }
