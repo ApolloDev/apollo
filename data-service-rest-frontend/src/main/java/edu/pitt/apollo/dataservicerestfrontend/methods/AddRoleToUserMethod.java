134f8b9aeb9ffabfa4132870f9c27ed9fb0750e6
nickmillett <nick.millett@gmail.com>
2015-06-26 18:17:03 -0400

    more fixes to get the services working data rest service and run manager rest service now have different package names

---
+++ data-service-rest-frontend/src/main/java/edu/pitt/apollo/dataservicerestfrontend/methods/AddRoleToUserMethod.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pitt.apollo.dataservicerestfrontend.methods;
+
+import edu.pitt.apollo.data_service_types.v3_0_0.AddRoleToUserMessage;
+import edu.pitt.apollo.exception.DataServiceException;
+import edu.pitt.apollo.exception.DeserializationException;
+import edu.pitt.apollo.exception.SerializationException;
+import edu.pitt.apollo.exception.UnsupportedSerializationFormatException;
+import edu.pitt.apollo.dataservicerestfrontend.utils.ResponseMessageBuilder;
+import edu.pitt.apollo.services_common.v3_0_0.ObjectSerializationInformation;
+import edu.pitt.apollo.services_common.v3_0_0.Request;
+import edu.pitt.apollo.services_common.v3_0_0.RequestMeta;
+import edu.pitt.apollo.services_common.v3_0_0.SerializationFormat;
+import edu.pitt.apollo.services_common.v3_0_0.SoftwareIdentification;
+import edu.pitt.apollo.utilities.Deserializer;
+import edu.pitt.apollo.utilities.DeserializerFactory;
+import edu.pitt.apollo.utilities.XMLDeserializer;
+import org.springframework.http.HttpStatus;
+
+/**
+ *
+ * @author nem41
+ */
+public class AddRoleToUserMethod extends BaseDataServiceAccessorMethod {
+
+	public AddRoleToUserMethod(String username, String password, SerializationFormat serializationFormat) throws UnsupportedSerializationFormatException {
+		super(username, password, serializationFormat);
+	}
+
+	public String addRoleToUser(String messageBody) throws UnsupportedSerializationFormatException, SerializationException {
+
+		try {
+			Request requestMessageObject = new XMLDeserializer().getObjectFromMessage(messageBody, Request.class);
+			RequestMeta meta = requestMessageObject.getRequestMeta();
+			ObjectSerializationInformation config = meta.getRequestBodySerializationInformation();
+
+			SerializationFormat format = config.getFormat();
+			Deserializer deserializer = DeserializerFactory.getDeserializer(format);
+
+			String className = config.getClassName();
+			String classNamespace = config.getClassNameSpace();
+
+			Object object = deserializer.getObjectFromMessage(messageBody, className, classNamespace);
+
+			if (!(object instanceof AddRoleToUserMessage)) {
+				responseBuilder.setStatus(HttpStatus.BAD_REQUEST, "The object in the message body was not an instance of AddRoleToUserMessage");
+			} else {
+				AddRoleToUserMessage message = (AddRoleToUserMessage) object;
+
+				String usernameForRole = message.getUserName();
+				String userPasswordForRole = message.getUserPassword();
+				SoftwareIdentification softwareId = message.getSoftwareIdentification();
+				boolean canRunSoftware = message.isCanRun();
+				boolean canRequestPrivileged = message.isCanRequestPrivileged();
+
+				try {
+					impl.addUserRole(usernameForRole, userPasswordForRole, softwareId, canRunSoftware, canRequestPrivileged, authentication);
+
+					responseBuilder.setStatus(HttpStatus.OK, ResponseMessageBuilder.DEFAULT_SUCCESS_MESSAGE);
+				} catch (DataServiceException ex) {
+					responseBuilder.setStatus(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+				}
+			}
+		} catch (DeserializationException | UnsupportedSerializationFormatException ex) {
+			responseBuilder.setStatus(HttpStatus.OK, ex.getMessage());
+		}
+
+		return serializer.serializeObject(responseBuilder.getResponse());
+	}
+
+}
---
+++ run-manager-service-rest-frontend/src/main/java/edu/pitt/apollo/runmanagerservicerestfrontend/controller/RunsController.java
@@ -0,0 +1,194 @@
+package edu.pitt.apollo.runmanagerservicerestfrontend.controller;
+
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+import edu.pitt.apollo.exception.SerializationException;
+import edu.pitt.apollo.exception.UnsupportedSerializationFormatException;
+import edu.pitt.apollo.runmanagerservicerestfrontend.exception.UnsupportedRunActionException;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.AddRunIdsToSimulationGroupForRun;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.DeleteRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.GetLastServiceToBeCalledForRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.GetRunIdsInSimulationGroupForRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.GetSoftwareIdentificationForRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.GetStatusOfRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.InsertRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.StartRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.SetLastServiceToBeCalledForRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.SetStatusOfRunMethod;
+import edu.pitt.apollo.runmanagerservicerestfrontend.methods.TerminateRunMethod;
+import edu.pitt.apollo.services_common.v3_0_0.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.GET;
+import java.math.BigInteger;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+
+/**
+ * Created by dcs27 on 5/15/15. Purpose: This class contains the RESTful interfaces associated with the runs collection.
+ */
+@Controller
+@RequestMapping("/ws")
+public class RunsController {
+	
+	@GET
+	@ApiOperation(value = "Get simulation group IDs for run.", notes = "Returns the simulation group IDs for run using a comma separated input.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}/rungroup", method = RequestMethod.GET, headers = "Accept=application/xml")
+	public @ResponseBody
+	String getRunIdsInSimulationGroup(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException {
+		
+		return new GetRunIdsInSimulationGroupForRunMethod(username, password, SerializationFormat.XML).getRunIdsInSimulationGroupForRun(runId);
+	}
+	
+	@GET
+	@ApiOperation(value = "Get software identification.", notes = "Returns the software identification for the given run ID.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}/softwareIdentification", method = RequestMethod.GET, headers = "Accept=application/xml")
+	public @ResponseBody
+	String getSoftwareIdentificationForRun(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException {
+		
+		return new GetSoftwareIdentificationForRunMethod(username, password, SerializationFormat.XML).getSoftwareIdentificationForRun(runId);
+	}
+	
+	@POST
+	@ApiOperation(value = "Insert run.", notes = "Inserts a given run into the runs collection.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/runs", method = RequestMethod.POST, headers = "Accept=application/xml")
+	public @ResponseBody
+	String postRunToRunsCollection(@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password,
+			@ApiParam(value = "Run message.", required = true) @RequestBody String messageBody) throws UnsupportedSerializationFormatException, SerializationException {
+		return new InsertRunMethod(username, password, SerializationFormat.XML).insertRun(messageBody);
+	}
+	
+	@POST
+	@ApiOperation(value = "Set status.", notes = "Sets the status of a given run ID using a MethodCallStatusEnum and status message.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}/status", method = RequestMethod.POST, headers = "Accept=application/xml")
+	public @ResponseBody
+	String updateStatusOfRun(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Method call status enum", required = true) @RequestParam("methodCallStatusEnum") MethodCallStatusEnum statusToUpdateTo,
+			@ApiParam(value = "Status message", required = true) @RequestParam("statusMessage") String statusMessage,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException {
+		
+		return new SetStatusOfRunMethod(username, password, SerializationFormat.XML).setStatusOfRun(runId, statusToUpdateTo, username);
+	}
+	
+	@POST
+	@ApiOperation(value = "Set the last service to be called for the run.", notes = "Sets the software identification for the last service to be called for a given run ID.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}/lastServiceToBeCalled", method = RequestMethod.POST, headers = "Accept=application/xml")
+	public @ResponseBody
+	String updateLastServiceToBeCalledForRun(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Software name", required = true) @RequestParam("softwareName") String softwareName,
+			@ApiParam(value = "Software version", required = true) @RequestParam("softwareVersion") String softwareVersion,
+			@ApiParam(value = "Software developer", required = true) @RequestParam("softwareDeveloper") String softwareDeveloper,
+			@ApiParam(value = "Apollo software type enum", required = true) @RequestParam("softwareTypeEnum") ApolloSoftwareTypeEnum softwareTypeEnum,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException {
+		
+		return new SetLastServiceToBeCalledForRunMethod(username, password, SerializationFormat.XML).setLastServiceToBeCalledForRunMethod(runId, softwareName,
+				softwareVersion, softwareDeveloper, softwareTypeEnum);
+	}
+	
+	@GET
+	@ApiOperation(value = "Get last service to be called.", notes = "Returns the software identification for the last service to be called for a given run ID.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}/lastServiceToBeCalled", method = RequestMethod.GET, headers = "Accept=application/xml")
+	public @ResponseBody
+	String getLastServiceToBeCalledForRun(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException {
+		
+		return new GetLastServiceToBeCalledForRunMethod(username, password, SerializationFormat.XML).getLastServiceToBeCalledForRun(runId);
+	}
+	
+	@POST
+	@ApiOperation(value = "Set simulation group IDs for run.", notes = "Sets the simulation group IDs for run using a comma separated input.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}/rungroup", method = RequestMethod.POST, headers = "Accept=application/xml")
+	public @ResponseBody
+	String addRunIdsToSimulationGroupForRun(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "List of run IDs to associate.", required = true) @RequestParam("runIdsToAssociate") String runIdsToAssociate,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException {
+		
+		return new AddRunIdsToSimulationGroupForRun(username, password, SerializationFormat.XML).addRunIdsToSimulationGroupForRun(runId, runIdsToAssociate);
+		
+	}
+	
+	@DELETE
+	@ApiOperation(value = "Delete run.", notes = "Deletes run associated with the given run ID from the system. A user will need to have proper authentication to perform this task.", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}", method = RequestMethod.DELETE, headers = "Accept=application/xml")
+	public @ResponseBody
+	String deleteRun(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws SerializationException, UnsupportedSerializationFormatException {
+		
+		return new DeleteRunMethod(username, password, SerializationFormat.XML).deleteRun(runId);
+	}
+	
+	@GET
+	@ApiOperation(value = "Get status.", notes = "Returns the method call status and message for the given run ID", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}/status", method = RequestMethod.GET, headers = "Accept=application/xml")
+	public @ResponseBody
+	String getStatusOfRun(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException {
+		
+		return new GetStatusOfRunMethod(username, password, SerializationFormat.XML).getStatusForRun(runId);
+	}
+	
+	@POST
+	@ApiOperation(value = "Perform action on the run.", notes = "Performs an action on the run (start or terminate).", response = String.class)
+	@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "")
+	})
+	@RequestMapping(value = "/run/{runId}", method = RequestMethod.POST, headers = "Accept=application/xml")
+	public @ResponseBody
+	String run(@ApiParam(value = "Run ID.", required = true) @PathVariable("runId") BigInteger runId,
+			@ApiParam(value = "Action", required = true) @RequestParam("action") RunActionEnum action,
+			@ApiParam(value = "Username", required = true) @RequestParam("username") String username,
+			@ApiParam(value = "Password", required = true) @RequestParam("password") String password) throws UnsupportedSerializationFormatException, SerializationException, UnsupportedRunActionException {
+		
+		switch (action) {
+			case START:
+				return new StartRunMethod(username, password, SerializationFormat.XML).startRun(runId);
+			case TERMINATE:
+				return new TerminateRunMethod(username, password, SerializationFormat.XML).terminateRun(runId);
+			default:
+				throw new UnsupportedRunActionException("The specified run action value \"" + action + "\" is not recognized");
+		}
+		
+	}
+	
+}
