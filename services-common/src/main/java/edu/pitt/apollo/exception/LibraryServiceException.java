3dc7f0765c01a735336c4e75e6940ef46b5042f0
nickmillett <nick.millett@gmail.com>
2015-08-18 16:27:28 -0400

    added library service exception to library interface

---
+++ services-common/src/main/java/edu/pitt/apollo/exception/LibraryServiceException.java
@@ -0,0 +1,11 @@
+package edu.pitt.apollo.exception;
+
+/**
+ * Created by nem41 on 8/18/15.
+ */
+public class LibraryServiceException extends Exception {
+
+    public LibraryServiceException(String message) {
+        super(message);
+    }
+}
--- services-common/src/main/java/edu/pitt/apollo/interfaces/LibraryServiceInterface.java
+++ services-common/src/main/java/edu/pitt/apollo/interfaces/LibraryServiceInterface.java
@@ -1,5 +1,6 @@
 package edu.pitt.apollo.interfaces;
 
+import edu.pitt.apollo.exception.LibraryServiceException;
 import edu.pitt.apollo.library_service_types.v3_0_2.*;
 import edu.pitt.apollo.services_common.v3_0_2.Authentication;
 
@@ -10,32 +11,32 @@ import javax.xml.datatype.XMLGregorianCalendar;
  */
 public interface LibraryServiceInterface {
 
-    public QueryResult query(QueryMessage queryMessage);
+    public QueryResult query(QueryMessage queryMessage) throws LibraryServiceException;
 
-    public GetLibraryItemContainerResult getLibraryItemContainer(int urn, int version, Authentication authentication);
+    public GetLibraryItemContainerResult getLibraryItemContainer(int urn, int version, Authentication authentication) throws LibraryServiceException;
 
-    public UpdateLibraryItemContainerResult updateLibraryItemContainer(int urn, LibraryItemContainer libraryItemContainer, String comment, Authentication authentication);
+    public UpdateLibraryItemContainerResult updateLibraryItemContainer(int urn, LibraryItemContainer libraryItemContainer, String comment, Authentication authentication) throws LibraryServiceException;
 
-    public AddLibraryItemContainerResult addLibraryItemContainer(LibraryItemContainer libraryItemContainer, String comment, Authentication authentication);
+    public AddLibraryItemContainerResult addLibraryItemContainer(LibraryItemContainer libraryItemContainer, String comment, Authentication authentication) throws LibraryServiceException;
 
-    public GetReleaseVersionResult getLibraryItemReleaseVersion(int urn, Authentication authentication);
+    public GetReleaseVersionResult getLibraryItemReleaseVersion(int urn, Authentication authentication) throws LibraryServiceException;
 
-    public GetCommentsResult getCommentsForLibraryItem(int urn, int version, Authentication authentication);
+    public GetCommentsResult getCommentsForLibraryItem(int urn, int version, Authentication authentication) throws LibraryServiceException;
 
-    public GetVersionsResult getVersionNumbersForLibraryItem(int urn, Authentication authentication);
+    public GetVersionsResult getVersionNumbersForLibraryItem(int urn, Authentication authentication) throws LibraryServiceException;
 
-    public ModifyGroupOwnershipResult removeGroupAccessToLibraryItem(int urn, String group, Authentication authentication);
+    public ModifyGroupOwnershipResult removeGroupAccessToLibraryItem(int urn, String group, Authentication authentication) throws LibraryServiceException;
 
-    public GetLibraryItemURNsResult getLibraryItemURNs(String itemType, Authentication authentication);
+    public GetLibraryItemURNsResult getLibraryItemURNs(String itemType, Authentication authentication) throws LibraryServiceException;
 
-    public SetReleaseVersionResult setReleaseVersionForLibraryItem(int urn, int version, String comment, Authentication authentication);
+    public SetReleaseVersionResult setReleaseVersionForLibraryItem(int urn, int version, String comment, Authentication authentication) throws LibraryServiceException;
 
-    public ModifyGroupOwnershipResult grantGroupAccessToLibraryItem(int urn, String group, Authentication authentication);
+    public ModifyGroupOwnershipResult grantGroupAccessToLibraryItem(int urn, String group, Authentication authentication) throws LibraryServiceException;
 
-    public AddReviewerCommentResult addReviewerCommentToLibraryItem(int urn, int version, String comment, Authentication authentication);
+    public AddReviewerCommentResult addReviewerCommentToLibraryItem(int urn, int version, String comment, Authentication authentication) throws LibraryServiceException;
 
-    public SetLibraryItemAsNotReleasedResult setLibraryItemAsNotReleased(int urn, Authentication authentication);
+    public SetLibraryItemAsNotReleasedResult setLibraryItemAsNotReleased(int urn, Authentication authentication) throws LibraryServiceException;
 
-    public GetChangeLogForLibraryItemsModifiedSinceDateTimeResult getChangeLogForLibraryItemsModifiedSinceDateTime(XMLGregorianCalendar dateTime, Authentication authentication);
+    public GetChangeLogForLibraryItemsModifiedSinceDateTimeResult getChangeLogForLibraryItemsModifiedSinceDateTime(XMLGregorianCalendar dateTime, Authentication authentication) throws LibraryServiceException;
 
 }
