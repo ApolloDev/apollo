5a8de7ad667d5cc053e064da81cd8b3e77dbe4d3
Millett <nem41@nem41-mnb.local>
2016-10-20 10:25:51 -0400

    fixes for db schema and security manager

--- apollo_db_schema.sql
+++ apollo_db_schema.sql
@@ -5,9 +5,6 @@ All commands listed after "-- HSQLDB: END;" will NOT be run when this script is
 used in the unit tests. These commands are MySQL-specific and will not work with HSQLDB.
  */
 
-DROP VIEW IF EXISTS translator_output_content_view;
-DROP VIEW IF EXISTS software_input;
-
 DROP TABLE IF EXISTS run_status;
 DROP TABLE IF EXISTS run_status_description;
 DROP TABLE IF EXISTS simulation_group_definition;
@@ -43,34 +40,34 @@ CREATE TABLE software_identification (
 
 -- HSQLDB: ALTER TABLE software_identification ALTER COLUMN id RESTART WITH 1;
 
-INSERT INTO `software_identification` VALUES (1,'UPitt','Translator','1.0','translator','http://localhost:8080/translator-service-war-4.0-SNAPSHOT/services/translatorservice?wsdl',1, NULL, NULL, NULL, NULL),
-											 (2,'UPitt','SEIR','3.0','simulator','http://localhost:8080/pitt-simulator-service-4.0-SNAPSHOT/ws',1, NULL, NULL, NULL, NULL),
-											 (3,'UPitt,PSC,CMU','FRED','2.0.1_i','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-											 (4,'UPitt','Time Series Visualizer','1.0','visualizer','http://localhost:8080/visualizer-service-rest-frontend-4.0-SNAPSHOT/ws',1, NULL, NULL, NULL ,NULL),
-											 (5,'PSC','GAIA','1.0','visualizer','http://gaia.pha.psc.edu:13501/gaia?wsdl',1, NULL, NULL, NULL, NULL),
-											 (6,'Chao-FredHutchinsonCancerCenter','FluTE','1.15','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-											 (7,'UPitt','Anthrax','1.0','simulator','http://localhost:8080/pitt-simulator-service-war-4.0-SNAPSHOT/services/pittsimulatorservice?wsdl',1, NULL, NULL, NULL, NULL),
-                                             (8,'PSC','CLARA','0.5','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                                             (9,'Steve Bellan','Lancet Ebola','1.0','simulator','http://localhost:8080/pitt-simulator-service-war-4.0-SNAPSHOT/services/pittsimulatorservice?wsdl',1, 'Creative Commons Attribution 4.0 International License', '4.0', 'http://creativecommons.org/licenses/by/4.0/', 'The Lancet Ebola simulator was used to produce results for the following publication:\n\nBellan SE, JRC Pulliam, J Dushoff, and LA Meyers. Asymptomatic infection, natural immunity, and Ebola dynamics. Letter, submitted to Lancet.\n\nThe original source code for the simulator is available at https://github.com/ICI3D/Ebola. The code was changed for use with the Apollo Web Services to do the following:\n\n - Read initial compartment sizes, transmission and disease parameters from an external input file\n - Produce time series output files for each compartment\n - Compute a newly exposed time series from the cumulative exposed time series'),
-											 (10,'UPitt','Data Service','1.0','data','http://localhost:8080/data-service-war-4.0-SNAPSHOT/services/dataservice?wsdl',1, NULL, NULL, NULL, NULL),
-											 (11,'UPitt', 'Broker Service','4.0','broker','http://localhost:8080/broker-service-war-4.0-SNAPSHOT/services/apolloservice?wsdl',1, NULL, NULL, NULL ,NULL),
-                       (12,'any', 'any','any','endUserApplication','not_applicable',1, NULL, NULL, NULL ,NULL),
-                       (13,'UPitt', 'Run Manager','4.0','runManager','http://localhost:8080/run-manager-service-rest-frontend-4.0-SNAPSHOT/',1, NULL, NULL, NULL ,NULL),
-                       (14,'Swiss TPH','OpenMalaria','R0063','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (15,'Swiss TPH','OpenMalaria','R0065','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (16,'Swiss TPH','OpenMalaria','R0068','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (17,'Swiss TPH','OpenMalaria','R0111','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (18,'Swiss TPH','OpenMalaria','R0115','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (19,'Swiss TPH','OpenMalaria','R0121','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (20,'Swiss TPH','OpenMalaria','R0125','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (21,'Swiss TPH','OpenMalaria','R0131','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (22,'Swiss TPH','OpenMalaria','R0132','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (23,'Swiss TPH','OpenMalaria','R0133','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (24,'Swiss TPH','OpenMalaria','R0670','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (25,'Swiss TPH','OpenMalaria','R0674','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (26,'Swiss TPH','OpenMalaria','R0678','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (27,'Swiss TPH','OpenMalaria','base','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl',1, NULL, NULL, NULL, NULL),
-                       (28,'UPitt', 'Query Service','4.0','queryService','http://localhost:5000/',1, NULL, NULL, NULL ,NULL);
+INSERT INTO `software_identification` VALUES (1,'UPitt','Translator','1.0','translator','http://localhost:8080/translator-service-war-4.0-SNAPSHOT/services/translatorservice?wsdl', NULL, NULL, NULL, NULL),
+											 (2,'UPitt','SEIR','3.0','simulator','http://localhost:8080/pitt-simulator-service-4.0-SNAPSHOT/ws', NULL, NULL, NULL, NULL),
+											 (3,'UPitt,PSC,CMU','FRED','2.0.1_i','simulator','http://pscss.olympus.psc.edu:8097', NULL, NULL, NULL, NULL),
+											 (4,'UPitt','Time Series Visualizer','1.0','visualizer','http://localhost:8080/visualizer-service-rest-frontend-4.0-SNAPSHOT/ws', NULL, NULL, NULL ,NULL),
+											 (5,'PSC','GAIA','1.0','visualizer','http://gaia.pha.psc.edu:13501/gaia?wsdl', NULL, NULL, NULL, NULL),
+											 (6,'Chao-FredHutchinsonCancerCenter','FluTE','1.15','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+											 (7,'UPitt','Anthrax','1.0','simulator','http://localhost:8080/pitt-simulator-service-war-4.0-SNAPSHOT/services/pittsimulatorservice?wsdl', NULL, NULL, NULL, NULL),
+                        (8,'PSC','CLARA','0.5','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                        (9,'Steve Bellan','Lancet Ebola','1.0','simulator','http://localhost:8080/pitt-simulator-service-war-4.0-SNAPSHOT/services/pittsimulatorservice?wsdl', 'Creative Commons Attribution 4.0 International License', '4.0', 'http://creativecommons.org/licenses/by/4.0/', 'The Lancet Ebola simulator was used to produce results for the following publication:\n\nBellan SE, JRC Pulliam, J Dushoff, and LA Meyers. Asymptomatic infection, natural immunity, and Ebola dynamics. Letter, submitted to Lancet.\n\nThe original source code for the simulator is available at https://github.com/ICI3D/Ebola. The code was changed for use with the Apollo Web Services to do the following:\n\n - Read initial compartment sizes, transmission and disease parameters from an external input file\n - Produce time series output files for each compartment\n - Compute a newly exposed time series from the cumulative exposed time series'),
+											 (10,'UPitt','Data Service','1.0','data','http://localhost:8080/data-service-war-4.0-SNAPSHOT/services/dataservice?wsdl', NULL, NULL, NULL, NULL),
+											 (11,'UPitt', 'Broker Service','4.0','broker','http://localhost:8080/broker-service-war-4.0-SNAPSHOT/services/apolloservice?wsdl', NULL, NULL, NULL ,NULL),
+                       (12,'any', 'any','any','endUserApplication','not_applicable', NULL, NULL, NULL ,NULL),
+                       (13,'UPitt', 'Run Manager','4.0','runManager','http://localhost:8080/run-manager-service-rest-frontend-4.0-SNAPSHOT/', NULL, NULL, NULL ,NULL),
+                       (14,'Swiss TPH','OpenMalaria','R0063','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (15,'Swiss TPH','OpenMalaria','R0065','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (16,'Swiss TPH','OpenMalaria','R0068','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (17,'Swiss TPH','OpenMalaria','R0111','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (18,'Swiss TPH','OpenMalaria','R0115','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (19,'Swiss TPH','OpenMalaria','R0121','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (20,'Swiss TPH','OpenMalaria','R0125','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (21,'Swiss TPH','OpenMalaria','R0131','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (22,'Swiss TPH','OpenMalaria','R0132','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (23,'Swiss TPH','OpenMalaria','R0133','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (24,'Swiss TPH','OpenMalaria','R0670','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (25,'Swiss TPH','OpenMalaria','R0674','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (26,'Swiss TPH','OpenMalaria','R0678','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (27,'Swiss TPH','OpenMalaria','base','simulator','http://gaia.pha.psc.edu:8099/pscsimu?wsdl', NULL, NULL, NULL, NULL),
+                       (28,'UPitt', 'Query Service','4.0','queryService','http://127.0.0.1:5000', NULL, NULL, NULL ,NULL);
 
 CREATE TABLE simulation_groups (
   id INT NOT NULL AUTO_INCREMENT,
@@ -85,14 +82,14 @@ CREATE TABLE run (
   md5_hash_of_run_message CHAR(32) NOT NULL,
   md5_collision_id INT NOT NULL DEFAULT 1,
   software_id INT,
-  user_id INT NOT NULL,
+  requester_id INT NOT NULL,
   file_store_directory_hash CHAR(32) NOT NULL,
   simulation_group_id INT,
   last_service_to_be_called int,
   PRIMARY KEY (id),
   CONSTRAINT run_unique UNIQUE (md5_hash_of_run_message, md5_collision_id),
   CONSTRAINT fk_software_id FOREIGN KEY (software_id) REFERENCES software_identification(id),
-  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id),
+  CONSTRAINT fk_user_id FOREIGN KEY (requester_id) REFERENCES users (id),
   CONSTRAINT fk_simulation_group_id FOREIGN KEY (simulation_group_id) REFERENCES simulation_groups (id),
   CONSTRAINT fk_last_service_to_be_called FOREIGN KEY (last_service_to_be_called) REFERENCES software_identification (id)
 );
--- security-manager/src/main/java/edu/pitt/securitymanager/utilities/ApolloSecurityManager.java
+++ security-manager/src/main/java/edu/pitt/securitymanager/utilities/ApolloSecurityManager.java
@@ -1,17 +1,21 @@
 package edu.pitt.securitymanager.utilities;
 
-import com.auth0.jwt.JWTVerifier;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import edu.pitt.apollo.ApolloServiceConstants;
 import edu.pitt.apollo.db.ApolloDbUtils;
 import edu.pitt.apollo.db.exceptions.ApolloDatabaseException;
-import edu.pitt.apollo.services_common.v4_0.ServiceRecord;
-import edu.pitt.securitymanager.exception.ApolloSecurityException;
 import edu.pitt.apollo.services_common.v4_0.Authentication;
+import edu.pitt.apollo.services_common.v4_0.ServiceRecord;
 import edu.pitt.apollo.types.v4_0.SoftwareIdentification;
+import edu.pitt.securitymanager.exception.ApolloSecurityException;
 import edu.pitt.securitymanager.exception.UserNotAuthorizedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.postgresql.util.Base64;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -28,16 +32,22 @@ import java.util.*;
  */
 public class ApolloSecurityManager {
 
+    private static final String APOLLO_CLIENT_ID_PROPERTY = "apollo_client_id";
+    private static final String APOLLO_CLIENT_ID;
     private static final String APOLLO_CLIENT_SECRET;
     private static final String APOLLO_CLIENT_SECRET_PROPERTY = "apollo_client_secret";
     private static final String HUB_URL_PROPERTY = "hub_url";
     private static final String HUB_URL;
+    private static final String AUTH0_URL_PROPERTY = "auth0_url";
+    private static final String AUTH0_URL;
     private static final String USER_ID_KEY = "sub";
     private static final String APP_METADATA_KEY = "appMetadata";
     private static final String ALLOWED_SERVICES_KEY = "allowedServices";
     private static final String SOFTWARE_DEVELOPER_KEY = "d";
     private static final String SOFTWARE_NAME_KEY = "n";
     private static final String SOFTWARE_VERSION_KEY = "v";
+    private static final String IS_SERVICE_KEY = "isService";
+    private static final Map<String, String> userDelegatedTokenMap = new HashMap<>();
 
     static {
 
@@ -46,6 +56,8 @@ public class ApolloSecurityManager {
             securityProperties.load(new FileInputStream(ApolloServiceConstants.APOLLO_DIR + "apollo_security.properties"));
             APOLLO_CLIENT_SECRET = securityProperties.getProperty(APOLLO_CLIENT_SECRET_PROPERTY);
             HUB_URL = securityProperties.getProperty(HUB_URL_PROPERTY);
+            AUTH0_URL = securityProperties.getProperty(AUTH0_URL_PROPERTY);
+            APOLLO_CLIENT_ID = securityProperties.getProperty(APOLLO_CLIENT_ID_PROPERTY);
         } catch (IOException ex) {
             throw new ExceptionInInitializerError("IOException: " + ex.getMessage());
         }
@@ -55,18 +67,19 @@ public class ApolloSecurityManager {
     private static class UserProfileData {
         public List<SoftwareIdentification> allowedSoftware;
         public String userId;
-        public boolean isService;
     }
 
     public static void authorizeUserForRunData(Authentication authentication, BigInteger runId) throws ApolloSecurityException {
-        UserProfileData userProfile = getUserProfileFromAuthentication(authentication);
+        Jws<Claims> claims = getClaims(authentication);
+        UserProfileData userProfile = getUserProfileFromClaims(claims);
         checkOwnershipOfRun(userProfile, runId);
-        setAuthenticationUserName(authentication, userProfile);
+        setAuthenticationUserName(authentication, userProfile.userId);
     }
 
     public static void authorizeUserForSpecifiedSoftware(Authentication authentication, SoftwareIdentification softwareId)
             throws ApolloSecurityException {
-        UserProfileData userProfile = getUserProfileFromAuthentication(authentication);
+        Jws<Claims> claims = getClaims(authentication);
+        UserProfileData userProfile = getUserProfileFromClaims(claims);
 
         boolean foundSoftware = false;
         for (SoftwareIdentification userSoftwareIdentification : userProfile.allowedSoftware) {
@@ -80,25 +93,29 @@ public class ApolloSecurityManager {
             throw new UserNotAuthorizedException("The user is not authorized to run the specified software");
         }
 
-        setAuthenticationUserName(authentication, userProfile);
+        setAuthenticationUserName(authentication, userProfile.userId);
     }
 
     public static void authorizeService(Authentication authentication) throws ApolloSecurityException {
-        UserProfileData userProfileData = getUserProfileFromAuthentication(authentication);
-        if (!userProfileData.isService) {
+        Jws<Claims> claims = getClaims(authentication);
+        if (!isService(claims)) {
             throw new ApolloSecurityException("Only services are allowed to execute this action");
+        } else {
+            setAuthenticationUserName(authentication, getUserNameFromClaims(claims));
         }
 
-        setAuthenticationUserName(authentication, userProfileData);
     }
 
     public static void filterSoftwareListForServiceOrUser(Authentication authentication,
                                                           List<ServiceRecord> serviceRecords) throws ApolloSecurityException {
-        UserProfileData userProfile = getUserProfileFromAuthentication(authentication);
-        if (!userProfile.isService) {
+
+        Jws<Claims> claims = getClaims(authentication);
+        if (!isService(claims)) {
+
+            UserProfileData userProfile = getUserProfileFromClaims(claims);
 
             List<SoftwareIdentification> userSoftwareList = userProfile.allowedSoftware;
-            for (Iterator<ServiceRecord> iterator = serviceRecords.iterator(); iterator.hasNext();) {
+            for (Iterator<ServiceRecord> iterator = serviceRecords.iterator(); iterator.hasNext(); ) {
                 boolean userAllowedToRun = false;
                 SoftwareIdentification software = iterator.next().getSoftwareIdentification();
                 for (SoftwareIdentification userSoftware : userSoftwareList) {
@@ -115,16 +132,21 @@ public class ApolloSecurityManager {
     }
 
     public static void authorizeServiceOrUserForRunData(Authentication authentication, BigInteger runId) throws ApolloSecurityException {
-        UserProfileData userProfile = getUserProfileFromAuthentication(authentication);
-        if (!userProfile.isService) {
+
+        Jws<Claims> claims = getClaims(authentication);
+        if (!isService(claims)) {
+            UserProfileData userProfile = getUserProfileFromClaims(claims);
             // check ownership
             checkOwnershipOfRun(userProfile, runId);
+            setAuthenticationUserName(authentication, userProfile.userId);
+        } else {
+            setAuthenticationUserName(authentication, getUserNameFromClaims(claims));
         }
-        setAuthenticationUserName(authentication, userProfile);
     }
 
     public static Authentication getUsernameAuthentication(Authentication authentication) throws ApolloSecurityException {
-        UserProfileData userProfileData = getUserProfileFromAuthentication(authentication);
+        Jws<Claims> claims = getClaims(authentication);
+        UserProfileData userProfileData = getUserProfileFromClaims(claims);
         Authentication newAuthentication = new Authentication();
         newAuthentication.setAuthorizationType(null);
         newAuthentication.setPayload(userProfileData.userId);
@@ -145,28 +167,97 @@ public class ApolloSecurityManager {
         }
     }
 
-    private static UserProfileData getUserProfileFromAuthentication(Authentication authentication) throws ApolloSecurityException {
-
-        String userId = null;
+    private static Jws<Claims> getClaims(Authentication authentication) throws ApolloSecurityException {
         switch (authentication.getAuthorizationType()) {
-
             case JWT:
                 try {
-                    final JWTVerifier verifier = new JWTVerifier(APOLLO_CLIENT_SECRET);
-                    final Map<String, Object> jwtData = verifier.verify(authentication.getPayload());
-
-                    userId = (String) jwtData.get(USER_ID_KEY);
-
-                } catch (Exception e) {
-                    // Invalid Token
-                    e.printStackTrace();
+                    Jws<Claims> claims = Jwts.parser().setSigningKey(Base64.encodeBytes(APOLLO_CLIENT_SECRET.getBytes())).parseClaimsJws(authentication.getPayload());
+                    return claims;
+                } catch (Exception ex) {
+                    throw new ApolloSecurityException("Exception verifying JWT: " + ex.getMessage());
                 }
-                break;
             case SSO:
-                break;
+                try {
+                    String token = getDelegatedToken(authentication.getPayload());
+                    Jws<Claims> claims = Jwts.parser().setSigningKey(APOLLO_CLIENT_SECRET).parseClaimsJws(token);
+                    return claims;
+                } catch (Exception ex) {
+                    throw new ApolloSecurityException("Exception verifying JWT: " + ex.getMessage());
+                }
+            default:
+                throw new ApolloSecurityException("Unrecognized authorization type");
         }
+    }
+
+    private static boolean isService(Jws<Claims> claims) {
+        Boolean isService = false;
+        if (claims.getBody().get(IS_SERVICE_KEY) != null) {
+            isService = Boolean.parseBoolean((String) claims.getBody().get(IS_SERVICE_KEY));
+        }
+        return isService;
+    }
+
+    private static String getUserNameFromClaims(Jws<Claims> claims) {
+        return (String) claims.getBody().get(USER_ID_KEY);
+    }
+
+    private static UserProfileData getUserProfileFromClaims(Jws<Claims> claims) throws ApolloSecurityException {
+
+        String userId = getUserNameFromClaims(claims);
 
         return getUserProfile(userId);
+    }
+
+    private static String getDelegatedToken(String userIdToken) throws ApolloSecurityException {
+
+        if (userDelegatedTokenMap.containsKey(userIdToken)) {
+            return userDelegatedTokenMap.get(userIdToken);
+        }
+
+        try {
+            URL obj = new URL(AUTH0_URL + "delegation");
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+
+            Gson gson = new Gson();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("client_id", APOLLO_CLIENT_ID);
+            jsonObject.addProperty("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
+            jsonObject.addProperty("id_token", userIdToken);
+            jsonObject.addProperty("target", APOLLO_CLIENT_ID);
+            jsonObject.addProperty("scope", "openid");
+            jsonObject.addProperty("api_type", "app");
+
+            String content = gson.toJson(jsonObject);
+
+
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.addRequestProperty("Content-Type", "application/" + "json");
+            con.setRequestProperty("Content-Length", Integer.toString(content.length()));
+            con.getOutputStream().write(content.getBytes("UTF8"));
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            String responseAsString = response.toString();
+            JsonParser parser = new JsonParser();
+            JsonObject newData = parser.parse(responseAsString).getAsJsonObject();
+            String newToken = newData.get("id_token").getAsString();
+
+            userDelegatedTokenMap.put(userIdToken, newToken);
+            return newToken;
+        } catch (IOException ex) {
+            throw new ApolloSecurityException("IOException: " + ex.getMessage());
+        }
+
     }
 
     private static UserProfileData getUserProfile(String userId) throws ApolloSecurityException {
@@ -194,6 +285,7 @@ public class ApolloSecurityManager {
             JsonObject userProfile = parser.parse(response.toString()).getAsJsonObject();
 
             JsonObject metadata = userProfile.get(APP_METADATA_KEY).getAsJsonObject();
+
             JsonArray allowedServices = metadata.get(ALLOWED_SERVICES_KEY).getAsJsonArray();
 
             List<SoftwareIdentification> softwareIdentificationList = new ArrayList<>();
@@ -210,7 +302,6 @@ public class ApolloSecurityManager {
             UserProfileData userProfileData = new UserProfileData();
             userProfileData.allowedSoftware = softwareIdentificationList;
             userProfileData.userId = userId;
-            userProfileData.isService = false;
             return userProfileData;
 
         } catch (IOException ex) {
@@ -219,15 +310,15 @@ public class ApolloSecurityManager {
     }
 
     private static boolean softwareEqual(SoftwareIdentification softwareIdentification1,
-                                  SoftwareIdentification softwareIdentification2) {
+                                         SoftwareIdentification softwareIdentification2) {
         return (softwareIdentification1.getSoftwareName().equals(softwareIdentification2.getSoftwareName())
                 && softwareIdentification1.getSoftwareDeveloper().equals(softwareIdentification2.getSoftwareDeveloper())
                 && softwareIdentification1.getSoftwareVersion().equals(softwareIdentification2.getSoftwareVersion()));
 
     }
 
-    private static void setAuthenticationUserName(Authentication authentication, UserProfileData userProfileData) {
+    private static void setAuthenticationUserName(Authentication authentication, String userName) {
         authentication.setAuthorizationType(null);
-        authentication.setPayload(userProfileData.userId);
+        authentication.setPayload(userName);
     }
 }
